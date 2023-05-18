baefa62c148b377709e36fca26ef756bec62bf0d
huzidev <huzaifa.iqdev@gmail.com>
2023-05-19 04:40:18 +0500

    front-end linked

--- blog-web/service/start/routes.ts
+++ blog-web/service/start/routes.ts
@@ -20,6 +20,6 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async ({ view }) => {
-  return view.render('welcome')
+Route.get('/test', async () => {
+  return "Hello, World!"
 })
