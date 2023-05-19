bbb8e0d2aab8d66b62a2ff6a6ec768b247d23e91
huzidev <huzaifa.iqdev@gmail.com>
2023-05-19 18:40:33 +0500

    data fetched

--- blog-app/blog-web/src/App.tsx
+++ blog-app/blog-web/src/App.tsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
+
 export default function App(): JSX.Element {
+
+  const [blogs, setBlogs] = useState([]);
+
   async function test() {
-    const response = await fetch("http://127.0.0.1:3333/articles")
-    console.log("Response", response);
+    const response = await fetch("http://127.0.0.1:3333/articles");
+    const result = await response.json();
+    console.log("Result", result.data);
+    setBlogs(result.data);
   }
+
+  const fetchedData = blogs.map((ele: any) => {
+    return (
+      <>
+        <h1>
+          ID: {ele.id}
+        </h1>
+        <h2>
+          Title : {ele.title}
+        </h2>
+        <h3>
+          Text: {ele.content}
+        </h3>
+      </>
+    )
+  })
+
   return (
     <div>
       <button onClick={test}>
         Get Data
       </button>
+      {fetchedData}
     </div>
   )
 }
