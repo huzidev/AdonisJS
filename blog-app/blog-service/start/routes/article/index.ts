db3cdd579bc95fd03afe6420392c7387fd14c89f
huzidev <huzaifa.iqdev@gmail.com>
2023-05-19 16:24:39 +0500

    endpoint

--- blog-app/blog-service/start/routes/article/index.ts
+++ blog-app/blog-service/start/routes/article/index.ts
@@ -1,4 +1,8 @@
+import Route from '@ioc:Adonis/Core/Route';
 
 export default function ArticlesRoutes(params:type) {
     const path = "ArticlesController";
+    Route.get("/articles", () => {
+
+    })
 }
