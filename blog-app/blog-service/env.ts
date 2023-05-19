80c15b9f3e86861030c585336cbe9b29b069c846
huzidev <huzaifa.iqdev@gmail.com>
2023-05-19 15:27:30 +0500

    mysql databases added

--- blog-app/blog-service/env.ts
+++ blog-app/blog-service/env.ts
@@ -23,4 +23,11 @@ export default Env.rules({
 	SESSION_DRIVER: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
 	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+	// added this after installing lucid for database
+	DB_CONNECTION: Env.schema.string(),
+	MYSQL_HOST: Env.schema.string({ format: 'host' }),
+    MYSQL_PORT: Env.schema.number(),
+    MYSQL_USER: Env.schema.string(),
+    MYSQL_PASSWORD: Env.schema.string.optional(),
+    MYSQL_DB_NAME: Env.schema.string()
 })
