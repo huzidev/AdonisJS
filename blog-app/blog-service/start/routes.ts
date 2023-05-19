6c2cabdcf18603a5a737a284134aa99f59010861
huzidev <huzaifa.iqdev@gmail.com>
2023-05-19 13:36:09 +0500

    blog app

--- blog-app/blog-service/start/routes.ts
+++ blog-app/blog-service/start/routes.ts
@@ -20,6 +20,6 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/test', async () => {
+Route.get('*', async () => {
   return "Hello, World!"
 })
--- blog-app/blog-web/src/App.tsx
+++ blog-app/blog-web/src/App.tsx
@@ -1,6 +1,6 @@
 export default function App(): JSX.Element {
   async function test() {
-    const response = await fetch("http://127.0.0.1:3333/test")
+    const response = await fetch("http://127.0.0.1:3333")
     console.log("Response", response);
   }
   return (
