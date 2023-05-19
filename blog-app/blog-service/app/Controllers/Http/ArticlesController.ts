355db8dc628ae5f5ad4834a03e3842be07b6a296
huzidev <huzaifa.iqdev@gmail.com>
2023-05-19 17:51:58 +0500

    get blogs

--- blog-app/blog-service/app/Controllers/Http/ArticlesController.ts
+++ blog-app/blog-service/app/Controllers/Http/ArticlesController.ts
@@ -4,7 +4,7 @@ import Database from "@ioc:Adonis/Lucid/Database";
 // Controller used to call the functions created in routes here so routes won't get messed up
 
 export default class ArticlesController {
-    public async view() {
+    public async getBlogs() {
         const response = await Database.from("articles").select("*");
         return response;  
     }
--- blog-app/blog-service/start/routes/article/index.ts
+++ blog-app/blog-service/start/routes/article/index.ts
@@ -2,5 +2,5 @@ import Route from '@ioc:Adonis/Core/Route';
 
 export default function ArticlesRoutes() {
     const path = "ArticlesController";
-    Route.get("/articles", `${path}.view`); // .view is function created in app/controllers/ArticlesController.ts
+    Route.get("/articles", `${path}.getBlogs`); // .getBlogs is function created in app/controllers/ArticlesController.ts
 }
