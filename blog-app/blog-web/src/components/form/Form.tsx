04a7832c5caba28ad3846133d8d1d4e837c39601
huzidev <huzaifa.iqdev@gmail.com>
2023-05-19 18:59:43 +0500

    blogs text

--- blog-app/blog-web/src/components/form/Form.tsx
+++ blog-app/blog-web/src/components/form/Form.tsx
@@ -5,6 +5,9 @@ export default function Form() {
         <h1>
             Add blogs
         </h1>
+        <input type="text" name="title" placeholder="Article's Title"/>
+        <input type="file" name="image" />
+        <textarea name="content" id="" cols="30" rows="10" placeholder="Enter Yours Text Here"></textarea>
     </div>
   )
 }
