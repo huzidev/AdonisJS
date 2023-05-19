5b13c1879924d2d905309d3ef0c0d691a8539bc6
huzidev <huzaifa.iqdev@gmail.com>
2023-05-19 16:01:45 +0500

    querey fetched

--- blog-app/blog-web/src/App.tsx
+++ blog-app/blog-web/src/App.tsx
@@ -1,12 +1,12 @@
 export default function App(): JSX.Element {
   async function test() {
-    const response = await fetch("http://127.0.0.1:3333")
+    const response = await fetch("http://127.0.0.1:3333/articles")
     console.log("Response", response);
   }
   return (
     <div>
       <button onClick={test}>
-        Check
+        Get Data
       </button>
     </div>
   )
