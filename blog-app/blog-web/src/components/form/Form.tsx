6d660997c7aefcd875438eb5660832e8f05c6172
huzidev <huzaifa.iqdev@gmail.com>
2023-05-19 20:00:35 +0500

    image added

--- blog-app/blog-web/src/components/form/Form.tsx
+++ blog-app/blog-web/src/components/form/Form.tsx
@@ -1,13 +1,74 @@
+import axios from "axios";
+import { useState } from "react";
+import { ArticleType } from "./types";
 
-export default function Form() {
+export default function Form(): JSX.Element {
+  const initialState: ArticleType = {title: "", image: "", content: ""};
+
+  const [article, setArticle] = useState(initialState)
+
+  const { title, image, content } = article;
+
+  function inputHandler(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    setArticle({
+      ...article,
+      [e.target.name]: e.target.value
+    });
+  };
+
+   var data: ArticleType = {
+      title,
+      image,
+      content
+  };
+
+  const config = {
+    method: 'post',
+    url: "http://127.0.0.1:3333/add",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    data: JSON.stringify(data)
+  }
+
+  async function addBlog() {
+    try {
+      const response = await axios(config);
+      console.log("Response", response);
+    } catch (e) {
+      console.log("Error", e);
+    }
+  }
+
   return (
     <div>
         <h1>
             Add blogs
         </h1>
-        <input type="text" name="title" placeholder="Article's Title"/>
-        <input type="file" name="image" />
-        <textarea name="content" id="" cols="30" rows="10" placeholder="Enter Yours Text Here"></textarea>
+          <input 
+            type="text" 
+            name="title" 
+            value={title}
+            onChange={inputHandler}
+            placeholder="Article's Title"
+            />
+          <input 
+            type="file" 
+            name="image" 
+            onChange={inputHandler}
+            value={image} 
+            />
+          <textarea 
+            name="content" 
+            value={content} 
+            onChange={inputHandler}
+            id="" cols={30} 
+            rows={10} 
+            placeholder="Enter Yours Text Here">
+          </textarea>
+          <button onClick={addBlog}>
+            Add Blog
+          </button>
     </div>
   )
 }
