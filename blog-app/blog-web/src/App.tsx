c26eee216a57c9c7b71ea71b38671efbd2989dfa
huzidev <huzaifa.iqdev@gmail.com>
2023-05-19 18:54:51 +0500

    nav bar added

--- blog-app/blog-web/src/App.tsx
+++ blog-app/blog-web/src/App.tsx
@@ -5,11 +5,13 @@ import {
 } from "react-router-dom";
 import Blogs from "./components/blogs/Blogs";
 import Form from "./components/form/Form";
+import Nav from "./components/nav/Nav";
 
 export default function App(): JSX.Element {
   return (
     <div> 
       <Router>
+        <Nav />
         <Routes>
           Stable-diffsuion
           <Route path="/blogs" element={<Blogs />} />
--- blog-app/blog-web/src/components/nav/Nav.tsx
+++ blog-app/blog-web/src/components/nav/Nav.tsx
@@ -1,8 +1,10 @@
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
   return (
     <div>
-
+        <Link to="/blogs">Blogs</Link>
+        <Link to="/add">Add Blogs</Link>
     </div>
   )
 }
