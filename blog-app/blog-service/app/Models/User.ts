aed898e5c35e284f74960728840b920edc58045d
huzidev <huzaifa.iqdev@gmail.com>
2023-05-22 23:13:02 +0500

    users model

--- blog-app/blog-service/app/Models/User.ts
+++ blog-app/blog-service/app/Models/User.ts
@@ -1,6 +1,6 @@
-import Hash from '@ioc:Adonis/Core/Hash'
-import { BaseModel, beforeSave, column } from '@ioc:Adonis/Lucid/Orm'
-import { DateTime } from 'luxon'
+import Hash from '@ioc:Adonis/Core/Hash';
+import { BaseModel, beforeSave, column } from '@ioc:Adonis/Lucid/Orm';
+import { DateTime } from 'luxon';
 
 export default class User extends BaseModel {
   // created automatically from node ace auth configure
--- blog-app/blog-service/start/routes/user/index.ts
+++ blog-app/blog-service/start/routes/user/index.ts
@@ -2,5 +2,5 @@ import Route from '@ioc:Adonis/Core/Route';
 
 export default function ArticlesRoutes() {
     const path = "UsersController";
-    Route.get("/articles", `${path}.getBlogs`);
+    Route.post("/signup", `${path}.create`);
 }
