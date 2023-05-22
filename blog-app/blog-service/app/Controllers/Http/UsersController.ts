856b1c61638f792ff902099183a0b41bbcc70e98
huzidev <huzaifa.iqdev@gmail.com>
2023-05-22 23:16:18 +0500

    duplicate entry error

--- blog-app/blog-service/app/Controllers/Http/UsersController.ts
+++ blog-app/blog-service/app/Controllers/Http/UsersController.ts
@@ -2,6 +2,16 @@
 
 export default class UsersController {
     public async create() {
-
+        try {
+            
+        } catch (e) {
+            if (e.code === 'ER_DUP_ENTRY') {
+                throw {
+                message: 'Email already in use',
+                status: 409,
+                }
+            }
+            throw e
+        }
     }
 }
