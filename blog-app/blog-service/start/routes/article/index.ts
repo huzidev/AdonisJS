669ee9940925f0b61f81a990274169d9bcbedc49
huzidev <huzaifa.iqdev@gmail.com>
2023-05-22 18:35:09 +0500

    delete route

--- blog-app/blog-service/start/routes/article/index.ts
+++ blog-app/blog-service/start/routes/article/index.ts
@@ -4,6 +4,10 @@ export default function ArticlesRoutes() {
     const path = "ArticlesController";
     Route.get("/articles", `${path}.getBlogs`); // .getBlogs is function created in app/controllers/ArticlesController.ts
     Route.post("/add_article", `${path}.addBlog`);
-    Route.get("/article/:id", `${path}.getById`).where("id", /^[0-9]+$/);
-    Route.put("/article/edit/:id", `${path}.updateBlog`).where("id", /^[0-9]+$/);
+    Route.get("/article/:id", `${path}.getById`)
+        .where("id", /^[0-9]+$/);
+    Route.put("/article/edit/:id", `${path}.updateBlog`)
+        .where("id", /^[0-9]+$/);
+    Route.delete("/articles/delete/:id", `${path}.deleteBlog`)
+        .where("id", /^[0-9]+$/);
 }
