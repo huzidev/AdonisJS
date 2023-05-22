0d23b633e4fc269b3b9ee65bb3907a6aa0fc3618
huzidev <huzaifa.iqdev@gmail.com>
2023-05-22 22:29:23 +0500

    input handler

--- blog-app/blog-web/src/components/pages/auth/login/Login.tsx
+++ blog-app/blog-web/src/components/pages/auth/login/Login.tsx
@@ -4,7 +4,15 @@ import { AuthLogin } from "./types";
 export default function login() {
     const initialState: AuthLogin = { username: "", password: "" }
     const [user, setUser] = useState(initialState)
-  
+    
+    function inputHandler(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        setUser({
+        ...user,
+        [e.target.name]: e.target.value
+        });
+    };
+        
+
     return (
         <div>
             <h1>
