dcc576383028c63ac0a8d82184c869ca9e72845d
huzidev <huzaifa.iqdev@gmail.com>
2023-05-22 23:13:50 +0500

    users confirmation model

--- blog-app/blog-service/app/Models/User.ts
+++ blog-app/blog-service/app/Models/User.ts
@@ -8,6 +8,9 @@ export default class User extends BaseModel {
   public id: number
 
   @column()
+  public username: string
+
+  @column()
   public email: string
 
   @column({ serializeAs: null })
