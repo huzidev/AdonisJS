fd949e5ee1366c206a058f5daf8cd97a153200f5
huzidev <huzaifa.iqdev@gmail.com>
2023-05-22 23:23:05 +0500

    user registeration

--- blog-app/blog-service/app/Controllers/Http/UsersController.ts
+++ blog-app/blog-service/app/Controllers/Http/UsersController.ts
@@ -1,13 +1,21 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
+import User from "App/Models/User";
+import { CreateUser } from "App/Validators/CreateUserValidator";
+
 export default class UsersController {
-    public async create() {
+    public async create({ request }) {
         try {
-            
+            const body = await request.validate(CreateUser);
+            await User.create({ ...body });
+            return {
+                data: body, 
+                message: "User registered successfully" 
+            }
         } catch (e) {
-            if (e.code === 'ER_DUP_ENTRY') {
+            if (e.code === "ER_DUP_ENTRY") {
                 throw {
-                message: 'Email already in use',
+                message: "Email already in use",
                 status: 409,
                 }
             }
