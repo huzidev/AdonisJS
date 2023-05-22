00d99197dbd8117753e861cea956c8a1773843c4
huzidev <huzaifa.iqdev@gmail.com>
2023-05-22 23:53:14 +0500

    calling signin function

--- blog-app/blog-web/src/components/pages/auth/login/Login.tsx
+++ blog-app/blog-web/src/components/pages/auth/login/Login.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 import { AuthState } from "./types";
 
@@ -11,10 +12,26 @@ export default function Login() {
             [e.target.name]: e.target.value
         });
     };
+    
     const { username, password } = user;
 
+    var data: AuthState = {
+        username,
+        password
+    };
+
+    const config = {
+        method: 'post',
+        url: "http://127.0.0.1:3333/signin",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify(data)
+    };
+
     async function login() {
-        console.log("login");
+        const response = await axios(config);
+        console.log("Response for login", response);
     }
 
     return (
