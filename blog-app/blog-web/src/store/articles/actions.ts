ed8dd8aa8fda3990a4d336178f02afe2c2630bd9
huzidev <huzaifa.iqdev@gmail.com>
2023-05-23 20:21:17 +0500

    blogs array

--- blog-app/blog-web/src/store/articles/actions.ts
+++ blog-app/blog-web/src/store/articles/actions.ts
@@ -2,6 +2,8 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import api from "../../services/api";
 import * as endpoints from "./endpoints";
 
+const 
+
 export const getBlog = createAsyncThunk(endpoints.GET_BLOG, async () => {
     try {
        const response = await api.get(endpoints.GET_BLOG);
---
+++ blog-app/blog-web/src/store/articles/types.ts
@@ -0,0 +1,7 @@
+export interface BlogsState {
+    id: number;
+    custom_id: number;
+    title: string;
+    image: string
+    content: string
+}
