effff92dcc5b1c71ac1934e9df8be8cb6d9399f4
huzidev <huzaifa.iqdev@gmail.com>
2023-05-23 20:27:05 +0500

    extra reducers redux

--- blog-app/blog-web/src/store/articles/actions.ts
+++ blog-app/blog-web/src/store/articles/actions.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import api from "../../services/api";
 import * as endpoints from "./endpoints";
 import { BlogState } from "./types";
@@ -11,8 +11,21 @@ export const getBlog = createAsyncThunk(endpoints.GET_BLOG, async () => {
     try {
        const response = await api.get(endpoints.GET_BLOG);
        console.log("Response from redux", response.data);
+       return response.data;
     } catch (e) {
         console.log("Error", e);
     }
 });
 
+const getBlogSlice = createSlice({
+    name: "blog",
+    initialState: initialState,
+    reducers : {},
+    extraReducers: (builder) => {
+        builder.addCase(getBlog.fulfilled, (state, action) => {
+            state.blogs = action.payload;
+        })
+    }
+})
+
+export const imageAction = getBlogSlice.actions;
