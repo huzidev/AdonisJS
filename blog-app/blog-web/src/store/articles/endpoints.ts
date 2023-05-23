25456169163d953d2bcfe19b4bcc729526f52a53
huzidev <huzaifa.iqdev@gmail.com>
2023-05-24 01:04:48 +0500

    interface typ

--- blog-app/blog-web/src/store/articles/endpoints.ts
+++ blog-app/blog-web/src/store/articles/endpoints.ts
@@ -2,10 +2,4 @@ export const GET_BLOGS = "/articles";
 export const DELETE_BLOG = "/article/delete/";
 export const UPDATE_BLOG = "/article/edit/";
 export const GET_BLOG = "/article/";
-
-export const LIST = '/user/v1/list/';
-export const CREATE = '/user/v1/add';
-
-export const GET_BY_ID = '/user/v1/get/';
-export const UPDATE_BY_ID = '/user/v1/edit/';
-export const REMOVE_BY_ID = '/user/v1/remove/';
+export const ADD_BLOG = "/article/";
