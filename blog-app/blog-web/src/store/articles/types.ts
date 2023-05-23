35318f2d23376315a69804413d1b1679e23f1750
huzidev <huzaifa.iqdev@gmail.com>
2023-05-23 20:23:57 +0500

    blogs fetch

--- blog-app/blog-web/src/store/articles/types.ts
+++ blog-app/blog-web/src/store/articles/types.ts
@@ -1,7 +1,11 @@
-export interface BlogsState {
+export interface BlogDetails {
     id: number;
     custom_id: number;
     title: string;
     image: string
     content: string
+}
+
+export interface BlogState {
+    blogs: BlogDetails[];
 }
