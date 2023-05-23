7383567597b735c76edb8a67c84816e6ad6fc178
huzidev <huzaifa.iqdev@gmail.com>
2023-05-23 20:47:23 +0500

    blogs fethced

--- blog-app/blog-web/src/components/pages/articles/blogs/Blogs.tsx
+++ blog-app/blog-web/src/components/pages/articles/blogs/Blogs.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { getBlogs } from "../../../../store/articles/actions";
 import { useAppDispatch, useAppSelector } from "../../../../store/hooks/hooks";
@@ -7,23 +7,12 @@ import { useAppDispatch, useAppSelector } from "../../../../store/hooks/hooks";
 export default function Blogs(): JSX.Element {
   const dispatch = useAppDispatch();
   const allBlogs: any = useAppSelector(state => state.blogs.allBlogs);
-  const Navigate = useNavigate()
-  const [blogs, setBlogs] = useState(allBlogs);
+  const Navigate = useNavigate();
 
-  console.log("all", allBlogs);
-  
+  useEffect(() => {
+    dispatch(getBlogs());
+  }, [])
 
-  function callFromRedux() {
-    dispatch(getBlogs())
-  }
-
-  // async function getBlogs() {
-  //   const response = await fetch("http://127.0.0.1:3333/articles");
-  //   const result = await response.json();
-  //   console.log("Result", result.data);
-  //   setBlogs(result.data);
-  // }
-  
   async function deleteBlog(id: number) {
     const config = {
       method: 'delete',
@@ -36,39 +25,32 @@ export default function Blogs(): JSX.Element {
     alert("Blog deleted successfully");
   }
   
-  // useEffect(() => {
-  //   getBlogs();
-  // }, [])
-  
-  // const fetchedData = blogs.map((ele: any) => {
-  //   return (
-  //     <div key={ele.id}>
-  //       <h1>
-  //         ID: {ele.id}
-  //       </h1>
-  //       {/* <img src={ele.image} alt="Thumbnail" /> */}
-  //       <h2>
-  //         Title : {ele.title}
-  //       </h2>
-  //       <h3>
-  //         Text: {ele.content}
-  //       </h3>
-  //       <button onClick={() => Navigate(`/update/${ele.id}`)}>
-  //         Edit
-  //       </button>
-  //       <button onClick={() => deleteBlog(ele.id)}>
-  //         Delete
-  //       </button>
-  //     </div>
-  //   )
-  // })
+  const fetchedData = allBlogs.map((ele: any) => {
+    return (
+      <div key={ele.id}>
+        <h1>
+          ID: {ele.id}
+        </h1>
+        {/* <img src={ele.image} alt="Thumbnail" /> */}
+        <h2>
+          Title : {ele.title}
+        </h2>
+        <h3>
+          Text: {ele.content}
+        </h3>
+        <button onClick={() => Navigate(`/update/${ele.id}`)}>
+          Edit
+        </button>
+        <button onClick={() => deleteBlog(ele.id)}>
+          Delete
+        </button>
+      </div>
+    )
+  })
 
   return (
     <div>
-      {/* {fetchedData} */}
-      <button onClick={callFromRedux}>
-        Call from redux
-      </button>
+      {fetchedData}
     </div>
   )
 }
