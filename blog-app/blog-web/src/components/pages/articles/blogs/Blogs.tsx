cbae80d7c414eac1222d5d045640072cdf1440a7
huzidev <huzaifa.iqdev@gmail.com>
2023-05-23 21:00:28 +0500

    delete button worked

--- blog-app/blog-web/src/components/pages/articles/blogs/Blogs.tsx
+++ blog-app/blog-web/src/components/pages/articles/blogs/Blogs.tsx
@@ -1,30 +1,17 @@
-import axios from "axios";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { getBlogs } from "../../../../store/articles/actions";
+import { deleteBlog, getBlogs } from "../../../../store/articles/actions";
 import { useAppDispatch, useAppSelector } from "../../../../store/hooks/hooks";
 
 export default function Blogs(): JSX.Element {
   const dispatch = useAppDispatch();
-  const allBlogs: any = useAppSelector(state => state.blogs.allBlogs);
+  const allBlogs = useAppSelector(state => state.blogs.allBlogs);
   const Navigate = useNavigate();
 
   useEffect(() => {
     dispatch(getBlogs());
   }, [])
 
-  async function deleteBlog(id: number) {
-    const config = {
-      method: 'delete',
-      url: `http://127.0.0.1:3333/article/delete/${id}`,
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }
-    await axios(config);
-    alert("Blog deleted successfully");
-  }
-  
   const fetchedData = allBlogs.map((ele: any) => {
     return (
       <div key={ele.id}>
@@ -41,7 +28,7 @@ export default function Blogs(): JSX.Element {
         <button onClick={() => Navigate(`/update/${ele.id}`)}>
           Edit
         </button>
-        <button onClick={() => deleteBlog(ele.id)}>
+        <button onClick={() => dispatch(deleteBlog(ele.id))}>
           Delete
         </button>
       </div>
