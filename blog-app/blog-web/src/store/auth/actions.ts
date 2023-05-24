72b773be15a1e2d4212282e4985a4fce66b71a61
huzidev <huzaifa.iqdev@gmail.com>
2023-05-24 13:30:45 +0500

    await axios

--- blog-app/blog-web/src/store/auth/actions.ts
+++ blog-app/blog-web/src/store/auth/actions.ts
@@ -14,10 +14,11 @@ export const signUp = createAsyncThunk(endpoints.SIGNUP, async (data: UserState)
 
 export const signIn = createAsyncThunk(endpoints.SIGNIN, async (data: UserSignInState) => {
     try {
+        const response = await api.post(endpoints.SIGNIN, data);
+        console.log("Sign in resp", response);
         
     } catch (e) {
         console.log("Error", e);
-        
     }
 })
 
