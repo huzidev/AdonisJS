21b338d480f25cd7eeab654273cf9aebb18d5f8c
huzidev <huzaifa.iqdev@gmail.com>
2023-05-26 19:36:27 +0500

    user slug

--- blog-app/blog-web/src/components/pages/articles/BlogView/index.tsx
+++ blog-app/blog-web/src/components/pages/articles/BlogView/index.tsx
@@ -1,22 +1,38 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getBlog } from "../../../../store/articles/actions";
-import { useAppDispatch } from "../../../../store/hooks/hooks";
+import { useAppDispatch, useAppSelector } from "../../../../store/hooks/hooks";
 
 export default function BlogView(): JSX.Element {
   const params = useParams();
+  const allBlogs = useAppSelector((state) => state.blogs.getBlog);
   const dispatch = useAppDispatch();
 
-  function call() {
+  useEffect(() => {
     dispatch(getBlog(Number(params.id)))
-    console.log("id", params);
-  }
+  }, [])
+
+  // const { title, content } = allBlogs;
 
   return (
     <div>
         Blog
-        <button onClick={call}>
-          test 
-        </button>
+        <div 
+        className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <img
+            className="rounded-t-lg"
+            src="/docs/images/blog/image-1.jpg"
+            alt="Thumbnail"
+          />
+          <div className="p-5">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Title
+            </h5> 
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Message
+            </p>
+          </div>
+        </div>
     </div>
   )
 }
