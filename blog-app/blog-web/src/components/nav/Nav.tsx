1ab7a9bc5e1d816ed7a77ae509049f633cd7f272
huzidev <huzaifa.iqdev@gmail.com>
2023-05-26 14:44:22 +0500

    nav components

--- blog-app/blog-web/src/components/nav/Nav.tsx
+++ blog-app/blog-web/src/components/nav/Nav.tsx
@@ -37,13 +37,12 @@ export default function Nav() {
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
-                <a
-                  href="#"
+                <Link 
+                  to="/blogs"
                   className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                  aria-current="page"
                 >
-                  Home
-                </a>
+                  Blogs
+                </Link>
               </li>
               <li>
                 <a
