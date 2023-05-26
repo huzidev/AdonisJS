f686bffc668e6552b0df96449a68d44be51f2243
huzidev <huzaifa.iqdev@gmail.com>
2023-05-26 17:14:09 +0500

    buttons added

--- blog-app/blog-web/src/components/pages/articles/BlogView/index.tsx
+++ blog-app/blog-web/src/components/pages/articles/BlogView/index.tsx
@@ -10,7 +10,6 @@ export default function Blogs(): JSX.Element {
   const Navigate = useNavigate();
 
   console.log("all blogs", allBlogs);
-  
 
   useBlogsPageHooks();
   // useEffect(() => {
@@ -21,9 +20,6 @@ export default function Blogs(): JSX.Element {
     return (
       <div key={ele.id}>
         {/* <img src={ele.image} alt="Thumbnail" /> */}
-        <h3>Text: {ele.content}</h3>
-        <button onClick={() => Navigate(`/update/${ele.id}`)}>Edit</button>
-        <button onClick={() => dispatch(deleteBlog(ele.id))}>Delete</button>
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
           <img
             className="rounded-t-lg"
@@ -43,12 +39,28 @@ export default function Blogs(): JSX.Element {
             > 
               Read More
             </Link> */}
-            <button 
-            className="text-sm font-medium text-center text-white hover:text-blue-500"
-            onClick={() => console.log("custom id", ele.custom_id)}
+            <button
+              className="text-sm font-medium text-center text-white hover:text-blue-500"
+              onClick={() => console.log("custom id", ele.custom_id)}
             >
               Read More
             </button>
+            <div>
+              <button
+                type="button"
+                className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                onClick={() => Navigate(`/update/${ele.id}`)}
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                onClick={() => dispatch(deleteBlog(ele.id))}
+              >
+                Delete
+              </button>
+            </div>
           </div>
         </div>
       </div>
