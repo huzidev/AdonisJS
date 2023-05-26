acbf2488ac8e1c0386b38841f512735b63901101
huzidev <huzaifa.iqdev@gmail.com>
2023-05-26 17:20:38 +0500

    cards button

--- blog-app/blog-web/src/components/pages/articles/BlogView/index.tsx
+++ blog-app/blog-web/src/components/pages/articles/BlogView/index.tsx
@@ -48,14 +48,14 @@ export default function Blogs(): JSX.Element {
             <div>
               <button
                 type="button"
-                className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                className="text-white bg-gray-800 font-medium text-sm py-2.5"
                 onClick={() => Navigate(`/update/${ele.id}`)}
               >
                 Edit
               </button>
               <button
                 type="button"
-                className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                className="text-white bg-gray-800 font-medium text-sm ml-4 py-2.5"
                 onClick={() => dispatch(deleteBlog(ele.id))}
               >
                 Delete
