783c3c21755083ba47514e1c505f605cb68d694e
huzidev <huzaifa.iqdev@gmail.com>
2023-05-27 14:41:15 +0500

    ternary operator

--- blog-app/blog-web/src/components/nav/Nav.tsx
+++ blog-app/blog-web/src/components/nav/Nav.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { signOut } from "../../store/auth/actions";
+import KEYS from "../../store/auth/keys";
 import { useAppDispatch } from "../../store/hooks/hooks";
 
 export default function Nav() {
@@ -9,9 +10,9 @@ export default function Nav() {
     <div>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Blog App
-            </span>
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Blog App
+          </span>
           <button
             data-collapse-toggle="navbar-default"
             type="button"
@@ -37,7 +38,7 @@ export default function Nav() {
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
-                <Link 
+                <Link
                   to="/blogs"
                   className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                 >
@@ -45,37 +46,42 @@ export default function Nav() {
                 </Link>
               </li>
               <li>
-                <Link 
+                <Link
                   to="/add"
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Add Blog
                 </Link>
               </li>
-              <li>
-                <Link 
-                  to="/login"
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Login
-                </Link>
-              </li>
-              <li>
-                <Link   
-                  to="/register"
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Register
-                </Link>
-              </li>
-              <li>
-                <button   
-                  onClick={() => dispatch(signOut())}
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  SingOut
-                </button>
-              </li>
+              {localStorage.getItem(KEYS.TOKEN) ? (
+                <>
+                  <li>
+                    <Link
+                      to="/login"
+                      className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                    >
+                      Login
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/register"
+                      className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                    >
+                      Register
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <li>
+                  <button
+                    onClick={() => dispatch(signOut())}
+                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  >
+                    SingOut
+                  </button>
+                </li>
+              )}
             </ul>
           </div>
         </div>
