52f17ecfe1e8386f4ba9191d817620cca2897253
huzidev <huzaifa.iqdev@gmail.com>
2023-05-27 13:23:21 +0500

    route paths

--- blog-app/blog-service/start/routes/user/index.ts
+++ blog-app/blog-service/start/routes/user/index.ts
@@ -4,4 +4,5 @@ export default function UsersRoutes() {
     const path = "UsersController";
     Route.post("/sign_up", `${path}.signUp`);
     Route.post("/sign_in", `${path}.signIn`);
+    Route.post("/sign_out", `${path}.signOut`);
 }
