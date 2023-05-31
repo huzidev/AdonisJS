0f38a5c61b341f17fa9e596ebb790956fe90d907
huzidev <huzaifa.iqdev@gmail.com>
2023-05-31 11:38:43 +0500

    routes path

---
+++ blog-app/blog-web/src/Router/index.tsx
@@ -0,0 +1,30 @@
+import {
+    Route,
+    BrowserRouter as Router,
+    Routes
+} from "react-router-dom";
+import routes from './Router/routes';
+export default function App(): JSX.Element {
+  return (
+      <UserState>
+        <Router>
+          <Routes>
+          {routes.map(({ Component, path }) => {
+            return (
+              <Route
+              key={path}
+              path={path}
+              element={
+                <PageWrapper>
+                  <Component  />
+                </PageWrapper>
+              }
+              />
+            )
+          })} 
+            <Route path="*" element={<Error />}/>
+          </Routes>
+        </Router>
+      </UserState>
+  )
+}
---
+++ blog-app/blog-web/src/Router/paths.ts
@@ -0,0 +1,12 @@
+const ROUTE_PATHS = {
+  HOME: '/',
+  USER_SIGNUP: '/user/login',
+  USER_SIGNIN: '/user/register/',
+
+  ARTICLES: '/blogs/',
+  ARTICLES_VIEW: '/blog/',
+  ARTICLES_CREATE: '/blog/add',
+  ARTICLES_UPDATE: '/blog/edit/',
+};
+
+export default ROUTE_PATHS;
