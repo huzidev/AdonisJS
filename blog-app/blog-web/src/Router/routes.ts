5b0d988a2719f007f7b7fbe1068e80de4b6746b3
huzidev <huzaifa.iqdev@gmail.com>
2023-05-31 11:53:42 +0500

    react users path

--- blog-app/blog-web/src/Router/routes.ts
+++ blog-app/blog-web/src/Router/routes.ts
@@ -1,1 +1,14 @@
 
+
+interface AppRoute {
+  exact?: boolean;
+  path: string;
+  Component: React.LazyExoticComponent<() => JSX.Element>;
+}
+
+export const routes: AppRoute[] = [
+    {
+        exact: true,
+        
+    }
+]
