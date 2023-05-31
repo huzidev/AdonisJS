f981e850a6e2f59b631e0e3ec5c8916f9d63c70c
huzidev <huzaifa.iqdev@gmail.com>
2023-05-31 18:47:35 +0500

    button updated

--- blog-app/blog-web/src/pages/NotFound/index.tsx
+++ blog-app/blog-web/src/pages/NotFound/index.tsx
@@ -14,7 +14,7 @@ export default function NotFoundPage(): JSX.Element {
         dark:border-gray-700
       "
     >
-      <div className="px-40 py-20 bg-white rounded-md shadow-xl">
+      <div className="px-40 py-20 rounded-md shadow-xl bg-white border-gray-200 dark:bg-gray-900">
         <div className="flex flex-col items-center">
           <h1 className="font-bold text-blue-600 text-9xl">404</h1>
           <h6 className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
@@ -23,7 +23,10 @@ export default function NotFoundPage(): JSX.Element {
           <p className="mb-8 text-center text-gray-500 md:text-lg">
             Sorry, the page you visited does not exist.
           </p>
-          <button onClick={() => Navigate("/blogs")}>
+          <button 
+            className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" 
+            onClick={() => Navigate("/blogs")}
+          >
             Back Home
           </button>
         </div>
