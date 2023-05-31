45c58108f6bd164f922bb02e5387f5dc7a91e229
huzidev <huzaifa.iqdev@gmail.com>
2023-05-31 12:05:37 +0500

    routes impotred

--- blog-app/blog-web/src/Router/index.tsx
+++ blog-app/blog-web/src/Router/index.tsx
@@ -1,30 +1,26 @@
-// import {
-//     Route,
-//     BrowserRouter as Router,
-//     Routes
-// } from "react-router-dom";
-// import routes from './Router/routes';
-// export default function AppRouter(): JSX.Element {
-//   return (
-//       <UserState>
-//         <Router>
-//           <Routes>
-//           {routes.map(({ Component, path }) => {
-//             return (
-//               <Route
-//               key={path}
-//               path={path}
-//               element={
-//                 <PageWrapper>
-//                   <Component  />
-//                 </PageWrapper>
-//               }
-//               />
-//             )
-//           })} 
-//             <Route path="*" element={<Error />}/>
-//           </Routes>
-//         </Router>
-//       </UserState>
-//   )
-// }
+import {
+    Route,
+    BrowserRouter as Router,
+    Routes
+} from "react-router-dom";
+import { routes } from './routes';
+export default function AppRouter(): JSX.Element {
+  return (
+    <Router>
+        <Routes>
+        {routes.map(({ Component, path }) => {
+        return (
+            <Route
+            key={path}
+            path={path}
+            element={
+                <Component  />
+            }
+            />
+        )
+        })} 
+        <Route path="*" element={<Error />}/>
+        </Routes>
+    </Router>
+  )
+}
