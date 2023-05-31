34b55162bac1549f986ed498c6928afc01289a19
huzidev <huzaifa.iqdev@gmail.com>
2023-05-31 20:19:02 +0500

    updating auth user

--- blog-app/blog-service/app/Controllers/Http/UsersController.ts
+++ blog-app/blog-service/app/Controllers/Http/UsersController.ts
@@ -8,23 +8,15 @@ export default class UsersController {
     public async signUp({ request, auth }: HttpContextContract) {
         try {
             const body = await request.validate(CreateUser);
-            const userExist = await User.findBy("username", body.username);
-            if (userExist) {
-                throw {
-                    message: "Username already exist",
-                    status: 409
-                }
-            } else if (!userExist) {
-                // because we used new User() therefore use user.save() for saving at database
-                const user = new User();
-                user.fill(body);
-                await user.save();
-                const { token } = await auth.login(user);
-                return {
-                    token,
-                    data: user, 
-                    message: "User registered successfully" 
-                }
+            // because we used new User() therefore use user.save() for saving at database
+            const user = new User();
+            user.fill(body);
+            await user.save();
+            const { token } = await auth.login(user);
+            return {
+                token,
+                data: user, 
+                message: "User registered successfully" 
             }
         } catch (e) {
             if (e.code === "ER_DUP_ENTRY") {
