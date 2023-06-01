d4c9f98bea4ecddd7f55acee45bbac1fca6e0620
huzidev <huzaifa.iqdev@gmail.com>
2023-06-01 17:11:52 +0500

    margin

--- blog-app/blog-web/src/pages/auth/login/index.tsx
+++ blog-app/blog-web/src/pages/auth/login/index.tsx
@@ -33,9 +33,8 @@ export default function UserSignInPage() {
           Sign in to your account
         </h2>
       </div>
-
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <div>
+          <div className="mb-3">
             <label
               htmlFor="email"
               className="block text-sm font-medium leading-6 text-gray-900"
