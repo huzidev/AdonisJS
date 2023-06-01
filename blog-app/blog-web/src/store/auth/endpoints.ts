6a4421a0427cd3ba6dc41353bf816d0e6d7b4070
huzidev <huzaifa.iqdev@gmail.com>
2023-06-02 00:00:09 +0500

    endpoints updated

--- blog-app/blog-web/src/store/auth/endpoints.ts
+++ blog-app/blog-web/src/store/auth/endpoints.ts
@@ -2,4 +2,4 @@ export const SIGN_UP = "/auth/sign_up";
 export const SIGN_IN = "/auth/sign_in";
 export const SIGN_OUT = "/auth/sign_out";
 
-export const USER_DETAILS = '/auth/get/me';
+export const USER_DETAILS = '/user/get/me';
