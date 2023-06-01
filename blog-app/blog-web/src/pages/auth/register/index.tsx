024ea4d68d81b20afce86c3d8ffdd383e86ef391
huzidev <huzaifa.iqdev@gmail.com>
2023-06-01 17:02:56 +0500

    use state values

--- blog-app/blog-web/src/pages/auth/register/index.tsx
+++ blog-app/blog-web/src/pages/auth/register/index.tsx
@@ -98,14 +98,37 @@ export default function UserSignUpPage() {
                   id="password"
                   name="password"
                   type="password"
-                  value={user.password}
+                  value={password}
                   onChange={inputHandler}
                   required
                   className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
-
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Confirm Password
+                </label>
+                {/* <div className="text-sm">
+                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                    </div> */}
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="passwordConfirmation"
+                  type="password"
+                  value={passwordConfirmation}
+                  onChange={inputHandler}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
             <div>
               <button
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
