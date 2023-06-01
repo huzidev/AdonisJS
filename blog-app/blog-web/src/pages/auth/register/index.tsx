8668d8631131a84eefb7586469fa1b85fc63b3ef
huzidev <huzaifa.iqdev@gmail.com>
2023-06-01 17:03:29 +0500

    hyperlink

--- blog-app/blog-web/src/pages/auth/register/index.tsx
+++ blog-app/blog-web/src/pages/auth/register/index.tsx
@@ -134,18 +134,18 @@ export default function UserSignUpPage() {
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 onClick={signup}
               >
-                Sign in
+                Sign Up
               </button>
             </div>
           </form>
           <p className="mt-10 text-center text-sm text-gray-500">
-            Don't have an account?
+            Already have an account?
             <Link
-              to="/user/sign_up"
+              to="/user/sign_in"
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
               {" "}
-              Signup
+              Signin
             </Link>
           </p>
         </div>
