9ff6457d369cc64e5ff7d3ef3e6cef5f0aa684ea
huzidev <huzaifa.iqdev@gmail.com>
2023-06-01 16:54:27 +0500

    onClick event

--- blog-app/blog-web/src/pages/auth/login/index.tsx
+++ blog-app/blog-web/src/pages/auth/login/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { signIn } from "../../../store/auth/actions";
 import { useAppDispatch } from "../../../store/hooks/hooks";
 import { AuthState } from "./types";
@@ -31,30 +32,51 @@ export default function UserSignInPage() {
                 <div>
                     <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                     <div className="mt-2">
-                    <input id="email" name="email" type="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    <input 
+                        id="email" 
+                        name="email" 
+                        type="email" 
+                        value={user.email} 
+                        required
+                        onChange={inputHandler} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                    />
                     </div>
                 </div>
-
                 <div>
                     <div className="flex items-center justify-between">
                     <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                    <div className="text-sm">
+                    {/* <div className="text-sm">
                         <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-                    </div>
+                    </div> */}
                     </div>
                     <div className="mt-2">
-                    <input id="password" name="password" type="password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    <input 
+                        id="password" 
+                        name="password" 
+                        type="password" 
+                        value={user.password}
+                        onChange={inputHandler}
+                        required 
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                    />
                     </div>
                 </div>
 
                 <div>
-                    <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                    <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                        Sign in
+                    </button>
                 </div>
                 </form>
-
                 <p className="mt-10 text-center text-sm text-gray-500">
-                Not a member?
-                <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+                    Don't have an account?
+                    <Link 
+                        to="/user/sign_up"
+                        className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                    >
+                        {" "}Signup
+                    </Link>
                 </p>
             </div>
             </div>
