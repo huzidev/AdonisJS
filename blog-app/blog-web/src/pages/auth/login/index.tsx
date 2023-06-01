aa4fa6d8110417be3843d37d4911c2a1f8f2143b
huzidev <huzaifa.iqdev@gmail.com>
2023-06-01 16:14:47 +0500

    fixing login page error

--- blog-app/blog-web/src/pages/auth/login/index.tsx
+++ blog-app/blog-web/src/pages/auth/login/index.tsx
@@ -43,7 +43,7 @@ export default function UserSignInPage() {
                     </div>
                     </div>
                     <div className="mt-2">
-                    <input id="password" name="password" type="password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    <input id="password" name="password" type="password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                 </div>
 
