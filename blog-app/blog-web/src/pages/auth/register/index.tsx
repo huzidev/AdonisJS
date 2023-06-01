0c5204d3e1e5131aeca9f952b71fd447c35dc03e
huzidev <huzaifa.iqdev@gmail.com>
2023-06-01 17:01:52 +0500

    indentaion

--- blog-app/blog-web/src/pages/auth/register/index.tsx
+++ blog-app/blog-web/src/pages/auth/register/index.tsx
@@ -5,155 +5,158 @@ import { useAppDispatch } from "../../../store/hooks/hooks";
 import { RegisterState } from "./types";
 
 export default function UserSignUpPage() {
-    const initialState: RegisterState = { username: "", email: "", password: "", passwordConfirmation: "" };
-    const [user, setUser] = useState(initialState);
-    const dispatch = useAppDispatch();
-    
-    function inputHandler(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-        setUser({
-            ...user,
-            [e.target.name]: e.target.value
-        });
-    };
-    const { username, email, password, passwordConfirmation } = user;
+  const initialState: RegisterState = {
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+  };
+  const [user, setUser] = useState(initialState);
+  const dispatch = useAppDispatch();
 
-    async function signup() {
-        dispatch(signUp(user))
-    };
-    return (
-        <div>
-            <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign Up to your account
-        </h2>
-      </div>
+  function inputHandler(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  }
+  const { username, email, password, passwordConfirmation } = user;
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" method="POST">
-          <div>
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Usernmae
-            </label>
-            <div className="mt-2">
-              <input
-                id="username"
-                name="username"
-                type="text"
-                value={user.email}
-                required
-                onChange={inputHandler}
-                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Email
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                value={user.email}
-                required
-                onChange={inputHandler}
-                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div>
-            <div className="flex items-center justify-between">
+  async function signup() {
+    dispatch(signUp(user));
+  }
+  return (
+    <div>
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign Up to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" method="POST">
+            <div>
               <label
-                htmlFor="password"
+                htmlFor="username"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Password
+                Usernmae
               </label>
-              {/* <div className="text-sm">
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  value={user.email}
+                  required
+                  onChange={inputHandler}
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  value={user.email}
+                  required
+                  onChange={inputHandler}
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                {/* <div className="text-sm">
                         <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
                     </div> */}
-            </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                value={user.password}
-                onChange={inputHandler}
-                required
-                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <button
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={signup}
-            >
-              Sign in
-            </button>
-          </div>
-        </form>
-        <p className="mt-10 text-center text-sm text-gray-500">
-          Don't have an account?
-          <Link
-            to="/user/sign_up"
-            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >
-            {" "}
-            Signup
-          </Link>
-        </p>
-      </div>
-    </div>
-            <h1>
-                Register
-            </h1>
-            <input 
-                type="text"
-                name="username"
-                value={username} 
-                placeholder="Username"
-                onChange={inputHandler}
-            />
-            <input 
-                type="email"
-                name="email"
-                value={email} 
-                placeholder="Email"
-                onChange={inputHandler}
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={user.password}
+                  onChange={inputHandler}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
-            <input 
-                type="password"
-                name="password"
-                value={password} 
-                placeholder="Password"
-                onChange={inputHandler}
-            />
-            <input 
-                type="password"
-                name="passwordConfirmation"
-                value={passwordConfirmation} 
-                placeholder="Confirm Password"
-                onChange={inputHandler}
-            />
-            <button onClick={signup}>
-                Register
-            </button>
+              </div>
+            </div>
+
+            <div>
+              <button
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={signup}
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Don't have an account?
+            <Link
+              to="/user/sign_up"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              {" "}
+              Signup
+            </Link>
+          </p>
         </div>
-  )
+      </div>
+      <h1>Register</h1>
+      <input
+        type="text"
+        name="username"
+        value={username}
+        placeholder="Username"
+        onChange={inputHandler}
+      />
+      <input
+        type="email"
+        name="email"
+        value={email}
+        placeholder="Email"
+        onChange={inputHandler}
+      />
+      <input
+        type="password"
+        name="password"
+        value={password}
+        placeholder="Password"
+        onChange={inputHandler}
+      />
+      <input
+        type="password"
+        name="passwordConfirmation"
+        value={passwordConfirmation}
+        placeholder="Confirm Password"
+        onChange={inputHandler}
+      />
+      <button onClick={signup}>Register</button>
+    </div>
+  );
 }
