bf9d6d4ab2789a3c7650d8b7e987e6d282141af5
huzidev <huzaifa.iqdev@gmail.com>
2023-06-01 16:54:53 +0500

    onClick button link

--- blog-app/blog-web/src/pages/auth/login/index.tsx
+++ blog-app/blog-web/src/pages/auth/login/index.tsx
@@ -63,7 +63,9 @@ export default function UserSignInPage() {
                 </div>
 
                 <div>
-                    <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    <button 
+                        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        onClick={login}
                     >
                         Sign in
                     </button>
