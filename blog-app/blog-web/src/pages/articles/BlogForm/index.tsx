7db1670c7b634d92cfdc0ee65043ab8859e22d4b
huzidev <huzaifa.iqdev@gmail.com>
2023-06-01 17:55:44 +0500

    value type

--- blog-app/blog-web/src/pages/articles/BlogForm/index.tsx
+++ blog-app/blog-web/src/pages/articles/BlogForm/index.tsx
@@ -17,6 +17,68 @@ export default function AddBlogPage(): JSX.Element {
   };
   return (
     <div>
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          className="mx-auto h-10 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          alt="Your Company"
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Add Blog
+        </h2>
+      </div>
+      <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
+          <div className="mb-3">
+            <label
+              htmlFor="title"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Blog Title
+            </label>
+            <div className="mt-2">
+              <input
+                id="title"
+                name="title"
+                type="text"
+                value={title}
+                required
+                onChange={inputHandler}
+                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="mb-6">
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              {/* <div className="text-sm">
+                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                    </div> */}
+            </div>
+            <div className="mt-2 flex items-center">
+              <input
+                id="password"
+                name="password"
+                onChange={inputHandler}
+                required
+                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <button
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </button>
+          </div>
+      </div>
+    </div>
         <h1>
             Add blog
         </h1>
