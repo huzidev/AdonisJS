b457398947d1864d65d5fc04ff5db69581634206
huzidev <huzaifa.iqdev@gmail.com>
2023-06-02 17:15:02 +0500

    updating user profile

--- blog-app/blog-web/src/pages/user/Profile/index.tsx
+++ blog-app/blog-web/src/pages/user/Profile/index.tsx
@@ -2,6 +2,20 @@
 export default function ViewProfilePage() {
   return (
     <div>
+      <div 
+          className="w-11/12 my-8 mx-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-5">
+            <h1>
+              User Profile
+            </h1>
+            <h5 className="mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Name
+            </h5> 
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Email
+            </p>
+          </div>
+        </div>
       User Profile
     </div>
   )
