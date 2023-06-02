27ea08ae0d1a6e9d4948f541009ce545bfc54ac8
huzidev <huzaifa.iqdev@gmail.com>
2023-06-02 17:09:20 +0500

    adding user profile

--- blog-app/blog-web/src/components/Header/index.tsx
+++ blog-app/blog-web/src/components/Header/index.tsx
@@ -75,11 +75,12 @@ export default function Header() {
               ) : (
                 <>
                   <li>
-                    <button
+                    <Link
+                      to={ROUTE_PATHS.VIEW_PROFILE}
                       className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                     >
                       View Profile
-                    </button>
+                    </Link>
                   </li>
                   <li>
                     <button
