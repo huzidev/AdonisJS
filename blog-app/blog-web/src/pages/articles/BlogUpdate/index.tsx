c677ed1a94a11b0b0a707175f3cc2f853d5a93c4
huzidev <huzaifa.iqdev@gmail.com>
2023-06-02 15:57:10 +0500

    if statement

--- blog-app/blog-web/src/pages/articles/BlogUpdate/index.tsx
+++ blog-app/blog-web/src/pages/articles/BlogUpdate/index.tsx
@@ -31,6 +31,9 @@ export default function UpdateBlogPage(): JSX.Element {
   }, [getBlog]);
 
   function update() {
+    if ((title || image || content )) {
+      
+    }
     dispatch(
       updateBlog({
         ...updateArticle,
