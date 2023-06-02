916d9ae5058c3551f1b3f974a03ddfb26cba5c3a
huzidev <huzaifa.iqdev@gmail.com>
2023-06-02 15:15:11 +0500

    edit blog page updated

--- blog-app/blog-web/src/pages/articles/BlogUpdate/index.tsx
+++ blog-app/blog-web/src/pages/articles/BlogUpdate/index.tsx
@@ -8,44 +8,52 @@ export default function UpdateBlogPage(): JSX.Element {
   const dispatch = useAppDispatch();
   const getBlog = useAppSelector((s) => s.blogs.getBlog);
   useEditBlogPageHooks();
-  
-  const initialState: ArticleType = {id: null, title: "", image: "", content: ""};
+
+  const initialState: ArticleType = {
+    id: null,
+    title: "",
+    image: "",
+    content: "",
+  };
   const [updateArticle, setUpdateArticle] = useState(initialState);
-  
+
   const { id, title, image, content } = updateArticle;
-  
-  function inputHandler(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+
+  function inputHandler(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
     setUpdateArticle({
       ...updateArticle,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
-  };
-  
+  }
+
   useEffect(() => {
-    setUpdateArticle({...updateArticle, ...getBlog})
-  }, [getBlog])
+    setUpdateArticle({ ...updateArticle, ...getBlog });
+  }, [getBlog]);
 
   async function update() {
-    dispatch(updateBlog({
-      ...updateArticle, 
-      id
-    }));
+    dispatch(
+      updateBlog({
+        ...updateArticle,
+        id,
+      })
+    );
   }
   return (
-    <div> 
-        <div>
+    <div>
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Edit Yours Blog
-        </h2>
-      </div>
-      <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Edit Yours Blog
+          </h2>
+        </div>
+        <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
           <div className="mb-3">
             <label
               htmlFor="title"
@@ -57,11 +65,11 @@ export default function UpdateBlogPage(): JSX.Element {
               <input
                 id="title"
                 name="title"
+                value={title}
                 type="text"
                 required
                 onChange={inputHandler}
                 className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                placeholder="Write your blog title here..."
               />
             </div>
           </div>
@@ -81,58 +89,34 @@ export default function UpdateBlogPage(): JSX.Element {
           </div> */}
           <div className="mb-6">
             <div>
-              <label htmlFor="content" className="block text-sm font-medium leading-6 text-gray-900">
+              <label
+                htmlFor="content"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 Content
               </label>
               <div className="mt-2">
-                <textarea 
-                  name="content" 
-                  cols={30} 
-                  rows={10} 
+                <textarea
+                  name="content"
+                  cols={30}
+                  rows={10}
+                  value={content}
                   onChange={inputHandler}
-                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
-                  placeholder="Write your blog content here..."
-                  ></textarea>
-                </div>
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                ></textarea>
+              </div>
             </div>
           </div>
           <div>
             <button
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={update}
             >
-              Add Blog
+              Update Blog
             </button>
           </div>
+        </div>
       </div>
     </div>
-    </div>
-        <h1>
-            Update Blog
-        </h1>
-          <input 
-            type="text" 
-            name="title" 
-            value={title}
-            onChange={inputHandler}
-            placeholder="Article's Title"
-            />
-          {/* <input 
-            type="file" 
-            name="image" 
-            onChange={inputHandler}
-            value={image} 
-            /> */}
-          <textarea 
-            name="content" 
-            value={content} 
-            onChange={inputHandler}
-            id="" cols={30} 
-            rows={10} 
-            placeholder="Enter Yours Text Here">
-          </textarea>
-          <button onClick={update}>
-            Update Blog
-          </button>
-    </div>
-  )
+  );
 }
