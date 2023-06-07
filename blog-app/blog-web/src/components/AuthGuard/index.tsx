a7de1f88898e326832a75ef6d90fb7cff28df4da
huzidev <huzaifa.iqdev@gmail.com>
2023-06-07 22:03:24 +0500

    user use effect

--- blog-app/blog-web/src/components/AuthGuard/index.tsx
+++ blog-app/blog-web/src/components/AuthGuard/index.tsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useBlogsHook } from "store/articles";
 
 interface AuthGuardProps {
   children: JSX.Element;
 }
 export default function AuthGuard({ children }: AuthGuardProps): JSX.Element {
     const [state, setState] = useState<boolean>(false);
-    
+    const auth = useBlogsHook();
+    useEffect(() => {
+        
+    }, [])
 }
