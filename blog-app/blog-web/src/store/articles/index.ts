0153fd42e6669d3bac41cbc7707e9b73a22594de
huzidev <huzaifa.iqdev@gmail.com>
2023-06-25 16:02:08 +0500

    app dispathc

--- blog-app/blog-web/src/store/articles/index.ts
+++ blog-app/blog-web/src/store/articles/index.ts
@@ -1,9 +1,10 @@
 import { useSelector } from 'react-redux';
+import { useAppDispatch } from 'store/hooks/hooks';
 import { BlogList } from './types';
 
 export const useBlogs = () => {
   const state = useSelector((state: any) => state.blogs) as BlogList;
-  const allBlogsState = useSelector((state: any) => state.blogs.allBlogs);
+  const dispatch = useAppDispatch();
 
   return {
     allBlogsState,
