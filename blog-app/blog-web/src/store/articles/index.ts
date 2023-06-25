20791bbb8617d94775f39829bbb8aa6a6d9bd1d7
huzidev <huzaifa.iqdev@gmail.com>
2023-06-25 16:03:18 +0500

    export ty[e

--- blog-app/blog-web/src/store/articles/index.ts
+++ blog-app/blog-web/src/store/articles/index.ts
@@ -1,12 +1,15 @@
 import { useSelector } from 'react-redux';
 import { useAppDispatch } from 'store/hooks/hooks';
+import * as actions from "./actions";
 import { BlogList } from './types';
 
 export const useBlogs = () => {
   const state = useSelector((state: any) => state.blogs) as BlogList;
   const dispatch = useAppDispatch();
+  const getBlogs = () => dispatch(actions.getBlogs());
 
   return {
-    allBlogsState,
+    state,
+    getBlogs
   }
 }
