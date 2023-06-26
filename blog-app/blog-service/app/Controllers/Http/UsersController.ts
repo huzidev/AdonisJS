52171c7ce683c7940139ac4d09efee8482659012
huzidev <huzaifa.iqdev@gmail.com>
2023-06-26 09:31:26 +0500

    adonis paths

--- blog-app/blog-service/app/Controllers/Http/UsersController.ts
+++ blog-app/blog-service/app/Controllers/Http/UsersController.ts
@@ -1,6 +1,7 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import User from 'App/Models/User';
 import { UserUpdate } from 'App/Validators/UserValidator';
 
 export default class UsersController {
@@ -10,16 +11,27 @@ export default class UsersController {
 
     public async update({ request, params, auth }: HttpContextContract) {
         try {
-            const userId = auth.user?.id;
+            const userId: number | undefined = auth.user?.id;
             let body;
             if (userId) {
                 body = await request.validate(UserUpdate);
             }
             if (!body) {
                 throw {
-                message: 'Bad request',
-                status: 400,
+                    message: 'Bad request',
+                    status: 400
                 }
+            }
+
+            const user = User.findBy("id", userId);
+            if (!user) {
+                throw {
+                    message: 'User not found',
+                    status: 404
+                }
+            } else {
+                await User.query().where("id", userId!).update(body);
+                return { message: 'User updated successfully', data: user }
             }
         } catch (e) {
             throw e
--- blog-app/blog-service/start/routes/user/index.ts
+++ blog-app/blog-service/start/routes/user/index.ts
@@ -4,5 +4,6 @@ export default function UsersRoutes() {
     const path = "UsersController";
     Route.group(() => {
         Route.get("/get/me", `${path}.getMe`).middleware("auth");
+        Route.put("/edit/me", `${path}.update`).middleware("auth");
     }).prefix('/user')
 }       
