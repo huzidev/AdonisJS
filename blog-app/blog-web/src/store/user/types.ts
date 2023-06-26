beec44763e13328329dbe452d144396c1b0c0e8f
huzidev <huzaifa.iqdev@gmail.com>
2023-06-26 09:46:02 +0500

    user payload

--- blog-app/blog-web/src/store/user/types.ts
+++ blog-app/blog-web/src/store/user/types.ts
@@ -1,4 +1,10 @@
-export interface UserUpdatePayload {
+import { SubState } from "store/types";
+
+export interface UserUpdatePayload extends SubState {
     id: number | null;
     username: string;    
+}
+
+export interface UserState {
+    updateUser: UserUpdatePayload;
 }
