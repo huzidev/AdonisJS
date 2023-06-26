028afe4fdcde09652109a2624f6d49e11e900069
huzidev <huzaifa.iqdev@gmail.com>
2023-06-26 10:15:47 +0500

    user hook

--- blog-app/blog-web/src/store/user/index.ts
+++ blog-app/blog-web/src/store/user/index.ts
@@ -3,13 +3,13 @@ import { useAppDispatch } from 'store/hooks/hooks';
 import * as actions from "./actions";
 import { UserState, UserUpdatePayload } from './types';
 
-export const useBlogs = () => {
+export const useUser = () => {
   const state = useSelector((state: any) => state.blogs) as UserState;
   const dispatch = useAppDispatch();
   const updateUser = (payload: UserUpdatePayload) => dispatch(actions.updateById(payload)); 
 
   return {
     state,
-    
+    updateUser
   }
 }
