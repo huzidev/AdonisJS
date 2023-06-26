a367cef260aac951c90f2eaa23d49040b9d282e3
huzidev <huzaifa.iqdev@gmail.com>
2023-06-26 09:50:07 +0500

    user type

---
+++ blog-app/blog-web/src/store/user/reducer.ts
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { subState } from "store/states";
+import * as actions from "./actions";
+import { UserState } from "./types";
+
+const initialState: UserState = {
+    updateUser: { ...subState }
+}
+
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers : {},
+    extraReducers: (builder) => {
+        // updateUser
+        builder.addCase(actions.updateById.pending, (state) => {
+            state.updateUser!.loading = true;
+            state.updateUser!.error = false;
+        })
+        builder.addCase(actions.updateById.fulfilled, (state) => {
+            state.updateUser!.loading = false;
+            state.updateUser!.error = false;
+        })
+        builder.addCase(actions.updateById.rejected, (state) => {
+            state.updateUser!.loading = false;
+            state.updateUser!.error = true;
+        })
+});
+
+export default userSlice.reducer;
