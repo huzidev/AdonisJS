6c21addbefb00f846536f3f2d515ee8519cae4e9
huzidev <huzaifa.iqdev@gmail.com>
2023-06-26 09:20:03 +0500

    username update validation

--- blog-app/blog-service/app/Validators/UserValidator.ts
+++ blog-app/blog-service/app/Validators/UserValidator.ts
@@ -2,9 +2,7 @@ import { CustomMessages, rules, schema } from '@ioc:Adonis/Core/Validator'
 
 export class UserUpdate {
   public schema = schema.create({
-    title: schema.string.optional({ trim: true }, [rules.minLength(6)]),
-    content: schema.string.optional({ trim: true }, [rules.minLength(12)]),
-    image: schema.string.optional()
+    username: schema.string.optional({ trim: true }, [rules.minLength(4)]),
   })
 
   public messages: CustomMessages = {
