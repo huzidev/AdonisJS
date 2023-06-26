65279ff1e76ffee83a0d95b4af4e368007818b5b
huzidev <huzaifa.iqdev@gmail.com>
2023-06-26 10:15:26 +0500

    redux dispatach

--- blog-app/blog-web/src/store/user/index.ts
+++ blog-app/blog-web/src/store/user/index.ts
@@ -1,11 +1,12 @@
 import { useSelector } from 'react-redux';
 import { useAppDispatch } from 'store/hooks/hooks';
-import { UserState } from './types';
+import * as actions from "./actions";
+import { UserState, UserUpdatePayload } from './types';
 
 export const useBlogs = () => {
   const state = useSelector((state: any) => state.blogs) as UserState;
   const dispatch = useAppDispatch();
-
+  const updateUser = (payload: UserUpdatePayload) => dispatch(actions.updateById(payload)); 
 
   return {
     state,
