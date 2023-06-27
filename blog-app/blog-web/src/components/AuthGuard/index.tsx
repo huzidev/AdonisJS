97c5f8cb17e1b9c54db7d064388266f687e8aca7
huzidev <huzaifa.iqdev@gmail.com>
2023-06-27 13:59:05 +0500

    Auth Guard Updated

--- blog-app/blog-web/src/components/AuthGuard/index.tsx
+++ blog-app/blog-web/src/components/AuthGuard/index.tsx
@@ -9,33 +9,32 @@ interface AuthGuardProps {
 }
 export default function AuthGuard({ children }: AuthGuardProps): JSX.Element {
     const [state, setState] = useState<boolean>(false);
-    const auth = useAuth();
     const Navigate = useNavigate();
+    const auth = useAuth();
     const currentPath = window.location.pathname;
 
     useEffect(() => {
       const { init } = auth.state;
       if (!init.init && !init.loading) {
         auth.initUser();
+        return;
       }
-      // if loggedIn User
-      if (auth.state.user && 
-        (currentPath === ROUTE_PATHS.AUTH_SIGNIN || currentPath === ROUTE_PATHS.AUTH_SIGNUP)
-        ) {
-        Navigate("/");
-      } else if (!auth.state.user && 
-        (currentPath === ROUTE_PATHS.VIEW_PROFILE || currentPath === ROUTE_PATHS.EDIT_PROFILE)
-        ) {
-        Navigate(ROUTE_PATHS.AUTH_SIGNIN);
+      if (!init.init) {
+        return;
       }
-      if (init.init) {
-        setState(true);
+      if (!auth.state.user) {
+        Navigate("/")
+      } else if (auth.state.user) {
+        if (currentPath === ROUTE_PATHS.AUTH_SIGNIN || currentPath === ROUTE_PATHS.AUTH_SIGNUP) {
+          Navigate("/");
+        } else {
+          setState(true)
+        }
       }
+      setState(true)
     }, [auth.state]);
-
     if (!state) {
       return <PageLoader />;
     }
-
     return children;
 }
