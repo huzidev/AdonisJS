ca4bc160ba88b2ee4a62524ad4eb1bea7f6e9ced
huzidev <huzaifa.iqdev@gmail.com>
2023-06-27 20:53:10 +0500

    join sequence

--- blog-app/blog-web/src/pages/user/ViewProfile/index.tsx
+++ blog-app/blog-web/src/pages/user/ViewProfile/index.tsx
@@ -1,19 +1,31 @@
+import ROUTE_PATHS from "Router/paths";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useBlogs } from "store/articles";
 import { useAuth } from "store/auth";
 import { UserDetailState } from "./types";
 
 export default function ViewProfilePage() {
   const auth = useAuth();
+  const blogs = useBlogs();
   const data = auth.state.user;
   const [userDetails, setUserDetails] = useState<UserDetailState>({ 
     username: "", email: "", createdAt: ""
   })
   const formatedDate = new Date(userDetails.createdAt).toLocaleString();
 
+  const userId = auth.state.user?.id;
+
   useEffect(() => {
     setUserDetails({...userDetails, ...data})
+    blogs.getBlogs()
+
   }, [])
+
+  let blogById = blogs.state.getBlogs?.data; 
+
+  console.log("Articles Uploaded By User is/are", blogById.filter((blogs) => blogs.ownerId === 1));
+  let idBlog = blogById.filter((blogs) => blogs.ownerId === userId)
 
   return (
     <div>
@@ -44,6 +56,58 @@ export default function ViewProfilePage() {
             </Link>
           </div>
         </div>
+        <div 
+    className="w-10/12 m-auto flex flex-wrap"
+  >
+    {
+      idBlog!.map((blog: any) => {
+          return (
+            <div 
+              key={blog.id}
+              className="w-[30.33%] mt-8 mx-4"
+            >
+              {/* <img src={ele.image} alt="Thumbnail" /> */}
+              <div className="h-52 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <img
+                  className="rounded-t-lg"
+                  src="/docs/images/blog/image-1.jpg"
+                  alt="Thumbnail"
+                />
+                <div className="p-5">
+                  <h5 title={blog.title} className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {blog.title.length > 21 ? `${blog.title.slice(0, 21)}...` : blog.title }
+                  </h5> 
+                  <p title={blog.content} className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {blog.content.length > 50 ? `${blog.content.slice(0, 50)}...` : blog.content}
+                  </p>
+                  <Link
+                    to={ROUTE_PATHS.ARTICLE_VIEW + blog.slug}
+                    className="text-sm font-medium text-center text-white hover:text-blue-500"
+                  > 
+                    Read More
+                  </Link>
+                    <div>
+                      <Link
+                        to={ROUTE_PATHS.ARTICLE_UPDATE + blog.slug}
+                        type="button"
+                        className="text-white bg-gray-800 font-medium text-sm py-2.5"
+                      >
+                        Edit
+                      </Link>
+                      <button
+                        type="button"
+                        className="text-white bg-gray-800 font-medium text-sm ml-4 py-2.5"
+                        onClick={() => blogs.deleteBlog(blog.id)}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+    </div>
     </div>
   )
 }
