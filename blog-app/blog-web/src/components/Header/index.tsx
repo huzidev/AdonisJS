16399ba93d8dc99f8a0dadae43b55a6d975004c2
huzidev <huzaifa.iqdev@gmail.com>
2023-06-27 20:26:15 +0500

    maping headers

--- blog-app/blog-web/src/components/Header/index.tsx
+++ blog-app/blog-web/src/components/Header/index.tsx
@@ -45,7 +45,6 @@ export default function Header() {
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               {links.map((data, dataIndex) =>
                 user && loggedInPaths.includes(data.link) ? (
-                  <>
                     <li key={dataIndex}>
                       <Link
                         to={data.link}
@@ -60,15 +59,6 @@ export default function Header() {
                         {data.title}
                       </Link>
                     </li>
-                    <li>
-                      <button
-                        onClick={() => auth.signOut()}
-                        className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                      >
-                        SingOut
-                      </button>
-                    </li>
-                    </>
                 ) : !user && loggedOutPaths.includes(data.link) ? (
                   <li key={dataIndex}>
                     <Link
@@ -86,7 +76,16 @@ export default function Header() {
                   </li>
                 ) : null
               )}
-              
+              {user && (
+                <li>
+                  <button
+                    onClick={() => auth.signOut()}
+                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  >
+                    SingOut
+                  </button>
+                </li>
+              )}
             </ul>
           </div>
         </div>
