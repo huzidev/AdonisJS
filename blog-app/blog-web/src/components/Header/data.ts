0bf934c5c4f4074fd05eda3afe34e977287064f5
huzidev <huzaifa.iqdev@gmail.com>
2023-06-27 18:35:03 +0500

    header map

---
+++ blog-app/blog-web/src/components/Header/data.ts
@@ -0,0 +1,23 @@
+import ROUTE_PATHS from "Router/paths";
+export const links = [
+  {
+    title: 'Blogs',
+    link: ROUTE_PATHS.ARTICLES,
+  },
+  {
+    title: 'Login',
+    link: ROUTE_PATHS.ARTICLES,
+  },
+  {
+    title: 'Register',
+    link: ROUTE_PATHS.ARTICLES,
+  },
+  {
+    title: 'Add Blog',
+    link: ROUTE_PATHS.ARTICLES,
+  },
+  {
+    title: 'View Profile',
+    link: ROUTE_PATHS.ARTICLES,
+  }
+];
