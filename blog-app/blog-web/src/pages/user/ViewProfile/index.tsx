8935b0aeae7895e47e269700e9bdac4e8a038b4d
huzidev <huzaifa.iqdev@gmail.com>
2023-06-28 23:12:07 +0500

    condition fixed

--- blog-app/blog-web/src/pages/user/ViewProfile/index.tsx
+++ blog-app/blog-web/src/pages/user/ViewProfile/index.tsx
@@ -142,7 +142,7 @@ export default function ViewProfilePage() {
                 </div>
               );
             })
-          : (
+          : !blogs.state.getBlogs?.loading && (
             <div className="w-full mt-5 py-8 pl-5 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
               <h1 className="text-lg mb-6 font-bold tracking-tight text-white">
                 Oops... You haven't uploaded any blog yet
