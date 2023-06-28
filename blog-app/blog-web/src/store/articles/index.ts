7af2a98de8bfe2547ce87c3b3bc4d0989c48244d
huzidev <huzaifa.iqdev@gmail.com>
2023-06-28 09:34:07 +0500

    checking blogs state

--- blog-app/blog-web/src/store/articles/index.ts
+++ blog-app/blog-web/src/store/articles/index.ts
@@ -1,7 +1,8 @@
 import { useSelector } from 'react-redux';
 import { useAppDispatch } from 'store/hooks/hooks';
 import * as actions from "./actions";
-import { AddBlogPayload, BlogState, GetBlogPayload, UpdateBlogPayload } from './types';
+import { blogSlice } from './reducer';
+import { AddBlogPayload, Blog, BlogState, GetBlogPayload, UpdateBlogPayload } from './types';
 
 export const useBlogs = () => {
   const state = useSelector((state: any) => state.blogs) as BlogState;
@@ -11,6 +12,7 @@ export const useBlogs = () => {
   const addBlog = (payload: AddBlogPayload) => dispatch(actions.addBlog(payload));
   const updateBlog = (payload: UpdateBlogPayload) => dispatch(actions.updateBlog(payload));
   const deleteBlog = (payload: number) => dispatch(actions.deleteBlog(payload));
+  const updateBlogsState = (payload: Blog) => dispatch(blogSlice.actions.updateBlogsState(payload));
 
   return {
     state,
@@ -18,6 +20,7 @@ export const useBlogs = () => {
     getBlog,
     addBlog,
     updateBlog,
-    deleteBlog
+    deleteBlog,
+    updateBlogsState
   }
 }
