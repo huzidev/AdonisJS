f7d865c2c5ae1fb43013054b8a1f570ec5a3d5bd
huzidev <huzaifa.iqdev@gmail.com>
2023-06-28 15:30:33 +0500

    users apartments

--- blog-app/blog-web/src/pages/user/ViewProfile/index.tsx
+++ blog-app/blog-web/src/pages/user/ViewProfile/index.tsx
@@ -12,111 +12,140 @@ export default function ViewProfilePage() {
   const user = useUser();
   const params: any = useParams();
   const data = auth.state.user;
-  const [userDetails, setUserDetails] = useState<UserDetailState>({ 
-    username: "", email: "", createdAt: ""
-  })
+
+  const [userDetails, setUserDetails] = useState<UserDetailState>({
+    username: "",
+    email: "",
+    createdAt: "",
+  });
   const formatedDate = new Date(userDetails.createdAt).toLocaleString();
   const isLoading = blogs.state.getBlogs?.loading;
   const userId = auth.state.user?.id;
 
+  const userDataById = user.state.getUser?.data;
+
+  console.log("userData ID", userDataById);
+
+  // useEffect(() => {
+  //   if (params.id !== "me") {
+  //     user.getById(params.id);
+  //     setUserDetails({ ...userDetails, ...userDataById });
+  //   } else {
+  //     blogs.getBlogs();
+  //     setUserDetails({ ...userDetails, ...data });
+  //   }
+  // }, []);
+
   useEffect(() => {
     if (params.id !== "me") {
-      user.getById(params.id)
+      user.getById(params.id);
+    } else {
+      blogs.getBlogs();
+      setUserDetails({ ...userDetails, ...data });
     }
-    blogs.getBlogs()
-    setUserDetails({...userDetails, ...data})
-  }, [])
+  }, [params.id]);
 
-  let blogById = blogs.state.getBlogs?.data; 
-  let userBlogs = blogById.filter((blogs) => blogs.ownerId === userId)
+  useEffect(() => {
+    if (params.id !== "me" && userDataById) {
+      setUserDetails({ ...userDetails, ...user.state.getUser.data });
+    }
+  }, [user.state.getUser]);
+
+  let blogById = blogs.state.getBlogs?.data;
+  let userBlogs = blogById.filter((blogs) => blogs.ownerId === userId);
 
   return (
     <div>
-      <div 
-          className="w-11/12 my-8 mx-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-5">
-            <h1 className="mb-4 text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">
-              User Profile
-            </h1>
-            <h2 className="mb-4 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Name : {userDetails.username}
-            </h2> 
-            <h2 className="mb-4 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Email : {userDetails.email}
-            </h2> 
-            <h2 className="mb-4 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Joined Date : {formatedDate}
-            </h2> 
-            <h2 className="mb-4 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Total Blogs : {userBlogs.length}
-            </h2> 
-            {
-              params.id === "me" && (
-                <Link
-                  to={ROUTE_PATHS.EDIT_PROFILE}
-                  type="button"
-                  className="text-white bg-gray-800 font-medium text-sm py-2.5"
-                >
-                  Edit
-                </Link>
-              )
-            }
-           
-          </div>
-        </div>
-        <div 
-    className="w-10/12 m-auto flex flex-wrap"
-  > 
-    {userBlogs.length ? (
-      userBlogs.map((blog: any) => {
-          return (
-            <div 
-              key={blog.id}
-              className="w-[30.33%] mt-8 mx-4"
+      <div className="w-11/12 my-8 mx-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className="p-5">
+          <h1 className="mb-4 text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">
+            User Profile
+          </h1>
+          <h2 className="mb-4 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Name : {userDetails.username}
+          </h2>
+          <h2 className="mb-4 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Email : {userDetails.email}
+          </h2>
+          <h2 className="mb-4 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Joined Date : {formatedDate}
+          </h2>
+          <h2 className="mb-4 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Total Blogs : {userBlogs.length}
+          </h2>
+          {params.id === "me" && (
+            <Link
+              to={ROUTE_PATHS.EDIT_PROFILE}
+              type="button"
+              className="text-white bg-gray-800 font-medium text-sm py-2.5"
             >
-              {/* <img src={ele.image} alt="Thumbnail" /> */}
-              <div className="h-52 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <img
-                  className="rounded-t-lg"
-                  src="/docs/images/blog/image-1.jpg"
-                  alt="Thumbnail"
-                />
-                <div className="p-5">
-                  <h5 title={blog.title} className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    {blog.title.length > 21 ? `${blog.title.slice(0, 21)}...` : blog.title }
-                  </h5> 
-                  <p title={blog.content} className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                    {blog.content.length > 50 ? `${blog.content.slice(0, 50)}...` : blog.content}
+              Edit
+            </Link>
+          )}
+        </div>
+      </div>
+      <div className="w-10/12 m-auto flex flex-wrap">
+        {userBlogs.length
+          ? userBlogs.map((blog: any) => {
+              return (
+                <div key={blog.id} className="w-[30.33%] mt-8 mx-4">
+                  <p>
                   </p>
-                  <Link
-                    to={ROUTE_PATHS.ARTICLE_VIEW + blog.slug}
-                    className="text-sm font-medium text-center text-white hover:text-blue-500"
-                  > 
-                    Read More
-                  </Link>
-                    <div>
+                  {/* <img src={ele.image} alt="Thumbnail" /> */}
+                  <div className="h-52 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <img
+                      className="rounded-t-lg"
+                      src="/docs/images/blog/image-1.jpg"
+                      alt="Thumbnail"
+                    />
+                    <div className="p-5">
+                      <h5
+                        title={blog.title}
+                        className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+                      >
+                        {blog.title.length > 21
+                          ? `${blog.title.slice(0, 21)}...`
+                          : blog.title}
+                      </h5>
+                      <p
+                        title={blog.content}
+                        className="mb-3 font-normal text-gray-700 dark:text-gray-400"
+                      >
+                        {blog.content.length > 50
+                          ? `${blog.content.slice(0, 50)}...`
+                          : blog.content}
+                      </p>
                       <Link
-                        to={ROUTE_PATHS.ARTICLE_UPDATE + blog.slug}
-                        type="button"
-                        className="text-white bg-gray-800 font-medium text-sm py-2.5"
+                        to={ROUTE_PATHS.ARTICLE_VIEW + blog.slug}
+                        className="text-sm font-medium text-center text-white hover:text-blue-500"
                       >
-                        Edit
+                        Read More
                       </Link>
-                      <button
-                        type="button"
-                        className="text-white bg-gray-800 font-medium text-sm ml-4 py-2.5"
-                        onClick={() => blogs.deleteBlog(blog.id)}
-                      >
-                        Delete
-                      </button>
+                      <div>
+                        <Link
+                          to={ROUTE_PATHS.ARTICLE_UPDATE + blog.slug}
+                          type="button"
+                          className="text-white bg-gray-800 font-medium text-sm py-2.5"
+                        >
+                          Edit
+                        </Link>
+                        <button
+                          type="button"
+                          className="text-white bg-gray-800 font-medium text-sm ml-4 py-2.5"
+                          onClick={() => blogs.deleteBlog(blog.id)}
+                        >
+                          Delete
+                        </button>
+                      </div>
                     </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          );
-        })) : !userBlogs.length && !isLoading ? "You Haven't uploaded Any Blog Yet" : ""
-        }
+              );
+            })
+          : !userBlogs.length && !isLoading
+          ? "You Haven't uploaded Any Blog Yet"
+          : ""}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
