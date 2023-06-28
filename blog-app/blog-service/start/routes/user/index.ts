44777842a0721674142c8aec6f9549c90c1d3dd9
huzidev <huzaifa.iqdev@gmail.com>
2023-06-28 11:04:07 +0500

    all users list

--- blog-app/blog-service/start/routes/user/index.ts
+++ blog-app/blog-service/start/routes/user/index.ts
@@ -5,6 +5,6 @@ export default function UsersRoutes() {
     Route.group(() => {
         Route.get("/get/me", `${path}.getMe`).middleware("auth");
         Route.put("/edit/me", `${path}.update`).middleware("auth");
-        Route.put("/all", `${path}.getAllUser`);
+        Route.get("/all", `${path}.getAllUser`);
     }).prefix('/user')
 }       
