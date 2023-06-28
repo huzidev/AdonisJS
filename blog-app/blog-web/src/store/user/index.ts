9068c22008efceeac6cd80404ae8f977d0ff6d13
huzidev <huzaifa.iqdev@gmail.com>
2023-06-28 11:12:50 +0500

    allUsers state

--- blog-app/blog-web/src/store/user/index.ts
+++ blog-app/blog-web/src/store/user/index.ts
@@ -8,11 +8,13 @@ import { UserState, UserUpdatePayload } from './types';
 export const useUser = () => {
   const state = useSelector((state: any) => state.user) as UserState;
   const dispatch = useAppDispatch();
+  const allUser = () => dispatch(actions.allUser())
   const updateUser = (payload: UserUpdatePayload) => dispatch(actions.updateById(payload)); 
   const updateUserState = (payload: User) => dispatch(userSlice.actions.updateUser(payload));
-
+  
   return {
     state,
+    allUser,
     updateUser,
     updateUserState
   }
