2447a5d701fbb3ef592195cdf539e58aa006cb2a
huzidev <huzaifa.iqdev@gmail.com>
2023-06-29 11:12:54 +0500

    pagination

--- blog-app/blog-web/src/store/types.ts
+++ blog-app/blog-web/src/store/types.ts
@@ -3,3 +3,11 @@ export interface SubState {
     error: boolean;
     message?: string | null;
 }
+
+export interface PaginationMeta {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  lastPage: number;
+  firstPage: number;
+}
