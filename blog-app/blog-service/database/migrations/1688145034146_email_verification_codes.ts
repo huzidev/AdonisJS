f6e1563d2f0f9e641c5f5766feb796d8e3d51a38
huzidev <huzaifa.iqdev@gmail.com>
2023-06-30 22:16:04 +0500

    migration type

--- blog-app/blog-service/database/migrations/1688145034146_email_verification_codes.ts
+++ blog-app/blog-service/database/migrations/1688145034146_email_verification_codes.ts
@@ -1,17 +1,26 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
-export default class extends BaseSchema {
+export default class EmailVerificationCodes extends BaseSchema {
   protected tableName = 'email_verification_codes'
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('code', 6).notNullable()
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .notNullable()
+      table.boolean('is_active').defaultTo(true).notNullable()
+      table.timestamp('expires_at', { useTz: true }).nullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
+      table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(this.now())
 
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.unique(['code', 'user_id'])
+      table.index(['code', 'user_id'])
     })
   }
 
