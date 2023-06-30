2a0a6921e509ecbaed4734797fb1f3625eaa261f
huzidev <huzaifa.iqdev@gmail.com>
2023-06-30 18:35:07 +0500

    Link updated

--- blog-app/blog-web/src/pages/auth/Form/index.tsx
+++ blog-app/blog-web/src/pages/auth/Form/index.tsx
@@ -1,18 +1,29 @@
-import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import { useState } from "react";
-import { useAuth } from 'store/auth';
+import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import ROUTE_PATHS from "Router/paths";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useAuth } from "store/auth";
 
 export default function UserFormPage(): JSX.Element {
-    const auth = useAuth();
+  const auth = useAuth();
+  const currentPath = window.location.pathname;
   const [isLogInForm, setIsLogInForm] = useState(true);
   const [userLogIn, setUserLogIn] = useState({ email: "", password: "" });
-  const [userSignUp, setUserSignUp] = useState({ 
+  const [userSignUp, setUserSignUp] = useState({
     username: "",
     email: "",
     password: "",
-    passwordConfirmation: "", });
-  const [value, SetValue] = useState<boolean>(false); 
+    passwordConfirmation: "",
+  });
+  const [value, SetValue] = useState<boolean>(false);
+
+  useEffect(() => {
+    const stateCondition = currentPath.includes("/sign_in") ? true : false;
+    setIsLogInForm(stateCondition);
+    console.log("state condition", stateCondition);
+    
+}, [currentPath]);
 
   function inputHandlerLogIn(
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -31,14 +42,17 @@ export default function UserFormPage(): JSX.Element {
     });
   }
 
-  const title = isLogInForm ? 'SignIn' : 'SignUp';
-  const descReverse = isLogInForm ? "Don't have an account ?" : 'Already have an account ?';
+  const title = isLogInForm ? "SignIn" : "SignUp";
+  const titleReverse = isLogInForm ? "SignUp" : "SignIn";
+  const descReverse = isLogInForm
+    ? "Don't have an account ?"
+    : "Already have an account ?";
 
   function submit() {
     if (isLogInForm) {
-        auth.signIn(userLogIn)
+      auth.signIn(userLogIn);
     } else {
-        auth.signIn(userSignUp)
+      auth.signIn(userSignUp);
     }
   }
   return (
@@ -54,69 +68,76 @@ export default function UserFormPage(): JSX.Element {
         </h2>
       </div>
       <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
-          <div className="mb-3">
+        <div className="mb-3">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Email address
+          </label>
+          <div className="mt-2">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              value={userLogIn.email}
+              required
+              onChange={inputHandlerLogIn}
+              className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+        <div className="mb-6">
+          <div className="flex items-center justify-between">
             <label
-              htmlFor="email"
+              htmlFor="password"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
-              Email address
+              Password
             </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                value={userLogIn.email}
-                required
-                onChange={inputHandlerLogIn}
-                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-          <div className="mb-6">
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Password
-              </label>
-              {/* <div className="text-sm">
+            {/* <div className="text-sm">
                         <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
                     </div> */}
-            </div>
-            <div className="mt-2 flex items-center">
-              <input
-                id="password"
-                name="password"
-                type={value ? "text" : "password"}
-                value={userLogIn.password}
-                onChange={inputHandlerLogIn}
-                required
-                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-              <span onClick={() => SetValue(!value)} title={value ? "Hide Password" : "Show Password"}>
-                {value ? <RemoveRedEyeOutlinedIcon fontSize='small'/> : <VisibilityOffIcon fontSize='small'/>}
-              </span>
-            </div>
           </div>
-          <div>
-            <button
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={submit}
+          <div className="mt-2 flex items-center">
+            <input
+              id="password"
+              name="password"
+              type={value ? "text" : "password"}
+              value={userLogIn.password}
+              onChange={inputHandlerLogIn}
+              required
+              className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+            <span
+              onClick={() => SetValue(!value)}
+              title={value ? "Hide Password" : "Show Password"}
             >
-              {title}
-            </button>
+              {value ? (
+                <RemoveRedEyeOutlinedIcon fontSize="small" />
+              ) : (
+                <VisibilityOffIcon fontSize="small" />
+              )}
+            </span>
           </div>
-        <p className="mt-3 text-center text-sm text-gray-500">
-          {descReverse}
+        </div>
+        <div>
           <button
-            onClick={() => setIsLogInForm((state) => !state)}
-            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={submit}
           >
-            {" "}
             {title}
           </button>
+        </div>
+        <p className="mt-3 text-center text-sm text-gray-500">
+          {descReverse}
+          <Link
+            to={isLogInForm ? ROUTE_PATHS.AUTH_SIGNUP : ROUTE_PATHS.AUTH_SIGNIN}
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            &nbsp;
+            {titleReverse}
+          </Link>
         </p>
       </div>
     </div>
