e615c054ba13c149bf50e19f4204d4ecc588fb86
huzidev <huzaifa.iqdev@gmail.com>
2023-07-01 16:47:23 +0500

    user auth middleware

--- blog-app/blog-service/start/routes/auth/index.ts
+++ blog-app/blog-service/start/routes/auth/index.ts
@@ -6,5 +6,9 @@ export default function AuthRoutes() {
         Route.post("/sign_up", `${path}.signUp`);
         Route.post("/sign_in", `${path}.signIn`);
         Route.post("/sign_out", `${path}.signOut`).middleware('auth:any');
+
+        // user can only access when user is not verified
+        Route.post("/verify_email/send_code", `${path}.verifyEmailSendCode`).middleware("auth:no_verify");
+        Route.post("/verify_email/verify_code", `${path}.verifyEmailVerifyCode`).middleware("auth:no_verify");
     }).prefix('/auth')
 }       
