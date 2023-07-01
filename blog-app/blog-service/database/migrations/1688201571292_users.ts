57f09c3d9898a9c97e966941a3e458657a6a202c
huzidev <huzaifa.iqdev@gmail.com>
2023-07-01 15:15:29 +0500

    schema migations

--- blog-app/blog-service/database/migrations/1688201571292_users.ts
+++ blog-app/blog-service/database/migrations/1688201571292_users.ts
@@ -1,18 +1,27 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 import User from 'App/Models/User'
 
+// ENUM means Validates the property to be one from the available choices
+
 export default class extends BaseSchema {
   protected tableName = 'users'
 
   public async up () {
     this.schema.alterTable(this.tableName, (table) => {
-      table.enum('role', User.roles).defaultTo(User.roles[0]).notNullable()
+      table.enum('role', User.roles).after("email").defaultTo(User.roles[0]).notNullable()
 
-   
+      table.boolean('is_active').defaultTo(true).notNullable()
+      table.boolean('is_banned').defaultTo(false).notNullable()
+      table.boolean('is_verified').defaultTo(false).notNullable()
     })
   }
 
   public async down () {
-    this.schema.dropTable(this.tableName)
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropColumn('role')
+      table.dropColumn('is_active')
+      table.dropColumn('is_banned')
+      table.dropColumn('is_verified')
+    })
   }
 }
