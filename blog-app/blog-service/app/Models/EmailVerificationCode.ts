38eff4290a5460c8fa9fc684d64ba1d9fb668714
huzidev <huzaifa.iqdev@gmail.com>
2023-07-01 12:23:27 +0500

    email verification

--- blog-app/blog-service/app/Models/EmailVerificationCode.ts
+++ blog-app/blog-service/app/Models/EmailVerificationCode.ts
@@ -1,5 +1,6 @@
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, BelongsTo, beforeCreate, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
 import { DateTime } from 'luxon'
+import User from './User'
 
 export default class EmailVerificationCode extends BaseModel {
   @column({ isPrimary: true })
@@ -14,9 +15,26 @@ export default class EmailVerificationCode extends BaseModel {
   @belongsTo(() => User)
   public user: BelongsTo<typeof User>
 
+  @column({ consume: (v) => !!v })
+  public isActive: boolean
+
+  @column.dateTime()
+  public expiresAt: DateTime
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @beforeCreate()
+  public static generateCode(model: EmailVerificationCode) {
+    model.generateCode(model)
+  }
+
+  public generateCode(model: EmailVerificationCode = this) {
+    model.code = random(101909, 929689)
+    model.isActive = true
+    model.expiresAt = DateTime.local().plus({ hours: 3 })
+  }
 }
