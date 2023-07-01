f374d701140e33c0e2c7021dc84c7d58e99a0dca
huzidev <huzaifa.iqdev@gmail.com>
2023-07-01 19:52:27 +0500

    custom rules

--- blog-app/blog-service/start/rules/verificationCode.ts
+++ blog-app/blog-service/start/rules/verificationCode.ts
@@ -1,27 +1,27 @@
-import { validator } from '@ioc:Adonis/Core/Validator'
+// import { validator } from '@ioc:Adonis/Core/Validator'
 
-// validator.rule is taking three parameters
-validator.rule("verificationCode", async (value, _, { pointer, arrayExpressionPointer, errorReporter }) => {
-    // verification code contains numbers only
-    const regex = /^[0-9]*$/ 
+// // validator.rule is taking three parameters
+// validator.rule("verificationCode", async (value, _, { pointer, arrayExpressionPointer, errorReporter }) => {
+//     // verification code contains numbers only
+//     const regex = /^[0-9]*$/ 
 
-    // if value contains other than numbers
-    if (!regex.test(value)) {
-      errorReporter.report(pointer, 'match', 'Invalid code', arrayExpressionPointer)
-      return
-    }
+//     // if value contains other than numbers
+//     if (!regex.test(value)) {
+//       errorReporter.report(pointer, 'match', 'Invalid code', arrayExpressionPointer)
+//       return
+//     }
 
-    // trimmed to trim extra whitespace
-    const trimmed = value.trim()
-    // if code length is not 6
-    if (trimmed.length !== 6) {
-      errorReporter.report(pointer, 'match', 'Invalid code', arrayExpressionPointer)
-    }
-  },
-  () => ({
-    // async is true because this rule will be used in await way like 
-    // const body = await request.validate(AuthVerifyEmailVerificationCode)
-    // verificationCode rule is created in AuthVerifyEmailVerificationCode
-    async: true,
-  })
-)
+//     // trimmed to trim extra whitespace
+//     const trimmed = value.trim()
+//     // if code length is not 6
+//     if (trimmed.length !== 6) {
+//       errorReporter.report(pointer, 'match', 'Invalid code', arrayExpressionPointer)
+//     }
+//   },
+//   () => ({
+//     // async is true because this rule will be used in await way like 
+//     // const body = await request.validate(AuthVerifyEmailVerificationCode)
+//     // verificationCode rule is created in AuthVerifyEmailVerificationCode
+//     async: true,
+//   })
+// )
---
+++ blog-app/blog-service/start/validator.ts
@@ -0,0 +1,37 @@
+/*
+|--------------------------------------------------------------------------
+| Preloaded File
+|--------------------------------------------------------------------------
+|
+| Any code written inside this file will be executed during the application
+| boot.
+|
+*/
+
+import { validator } from '@ioc:Adonis/Core/Validator'
+
+// validator.rule is taking three parameters
+validator.rule("verificationCode", async (value, _, { pointer, arrayExpressionPointer, errorReporter }) => {
+    // verification code contains numbers only
+    const regex = /^[0-9]*$/ 
+
+    // if value contains other than numbers
+    if (!regex.test(value)) {
+      errorReporter.report(pointer, 'match', 'Invalid code', arrayExpressionPointer)
+      return
+    }
+
+    // trimmed to trim extra whitespace
+    const trimmed = value.trim()
+    // if code length is not 6
+    if (trimmed.length !== 6) {
+      errorReporter.report(pointer, 'match', 'Invalid code', arrayExpressionPointer)
+    }
+  },
+  () => ({
+    // async is true because this rule will be used in await way like 
+    // const body = await request.validate(AuthVerifyEmailVerificationCode)
+    // verificationCode rule is created in AuthVerifyEmailVerificationCode
+    async: true,
+  })
+)
