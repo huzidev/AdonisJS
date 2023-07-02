526f6bebd36a1a7bf8abdec0fcb95215f8439496
huzidev <huzaifa.iqdev@gmail.com>
2023-07-02 11:50:22 +0500

    array from

--- blog-app/blog-web/src/pages/user/VerifyUser/index.tsx
+++ blog-app/blog-web/src/pages/user/VerifyUser/index.tsx
@@ -14,37 +14,20 @@ export default function VerifyUserPage(): JSX.Element {
               <p>We have sent a code to your email {auth.state.user?.email}</p>
             </div>
           </div>
-
           <div>
             <form action="" method="post">
               <div className="flex flex-col space-y-16">
                 <div className="flex flex-row items-center justify-between mx-auto w-full max-w-xs">
-                  <div className="w-16 h-16 ">
-                    <input
-                      className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
-                      type="text"
-                    />
-                  </div>
-                  <div className="w-16 h-16 ">
-                    <input
-                      className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
-                      type="text"
-                    />
-                  </div>
-                  <div className="w-16 h-16 ">
-                    <input
-                      className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
-                      type="text"
-                    />
-                  </div>
-                  <div className="w-16 h-16 ">
-                    <input
-                      className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
-                      type="text"
-                    />
-                  </div>
+                  {/* Array.from() to create an array of the length provided */}
+                  {Array.from({ length: 6 }, (_, index) => (
+                    <div className="w-16 h-16" key={index}>
+                      <input
+                        className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
+                        type="text"
+                      />
+                    </div>
+                  ))}
                 </div>
-
                 <div className="flex flex-col space-y-5">
                   <div>
                     <button className="flex flex-row items-center justify-center text-center w-full border rounded-xl outline-none py-5 bg-blue-700 border-none text-white text-sm shadow-sm">
