56a454f1a53240955dac624c82ca7b56688900fa
huzidev <huzaifa.iqdev@gmail.com>
2023-07-02 23:33:32 +0500

    user fetch data

--- blog-app/blog-web/src/pages/user/EditProfile/index.tsx
+++ blog-app/blog-web/src/pages/user/EditProfile/index.tsx
@@ -40,8 +40,8 @@ export default function EditProfilePage() {
   }, [params.id])
 
   useEffect(() => {
-    console.log("information", user.state.getUser.data?.username);
-}, [user.state.getUser.data]);
+      console.log("information", user.state.getUser.data?.username);
+  }, [user.state.getUser.data]);
 
   return (
     <div>
@@ -58,23 +58,47 @@ export default function EditProfilePage() {
         </div>
         <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
           <div className="mb-6">
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Username
-            </label>
-            <div className="mt-2">
-              <input
-                id="username"
-                name="username"
-                type="text"
-                value={updateDetails.username}
-                onChange={inputHandler}
-                required
-                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
+          {params.id === "me" ? (
+            <>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  value={updateDetails.username}
+                  onChange={inputHandler}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              </>
+          ) : (
+            <>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  value={user.state.getUser.data?.username}
+                  onChange={inputHandler}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              </>
+            )}
           </div>
           <div>
             <button
