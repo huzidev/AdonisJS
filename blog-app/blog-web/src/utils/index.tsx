731f0defc10cf131080ce148d00aded24b1fb5f6
huzidev <huzaifa.iqdev@gmail.com>
2023-07-02 21:29:47 +0500

    use ref

--- blog-app/blog-web/src/utils/index.tsx
+++ blog-app/blog-web/src/utils/index.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from "react";
 // T is called generic type means it'll automatically adapt whatever the type is given
 // when we called the usePrevious hook and gives the value it won't be necessary to define type as it will automatically adapts the type
 export function usePrevious<T>(value: T): T | null {
+  // useRef like document.getElementById or QuereySelector to store the value of field
   const ref = useRef<T>();
   useEffect(() => {
     ref.current = value;
