e7908e6ee67cdcf8c870c2778ad9fc431a65ba13
huzidev <huzaifa.iqdev@gmail.com>
2023-07-04 20:47:09 +0500

    button position fixed

--- blog-app/blog-web/src/pages/user/EditProfile/index.tsx
+++ blog-app/blog-web/src/pages/user/EditProfile/index.tsx
@@ -225,17 +225,18 @@ export default function EditProfilePage() {
                     {updateDetailsId.isVerified ? "Verified" : "Not Verified"}
                   </label>
                 </div>
+                 <div>
+                  <button
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    onClick={() => user.updateById({ ...updateDetailsId, id: params.id })}
+                  >
+                    Update Details
+                  </button>
+                </div>
               </>
             )}
           </div>
-          <div>
-            <button
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={() => user.updateById({ ...updateDetailsId, id: params.id })}
-            >
-              Update Details
-            </button>
-          </div>
+         
         </div>
       </div>
     </div>
