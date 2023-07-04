0493e61a2178d5fa33e781b73809f4a74a4ad02c
huzidev <huzaifa.iqdev@gmail.com>
2023-07-04 17:26:38 +0500

    onClick

--- blog-app/blog-web/src/pages/user/EditProfile/index.tsx
+++ blog-app/blog-web/src/pages/user/EditProfile/index.tsx
@@ -11,20 +11,22 @@ export default function EditProfilePage() {
   const user = useUser();
   const params = useParams();
   const userData = auth.state.user;
-  const [updateDetails, setUpdateDetails] = useState<UserDetailsEdit>({username: ""});
+  const [updateDetails, setUpdateDetails] = useState<UserDetailsEdit>({
+    username: "",
+  });
   const [isBoolean, setIsBoolean] = useState({
     isVerified: false,
     isBanned: false,
-    isActive: false
-  })
+    isActive: false,
+  });
   const prevUpdateState = usePrevious(user.state.updateMe);
   const updateState = user.state.updateMe;
   // const x = usePrevious<number>(65162);
 
   useEffect(() => {
-    setUpdateDetails({...updateDetails, ...userData})
-  }, [])
-  
+    setUpdateDetails({ ...updateDetails, ...userData });
+  }, []);
+
   function inputHandler(e: React.ChangeEvent<HTMLInputElement>) {
     setUpdateDetails({
       ...updateDetails,
@@ -33,16 +35,20 @@ export default function EditProfilePage() {
   }
 
   useEffect(() => {
-    if (prevUpdateState?.loading && !updateState?.loading && updateState?.data) {
-      user.updateUserState(updateState.data!)
+    if (
+      prevUpdateState?.loading &&
+      !updateState?.loading &&
+      updateState?.data
+    ) {
+      user.updateUserState(updateState.data!);
     }
   }, [updateState, prevUpdateState]);
 
   useEffect(() => {
     if (params.id !== "me") {
-      user.getById(Number(params.id))
+      user.getById(Number(params.id));
     }
-  }, [params.id])
+  }, [params.id]);
 
   let userName = user.state.getUser.data?.username;
   let isActive = user.state.getUser.data?.isActive;
@@ -50,13 +56,13 @@ export default function EditProfilePage() {
   let isVerified = user.state.getUser.data?.isVerified;
   let userRole = user.state.getUser.data?.role;
   useEffect(() => {
-      console.log("information", user.state.getUser.data);
-      setIsBoolean({
-        ...isBoolean, 
-        isActive: isActive ? true : false,
-        isBanned: isBanned ? true : false,
-        isVerified: isVerified ? true : false
-      })
+    console.log("information", user.state.getUser.data);
+    setIsBoolean({
+      ...isBoolean,
+      isActive: isActive ? true : false,
+      isBanned: isBanned ? true : false,
+      isVerified: isVerified ? true : false,
+    });
   }, [user.state.getUser.data]);
 
   return (
@@ -69,52 +75,59 @@ export default function EditProfilePage() {
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            {params.id === "me" ? "Edit Yours Details" : `Edit ${userName} Details`} 
+            {params.id === "me"
+              ? "Edit Yours Details"
+              : `Edit ${userName} Details`}
           </h2>
         </div>
         <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
           <div className="mb-6">
-          {params.id === "me" ? (
-            <>
-              <label
-                htmlFor="username"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Username
-              </label>
-              <div className="mt-2">
-                <input
-                  id="username"
-                  name="username"
-                  type="text"
-                  value={updateDetails.username}
-                  onChange={inputHandler}
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+            {params.id === "me" ? (
+              <>
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Username
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="username"
+                    name="username"
+                    type="text"
+                    value={updateDetails.username}
+                    onChange={inputHandler}
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
               </>
-          ) : (
-            // if admin clicked on user to edit info
-            <>
-              <label
-                htmlFor="username"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Username
-              </label>
-              <div className="mt-2">
-                <input
-                  id="username"
-                  name="username"
-                  type="text"
-                  value={userName}
-                  onChange={inputHandler}
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-              <label htmlFor="roles" className="block text-sm font-medium leading-6 my-2 text-gray-900">Roles</label>
+            ) : (
+              // if admin clicked on user to edit info
+              <>
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Username
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="username"
+                    name="username"
+                    type="text"
+                    value={userName}
+                    onChange={inputHandler}
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+                <label
+                  htmlFor="roles"
+                  className="block text-sm font-medium leading-6 my-2 text-gray-900"
+                >
+                  Roles
+                </label>
                 <select
                   id="roles"
                   className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -125,37 +138,73 @@ export default function EditProfilePage() {
                     </option>
                   ))}
                 </select>
-              <div className="flex items-center my-4">
-                <input id="checkbox" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray- dark:border-gray-600" 
-                checked={isBoolean.isActive ? true : false}
-                />
-                <label htmlFor="checkbox" className="ml-2 text-sm font-medium">
-                  {isActive ? "Active" : "Not Active"}
-                </label>
-              </div>
-              <div className="flex items-center mb-4">
-                <input id="checkbox" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray- dark:border-gray-600"
-                checked={isBoolean.isBanned ? true : false}
-                />
-                <label htmlFor="checkbox" className="ml-2 text-sm font-medium">
-                  {isBanned ? "Banned" : "Not Banned"}
-                </label>
-              </div>
-              <div className="flex items-center">
-                <input id="checkbox" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray- dark:border-gray-600" 
-                checked={isBoolean.isVerified ? true : false}
-                />
-                <label htmlFor="checkbox" className="ml-2 text-sm font-medium">
-                  {isVerified ? "Verified" : "Not Verified"}
-                </label>
-              </div>
+                <div className="flex items-center my-4">
+                  <input
+                    id="checkbox"
+                    type="checkbox"
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray- dark:border-gray-600"
+                    checked={isBoolean.isActive ? true : false}
+                    onClick={() =>
+                      setIsBoolean({
+                        ...isBoolean,
+                        isBanned: !isBoolean.isBanned,
+                      })
+                    }
+                  />
+                  <label
+                    htmlFor="checkbox"
+                    className="ml-2 text-sm font-medium"
+                  >
+                    {isActive ? "Active" : "Not Active"}
+                  </label>
+                </div>
+                <div className="flex items-center mb-4">
+                  <input
+                    id="checkbox"
+                    type="checkbox"
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray- dark:border-gray-600"
+                    checked={isBoolean.isBanned ? true : false}
+                    onClick={() =>
+                      setIsBoolean({
+                        ...isBoolean,
+                        isBanned: !isBoolean.isBanned,
+                      })
+                    }
+                  />
+                  <label
+                    htmlFor="checkbox"
+                    className="ml-2 text-sm font-medium"
+                  >
+                    {isBanned ? "Banned" : "Not Banned"}
+                  </label>
+                </div>
+                <div className="flex items-center">
+                  <input
+                    id="checkbox"
+                    type="checkbox"
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray- dark:border-gray-600"
+                    checked={isBoolean.isVerified ? true : false}
+                    onClick={() =>
+                      setIsBoolean({
+                        ...isBoolean,
+                        isBanned: !isBoolean.isBanned,
+                      })
+                    }
+                  />
+                  <label
+                    htmlFor="checkbox"
+                    className="ml-2 text-sm font-medium"
+                  >
+                    {isVerified ? "Verified" : "Not Verified"}
+                  </label>
+                </div>
               </>
             )}
           </div>
           <div>
             <button
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={() => user.updateUser({...updateDetails})}
+              onClick={() => user.updateUser({ ...updateDetails })}
             >
               Update Details
             </button>
@@ -163,5 +212,5 @@ export default function EditProfilePage() {
         </div>
       </div>
     </div>
-  )
+  );
 }
