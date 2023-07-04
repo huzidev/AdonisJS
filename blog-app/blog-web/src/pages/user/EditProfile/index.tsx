2586d6350682e996e9a611ce03d7c2cce46dda9b
huzidev <huzaifa.iqdev@gmail.com>
2023-07-04 23:33:51 +0500

    on select option fix

--- blog-app/blog-web/src/pages/user/EditProfile/index.tsx
+++ blog-app/blog-web/src/pages/user/EditProfile/index.tsx
@@ -76,7 +76,7 @@ export default function EditProfilePage() {
   console.log("user info", updateDetailsId);
   
 
-   function inputCheckHandler(e: React.ChangeEvent<HTMLInputElement>) {
+   function inputCheckHandler(e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) {
     setUpdateDetailsId({
       ...updateDetailsId,
       [e.target.name]: e.target.value,
@@ -154,8 +154,9 @@ export default function EditProfilePage() {
                 </label>
                 <select
                   id="roles"
-                  value={updateDetailsId.role} // Add this line
+                  value={updateDetailsId.role}
                   className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={inputCheckHandler}
                 >
                   {roles.map((role, roleIndex) => (
                     <option key={roleIndex} value={role}>
