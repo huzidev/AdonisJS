956632b3748e10f6fc696279bba0a26278a5b990
huzidev <huzaifa.iqdev@gmail.com>
2023-07-04 17:06:42 +0500

    user state value

--- blog-app/blog-web/src/pages/user/EditProfile/index.tsx
+++ blog-app/blog-web/src/pages/user/EditProfile/index.tsx
@@ -118,13 +118,15 @@ export default function EditProfilePage() {
                 </select>
               <div className="flex items-center my-4">
                 <input id="checkbox" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray- dark:border-gray-600" 
+                defaultChecked={isActive ? true : false}
                 />
                 <label htmlFor="checkbox" className="ml-2 text-sm font-medium">
                   {isActive ? "Active" : "Not Active"}
                 </label>
               </div>
               <div className="flex items-center mb-4">
-                <input id="checkbox" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray- dark:border-gray-600" 
+                <input id="checkbox" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray- dark:border-gray-600"
+                defaultChecked={isBanned ? true : false}
                 />
                 <label htmlFor="checkbox" className="ml-2 text-sm font-medium">
                   {isBanned ? "Banned" : "Not Banned"}
@@ -132,6 +134,7 @@ export default function EditProfilePage() {
               </div>
               <div className="flex items-center">
                 <input id="checkbox" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray- dark:border-gray-600" 
+                defaultChecked={isVerified ? true : false}
                 />
                 <label htmlFor="checkbox" className="ml-2 text-sm font-medium">
                   {isVerified ? "Verified" : "Not Verified"}
