3ea9aa4321197c1385624781c3a59ec073278ff8
huzidev <huzaifa.iqdev@gmail.com>
2023-07-04 12:43:23 +0500

    user role fied

--- blog-app/blog-web/src/pages/user/EditProfile/index.tsx
+++ blog-app/blog-web/src/pages/user/EditProfile/index.tsx
@@ -107,8 +107,9 @@ export default function EditProfilePage() {
               </div>
               <label htmlFor="roles" className="block text-sm font-medium leading-6 my-2 text-gray-900">Roles</label>
                 <select id="roles" className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                  <option selected>{userRole}</option>
 s                  {roles.map((role, roleIndex) => (
-                    <option key={roleIndex} selected>{role}</option>
+                    <option key={roleIndex}>{role}</option>
                   ))}
                 </select>
               <div className="flex items-center my-4">
