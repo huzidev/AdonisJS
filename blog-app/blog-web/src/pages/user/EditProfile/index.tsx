8403a8f18b390b463b0840f19c4eb4a9ec9cb644
huzidev <huzaifa.iqdev@gmail.com>
2023-07-04 12:06:31 +0500

    margin updated

--- blog-app/blog-web/src/pages/user/EditProfile/index.tsx
+++ blog-app/blog-web/src/pages/user/EditProfile/index.tsx
@@ -105,7 +105,7 @@ export default function EditProfilePage() {
                   className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-              <label htmlFor="roles" className="block text-sm font-medium leading-6 text-gray-900">Roles</label>
+              <label htmlFor="roles" className="block text-sm font-medium leading-6 my-2 text-gray-900">Roles</label>
                 <select id="roles" className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
 s                  {roles.map((role, roleIndex) => (
                     <option key={roleIndex} selected>{role}</option>
