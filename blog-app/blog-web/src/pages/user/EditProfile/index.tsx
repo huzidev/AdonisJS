67533d0d2b7c01f756b43fb449193e22747984f1
huzidev <huzaifa.iqdev@gmail.com>
2023-07-04 11:42:33 +0500

    user state manage

--- blog-app/blog-web/src/pages/user/EditProfile/index.tsx
+++ blog-app/blog-web/src/pages/user/EditProfile/index.tsx
@@ -43,6 +43,11 @@ export default function EditProfilePage() {
       console.log("information", user.state.getUser.data?.username);
   }, [user.state.getUser.data]);
 
+  let userName = user.state.getUser.data?.username;
+  let isActive = user.state.getUser.data?.isActive;
+  let isBanned = user.state.getUser.data?.isBanned;
+  let isVerified = user.state.getUser.data?.isVerified;
+
   return (
     <div>
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
@@ -53,7 +58,7 @@ export default function EditProfilePage() {
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            {params.id === "me" ? "Edit Yours Details" : `Edit ${user.state.getUser.data?.username} Details`} 
+            {params.id === "me" ? "Edit Yours Details" : `Edit ${userName} Details`} 
           </h2>
         </div>
         <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -92,11 +97,26 @@ export default function EditProfilePage() {
                   id="username"
                   name="username"
                   type="text"
-                  value={user.state.getUser.data?.username}
+                  value={userName}
                   onChange={inputHandler}
                   required
                   className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+              </div>
+              <div className="flex items-center my-4">
+                <input id="checkbox" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray- dark:border-gray-600" 
+                />
+                <label htmlFor="checkbox" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"></label>
+              </div>
+              <div className="flex items-center mb-4">
+                <input id="checkbox" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray- dark:border-gray-600" 
+                />
+                <label htmlFor="checkbox" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Sign Up As Blogger</label>
+              </div>
+              <div className="flex items-center">
+                <input id="checkbox" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray- dark:border-gray-600" 
+                />
+                <label htmlFor="checkbox" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Sign Up As Blogger</label>
               </div>
               </>
             )}
