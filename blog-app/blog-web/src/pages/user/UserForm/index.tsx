92c69ae978fc31cd46aad7b12482ad709a45310d
huzidev <huzaifa.iqdev@gmail.com>
2023-07-05 20:58:14 +0500

    user create page

--- blog-app/blog-web/src/pages/user/UserForm/index.tsx
+++ blog-app/blog-web/src/pages/user/UserForm/index.tsx
@@ -124,6 +124,108 @@ export default function UserFormPage() {
                     className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
+                <label
+                  htmlFor="roles"
+                  className="block text-sm font-medium leading-6 my-2 text-gray-900"
+                >
+                  Roles
+                </label>
+                <select
+                  id="roles"
+                  name="role" // MANDATORY to use name otherwise the inputHandler won't work
+                  value={updateDetailsId.role}
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={inputHandler}
+                >
+                  {roles.map((role, roleIndex) =>
+                    // if loggedIn user is admin then admin can't update user to super-admin
+                    updateDetailsId.role === "admin" &&
+                    role === "super-admin" ? null : (
+                      <option key={roleIndex} value={role}>
+                        {role}
+                      </option>
+                    )
+                  )}
+                </select>
+                <div className="flex items-center my-4">
+                  <input
+                    id="checkbox"
+                    type="checkbox"
+                    name="isActive"
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray- dark:border-gray-600"
+                    checked={updateDetailsId.isActive ? true : false}
+                    onClick={() =>
+                      setUpdateDetailsId({
+                        ...updateDetailsId,
+                        isActive: !updateDetailsId.isActive,
+                      })
+                    }
+                    onChange={inputHandler}
+                  />
+                  <label
+                    htmlFor="checkbox"
+                    className="ml-2 text-sm font-medium"
+                  >
+                    {updateDetailsId.isActive ? "Active" : "Not Active"}
+                  </label>
+                </div>
+                <div className="flex items-center mb-4">
+                  <input
+                    id="checkbox"
+                    type="checkbox"
+                    name="isBanned"
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray- dark:border-gray-600"
+                    checked={updateDetailsId.isBanned ? true : false}
+                    onChange={inputHandler}
+                    onClick={() =>
+                      setUpdateDetailsId({
+                        ...updateDetailsId,
+                        isBanned: !updateDetailsId.isBanned,
+                      })
+                    }
+                  />
+                  <label
+                    htmlFor="checkbox"
+                    className="ml-2 text-sm font-medium"
+                  >
+                    {updateDetailsId.isBanned ? "Banned" : "Not Banned"}
+                  </label>
+                </div>
+                <div className="flex items-center">
+                  <input
+                    id="checkbox"
+                    type="checkbox"
+                    name="isVerified"
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray- dark:border-gray-600"
+                    checked={updateDetailsId.isVerified ? true : false}
+                    onChange={inputHandler}
+                    onClick={() =>
+                      setUpdateDetailsId({
+                        ...updateDetailsId,
+                        isVerified: !updateDetailsId.isVerified,
+                      })
+                    }
+                  />
+                  <label
+                    htmlFor="checkbox"
+                    className="ml-2 text-sm font-medium"
+                  >
+                    {updateDetailsId.isVerified ? "Verified" : "Not Verified"}
+                  </label>
+                </div>
+                <div>
+                  <button
+                    className="flex mt-6 w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    onClick={() =>
+                      user.updateById({
+                        ...updateDetailsId,
+                        id: Number(params.id),
+                      })
+                    }
+                  >
+                    Create User
+                  </button>
+                </div>
               </>
             ) : params.id === "me" ? (
               <>
