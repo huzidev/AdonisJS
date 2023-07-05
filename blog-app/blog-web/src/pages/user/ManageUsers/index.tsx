a5f095364955345b19cab9a3c204053dea948796
huzidev <huzaifa.iqdev@gmail.com>
2023-07-05 11:00:52 +0500

    nbsp

--- blog-app/blog-web/src/pages/user/ManageUsers/index.tsx
+++ blog-app/blog-web/src/pages/user/ManageUsers/index.tsx
@@ -106,8 +106,7 @@ export default function UsersPage() {
         </div>
         <div className="flex flex-col items-center border-t bg-white px-5 py-5 sm:flex-row sm:justify-between">
           <span className="text-xs text-gray-600 sm:text-sm">
-            {" "}
-            Showing 1 to 5 of 12 Entries{" "}
+            Showing 1 to 5 of 12 Entries
           </span>
           <div className="mt-2 inline-flex sm:mt-0">
             <button className="mr-2 h-12 w-12 rounded-full border text-sm font-semibold text-gray-600 transition duration-150 hover:bg-gray-100">
