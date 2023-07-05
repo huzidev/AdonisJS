edd78fb571be49a19beaa96bc11a497f1adc69fe
huzidev <huzaifa.iqdev@gmail.com>
2023-07-05 14:04:32 +0500

    user current role

--- blog-app/blog-web/src/pages/user/EditProfile/index.tsx
+++ blog-app/blog-web/src/pages/user/EditProfile/index.tsx
@@ -151,11 +151,22 @@ export default function EditProfilePage() {
                   className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   onChange={inputHandler}
                 >
-                  {roles.map((role, roleIndex) => (
+                  {/* {roles.map((role, roleIndex) => (
                     <option key={roleIndex} value={role}>
                       {role}
                     </option>
-                  ))}
+                  ))} */}
+                  {roles.map((role, roleIndex) => {
+                    if (updateDetailsId.role === "admin" && role === "super-admin") {
+                      // Skip rendering the option if user's role is admin and current option is super-admin
+                      return null;
+                    }
+                    return (
+                      <option key={roleIndex} value={role}>
+                        {role}
+                      </option>
+                    );
+                  })}
                 </select>
                 <div className="flex items-center my-4">
                   <input
