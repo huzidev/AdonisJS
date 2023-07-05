f8c3f048474c82269ed22cae6567b4b13c3a8c84
huzidev <huzaifa.iqdev@gmail.com>
2023-07-05 13:30:25 +0500

    user input handler

--- blog-app/blog-web/src/pages/user/EditProfile/index.tsx
+++ blog-app/blog-web/src/pages/user/EditProfile/index.tsx
@@ -146,6 +146,7 @@ export default function EditProfilePage() {
                 </label>
                 <select
                   id="roles"
+                  name="role" // MANDATORY to use name otherwise the inputHandler won't work
                   value={updateDetailsId.role}
                   className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   onChange={inputHandler}
