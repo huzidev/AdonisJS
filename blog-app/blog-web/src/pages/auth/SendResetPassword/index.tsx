5f99d54355f3131b91cd3523c20baab449567230
huzidev <huzaifa.iqdev@gmail.com>
2023-07-07 10:15:17 +0500

    reset password code added

--- blog-app/blog-web/src/pages/auth/SendResetPassword/index.tsx
+++ blog-app/blog-web/src/pages/auth/SendResetPassword/index.tsx
@@ -17,11 +17,10 @@ export default function SendResetPasswordPage(): JSX.Element {
           alt="Your Company"
         />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign in to your account
+          Reset Yours Password
         </h2>
       </div>
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
           <div>
             <label
               htmlFor="email"
@@ -47,13 +46,13 @@ export default function SendResetPasswordPage(): JSX.Element {
           <div>
             <button
               type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 my-6 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={() => reset.sendResetPasswordCode(email)}
             >
-              Sign in
+                Send Code
             </button>
           </div>
-        </form>
-        <p className="mt-5 text-center text-sm text-gray-500">
+        <p className="text-center text-sm text-gray-500">
           Remembered your account ?
           <Link
             to={ROUTE_PATHS.AUTH_SIGNIN}
