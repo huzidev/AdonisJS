efb8107fa3ff8de71c2102a5845655ab01d490a3
huzidev <huzaifa.iqdev@gmail.com>
2023-07-07 09:47:36 +0500

    types

--- blog-app/blog-web/src/store/resetPassword/actions.ts
+++ blog-app/blog-web/src/store/resetPassword/actions.ts
@@ -1,7 +1,19 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import api from "services/api";
 import * as endpoints from "./endpoints";
+import { ResetPasswordSendCodeRequest } from "./types";
 
 
-export const sendResetPasswordCode = createAsyncThunk(endpoints.SEND_CODE, async () => {
-    
+export const sendResetPasswordCode = createAsyncThunk(endpoints.SEND_CODE, async (data: ResetPasswordSendCodeRequest) => {
+    try {
+        const response = await api.post(endpoints.SEND_CODE, data);
+        if (response.status === 200) {
+            alert("Code send successfully!")
+        }
+        console.log("response for reset password code");
+        return response.data.data;
+    } catch (e) {
+        console.log("Error", e);
+        
+    }
 })
--- blog-app/blog-web/src/store/resetPassword/types.ts
+++ blog-app/blog-web/src/store/resetPassword/types.ts
@@ -1,3 +1,11 @@
+import { SubState } from "store/types";
+
 export interface ResetPasswordSendCodeRequest {
   email: string; // only email is required for reset password code
+}
+
+export interface ResetPasswordState {
+  sendState: SubState;
+  resetState: SubState;
+ 
 }
