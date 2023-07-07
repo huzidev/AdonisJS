2dc374977284331487ea4167a84bdeb82f4c5701
huzidev <huzaifa.iqdev@gmail.com>
2023-07-07 10:07:36 +0500

    onChanage event

--- blog-app/blog-web/src/pages/auth/SendResetPassword/index.tsx
+++ blog-app/blog-web/src/pages/auth/SendResetPassword/index.tsx
@@ -5,7 +5,7 @@ import { useResetPassword } from "store/resetPassword";
 import { sendResetCode } from "./types";
 
 export default function SendResetPasswordPage(): JSX.Element {
-  const [email, setEmail] = useState<sendResetCode>("");
+  const [email, setEmail] = useState<sendResetCode>({ email: "" });
   const reset = useResetPassword();
 
   return (
@@ -34,6 +34,10 @@ export default function SendResetPasswordPage(): JSX.Element {
                 id="email"
                 name="email"
                 type="email"
+                value={email.email}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setEmail({ ...email, email: e.target.value })
+                }
                 autoComplete="email"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
