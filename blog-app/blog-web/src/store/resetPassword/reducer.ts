a75caa4e25822735682ab8b0bb0704d34ae6fa96
huzidev <huzaifa.iqdev@gmail.com>
2023-07-07 09:51:20 +0500

    reducers created

--- blog-app/blog-web/src/store/resetPassword/reducer.ts
+++ blog-app/blog-web/src/store/resetPassword/reducer.ts
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { subState } from "store/states";
+import * as actions from "./actions";
 import { ResetPasswordState } from "./types";
 
 const initialState: ResetPasswordState = {
@@ -12,7 +13,15 @@ export const resetSlice = createSlice({
     initialState,
     reducers: {},
     extraReducers: (builder) => {
-        
+        builder.addCase(actions.sendResetPasswordCode.pending, (state) => {
+            state.sendState = {loading: true, error: false};
+        })
+        builder.addCase(actions.sendResetPasswordCode.fulfilled, (state) => {
+            state.sendState = {loading: false, error: false};
+        })
+        builder.addCase(actions.sendResetPasswordCode.rejected, (state) => {
+            state.sendState = {loading: false, error: true};
+        })
     }
 })
 
