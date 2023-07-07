204126a5d5740933274446fb1b9b8cc19ce3f73b
huzidev <huzaifa.iqdev@gmail.com>
2023-07-07 09:55:29 +0500

    reset password code

--- blog-app/blog-web/src/store/resetPassword/index.ts
+++ blog-app/blog-web/src/store/resetPassword/index.ts
@@ -1,14 +1,15 @@
 import { useSelector } from 'react-redux';
-import { ResetPasswordState } from './types';
+import { useAppDispatch } from 'store/hooks/hooks';
+import * as actions from './actions';
+import { ResetPasswordSendCodeRequest, ResetPasswordState } from './types';
 
 export function useResetPassword() {
   const state = useSelector((state: any) => state.reset) as ResetPasswordState;
- 
-  // const updateuser = (payload: User) => dispatch(authSlice.actions.updateUser(payload));
-
+  const dispatch = useAppDispatch();
+  const sendResetPasswordCode = (payload: ResetPasswordSendCodeRequest) => dispatch(actions.sendResetPasswordCode(payload));
 
   return {
     state,
-  
+    sendResetPasswordCode
   };
 };
