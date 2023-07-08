638121ad4db27614096c4c50f42b7969bdfea0ea
huzidev <huzaifa.iqdev@gmail.com>
2023-07-08 13:04:41 +0500

    add favortie blogs

--- blog-app/blog-service/app/Controllers/Http/FavoriteArticlesController.ts
+++ blog-app/blog-service/app/Controllers/Http/FavoriteArticlesController.ts
@@ -1,10 +1,12 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import FavoriteArticle from 'App/Models/FavoriteArticle';
 import { AddFavoriteArticle } from 'App/Validators/FavoriteArticleValidator';
 
 export default class FavoriteArticlesController {
     public async add({ request }: HttpContextContract) {
         try {
             const body = await request.validate(AddFavoriteArticle);
+            await FavoriteArticle.create({...body})
         } catch (e) {
             throw e
         }
