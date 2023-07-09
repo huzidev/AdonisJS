e8c806e1bf760f414390649774ca5aea6262c36a
huzidev <huzaifa.iqdev@gmail.com>
2023-07-09 22:13:42 +0500

    custom hook

--- blog-app/blog-web/src/store/emailVerification/index.ts
+++ blog-app/blog-web/src/store/emailVerification/index.ts
@@ -1,11 +1,14 @@
 import { useSelector } from 'react-redux';
 import { useAppDispatch } from 'store/hooks/hooks';
+import * as actions from './actions';
 import { EmailVerificationState } from './types';
 
+
 export function useEmailVerification() {
-  const state = useSelector((state: any) => state) as EmailVerificationState;
+  const state = useSelector((state: any) => state.emailVerification) as EmailVerificationState;
   const dispatch = useAppDispatch();
-  
+  const sendCode = () => dispatch(actions.sendVerificationCode());
+  const verifyCode = (payload: AuthVerificationPayload) => dispatch(actions.verifyVerificationCode(payload));
 
   return {
     state,
