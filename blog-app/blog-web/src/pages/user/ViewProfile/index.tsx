a87a17b702a2cd934d115949f678ee8cf4201728
huzidev <huzaifa.iqdev@gmail.com>
2023-07-10 14:25:51 +0500

    fxing fetch data

--- blog-app/blog-web/src/pages/user/ViewProfile/index.tsx
+++ blog-app/blog-web/src/pages/user/ViewProfile/index.tsx
@@ -50,7 +50,7 @@ export default function ViewProfilePage() {
   
   useEffect(() => {
       blogs.getFavoriteBlogs(payload)
-  }, [auth.state.user?.role])
+  }, [auth.state.user?.role, params.id])
 
   // to store the data
   useEffect(() => {
@@ -117,11 +117,10 @@ export default function ViewProfilePage() {
           </h1>
         </div>
         <div className="w-11/12 mx-auto flex flex-wrap">
-          {userBlogs.length 
+          {userBlogs.length
             ? userBlogs.map((blog: any) => {
                 return (
                   <div key={blog.id} className="w-[30.33%] mt-8 mx-4">
-                    <p></p>
                     {/* <img src={ele.image} alt="Thumbnail" /> */}
                     <div className="h-52 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                       <img
@@ -174,9 +173,47 @@ export default function ViewProfilePage() {
                     </div>
                   </div>
                 );
-              })
-            : // so when user reload the page this won't be shown for the time page is reloading
-              !blogs.state.getBlogs?.loading && (
+              }) : (favoriteBlogs.data && favoriteBlogs.data.map((blog: any) => {
+                return (
+                  <div key={blog.id} className="w-[30.33%] mt-8 mx-4">
+                    {/* <img src={ele.image} alt="Thumbnail" /> */}
+                    <div className="h-52 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                      <img
+                        className="rounded-t-lg"
+                        src="/docs/images/blog/image-1.jpg"
+                        alt="Thumbnail"
+                      />
+                      <div className="p-5">
+                        <h5
+                          title={blog.title}
+                          className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+                        >
+                          {blog.title.length > 21
+                            ? `${blog.title.slice(0, 21)}...`
+                            : blog.title}
+                        </h5>
+                        <p
+                          title={blog.content}
+                          className="mb-3 font-normal text-gray-700 dark:text-gray-400"
+                        >
+                          {blog.content.length > 50
+                            ? `${blog.content.slice(0, 50)}...`
+                            : blog.content}
+                        </p>
+                        <Link
+                          to={ROUTE_PATHS.ARTICLE_VIEW + blog.slug}
+                          className="text-sm font-medium text-center text-white hover:text-blue-500"
+                        >
+                          Read More
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                )
+              }))
+              }
+             {/* so when user reload the page this won't be shown for the time page is reloading */}
+              {!blogs.state.getBlogs?.loading && (!userBlogs.length || !favoriteBlogs.data.length) && (
                 <div className="w-full mt-5 py-8 pl-5 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                   <h1 className="text-lg mb-6 font-bold tracking-tight text-white">
                     Oops...
@@ -204,9 +241,6 @@ export default function ViewProfilePage() {
                 </div>
               )}
         </div>
-        <button onClick={() => blogs.getFavoriteBlogs(payload)}>
-          Get fav blogs
-        </button>
       </div>
       <div className="w-11/12 m-auto mt-5">
         {currentPage !== lastPage && (
