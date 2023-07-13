9e37e117d7f6027eebb138ff06cf87c391076bdc
huzidev <huzaifa.iqdev@gmail.com>
2023-07-13 09:50:13 +0500

    reset password

--- blog-app/blog-web/src/pages/auth/ResetPassword/index.tsx
+++ blog-app/blog-web/src/pages/auth/ResetPassword/index.tsx
@@ -1,43 +1,43 @@
-import qs from 'query-string';
+import ROUTE_PATHS from "Router/paths";
+import qs from "query-string";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { useResetPassword } from "store/resetPassword";
 import { ResetPasswordCode } from "./types";
 
 export default function ResetPasswordPage(): JSX.Element {
   const state = useResetPassword();
   const params: any = {
-    ...qs.parse(window.location.search)
+    ...qs.parse(window.location.search),
   };
   const [resetState, setResetState] = useState<ResetPasswordCode>({
     email: params.email,
-    code: "", 
-    password: "", 
-    passwordConfirmation: ""
+    code: "",
+    password: "",
+    passwordConfirmation: "",
   });
 
   function handleOtpChange(e: React.ChangeEvent<HTMLInputElement>) {
     const value = e.target.value;
     // if user removes any character then the length will not changes
     // otherwise if we didn't use this suppose user enter the code 2645 and removes the 5 from UI then the 5 will remains stored in ours otp.code state therefore we've used slice() function
-     if (value === "") {
+    if (value === "") {
       setResetState((prevOtp) => ({
         ...prevOtp,
-        code: prevOtp.code.slice(0, prevOtp.code.length - 1)
+        code: prevOtp.code.slice(0, prevOtp.code.length - 1),
       }));
     } else {
       // Concatenate the value with otp.code
       setResetState((prevOtp) => ({ ...prevOtp, code: prevOtp.code + value }));
     }
   }
-  
+
   function inputHandler(e: React.ChangeEvent<HTMLInputElement>) {
     setResetState({
       ...resetState,
-      [e.target.name]: e.target.value
-    })
+      [e.target.name]: e.target.value,
+    });
   }
-
-  
 
   return (
     <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50">
@@ -52,47 +52,47 @@ export default function ResetPasswordPage(): JSX.Element {
             </div>
           </div>
           <div>
-              <div className="flex flex-col space-y-16">
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    New Password
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="password"
-                      name="password"
-                      type="password"
-                      value={resetState.password}
-                      onChange={inputHandler}
-                      autoComplete="email"
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
+            <div className="flex flex-col space-y-16">
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  New Password
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    value={resetState.password}
+                    onChange={inputHandler}
+                    autoComplete="email"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
                 </div>
-                <div>
-                  <label
-                    htmlFor="cpassword"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Confirm New Password
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="cpassword"
-                      name="passwordConfirmation"
-                      type="password"
-                      onChange={inputHandler}
-                      value={resetState.passwordConfirmation}
-                      autoComplete="email"
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                <div className="flex flex-row items-center justify-between mx-auto w-full max-w">
+              </div>
+              <div>
+                <label
+                  htmlFor="cpassword"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Confirm New Password
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="cpassword"
+                    name="passwordConfirmation"
+                    type="password"
+                    onChange={inputHandler}
+                    value={resetState.passwordConfirmation}
+                    autoComplete="email"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+                <div className="flex flex-row mt-8 items-center justify-between mx-auto w-full max-w">
                   {/* Array.from() to create an array of the length provided */}
                   {Array.from({ length: 6 }, (_, index) => (
                     <div className="w-16 h-16" key={index}>
@@ -105,28 +105,43 @@ export default function ResetPasswordPage(): JSX.Element {
                     </div>
                   ))}
                 </div>
-                </div>
-                <div className="flex flex-col space-y-5">
-                  <div> 
-                    <button className="flex flex-row items-center justify-center text-center w-full border rounded-xl outline-none py-5 bg-blue-700 border-none text-white text-sm shadow-sm"
-                    // disabled={otp.length !== 6}
-                      onClick={() => state.resetPassword({...resetState})}
-                    >
-                      Change Password
-                    </button>
-                  </div>
-                  <div className="flex flex-row items-center justify-center text-center text-sm font-medium space-x-1 text-gray-500">
-                    <p>Didn't recieve code?</p>
-                    <button
+              </div>
+              <div className="flex flex-row items-center justify-center text-center text-sm font-medium space-x-1 text-gray-500">
+                <p>Didn't recieve code?</p>
+                <button
                       className="flex flex-row items-center text-blue-600"
                       onClick={() => state.sendResetPasswordCode(params)}
                       // no need for sending id of user because only non-verified user can send this request and id will get fetched automatically in backend
                     >
                       Resend
                     </button>
-                  </div>
+              </div>
+              <div className="flex flex-col space-y-5">
+                <div>
+                  <button
+                    className="flex flex-row items-center justify-center text-center w-full border rounded-xl outline-none py-5 bg-blue-700 border-none text-white text-sm shadow-sm"
+                    // disabled={otp.length !== 6}
+                    onClick={() => state.resetPassword({ ...resetState })}
+                  >
+                    Change Password
+                  </button>
+                </div>
+                <div className="flex flex-row items-center justify-center text-center text-sm font-medium space-x-1 text-gray-500">
+                  <p className="text-center text-sm text-gray-500">
+                    Remembered your account ?
+                    <Link
+                      to={ROUTE_PATHS.AUTH_SIGNIN}
+                      className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                    >
+                      {" "}
+                      Sign In
+                    </Link>
+                  </p>
+                  
+                  
                 </div>
               </div>
+            </div>
           </div>
         </div>
       </div>
