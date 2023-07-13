19c879a7b214f457867c315800a7519e41ab8cd8
huzidev <huzaifa.iqdev@gmail.com>
2023-07-13 09:59:42 +0500

    react form required

--- blog-app/blog-web/src/pages/auth/SendResetPassword/index.tsx
+++ blog-app/blog-web/src/pages/auth/SendResetPassword/index.tsx
@@ -27,6 +27,11 @@ export default function SendResetPasswordPage(): JSX.Element {
 
   useResetPasswordPageHooks();
 
+
+  function submit() {
+      state.sendResetPasswordCode(email)
+  }
+
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -40,37 +45,33 @@ export default function SendResetPasswordPage(): JSX.Element {
         </h2>
       </div>
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <div>
             <label
               htmlFor="email"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
               Email
             </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                value={email.email}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setEmail({ ...email, email: e.target.value })
-                }
-                autoComplete="email"
-                required
-                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            <form onSubmit={submit}>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  value={email.email}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setEmail({ ...email, email: e.target.value })
+                  }
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              <input 
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 my-6 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                type="submit"
+                value="Send Code"
               />
-            </div>
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 my-6 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={() => state.sendResetPasswordCode(email)}
-            >
-                Send Code
-            </button>
-          </div>
+            </form>
         <p className="text-center text-sm text-gray-500">
           Remembered your account ?
           <Link
