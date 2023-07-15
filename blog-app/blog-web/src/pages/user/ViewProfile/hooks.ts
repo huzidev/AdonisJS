2bd39fe28c2cbc39848044a560dee8c92878671a
huzidev <huzaifa.iqdev@gmail.com>
2023-07-15 13:09:36 +0500

    notificatio hook

--- blog-app/blog-web/src/pages/user/ViewProfile/hooks.ts
+++ blog-app/blog-web/src/pages/user/ViewProfile/hooks.ts
@@ -1,26 +1,35 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useBlogs } from "store/articles";
 import { useUser } from "store/user";
 import { usePrevious } from "utils/hooks";
 import { successNotification } from "utils/notifications";
 
 export function useViewProfilePageHook(): void {
   const user = useUser();
+  const blogs = useBlogs();
   const username = user.state.getUser.data?.username;
   const params = useParams();
-  const state = user.state.getUser;
-  const prev = usePrevious(state);
+  const userState = user.state.getUser;
+  const blogState = blogs.state.deleteBlog;
+  const prevUser = usePrevious(userState);
+  const prevBlog = usePrevious(blogState);
 
   useEffect(() => {
     if (params.id === "me") {
       successNotification("Your details fetched successfully!");
     }
-    if (prev?.loading) {
-      if (!state.loading && !state.error) {
+    if (prevUser?.loading) {
+      if (!userState.loading && !userState.error) {
         if (username) {
           successNotification(`${username}'s details fetched successfully`);
         }
       }
     }
-  }, [username, params.id, state]);
+    if (prevBlog?.loading) {
+      if (!blogState.loading && !blogState.error) {
+        successNotification(blogState.message)
+      }
+    }
+  }, [username, params.id, userState, blogState]);
 }
