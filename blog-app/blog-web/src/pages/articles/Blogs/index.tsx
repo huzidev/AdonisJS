c8c9cf8db3419e8c5cc1f6814dacf56c50b3368c
huzidev <huzaifa.iqdev@gmail.com>
2023-07-15 15:53:29 +0500

    get blogs error

--- blog-app/blog-web/src/pages/articles/Blogs/index.tsx
+++ blog-app/blog-web/src/pages/articles/Blogs/index.tsx
@@ -45,7 +45,7 @@ export default function ViewBlogsPage(): JSX.Element {
           );
           const uploadedByUserId = uploadedByUser && uploadedByUser.id;
           const uploadedByUsername = uploadedByUser && uploadedByUser.username;
-          console.log("uploadByUsername", uploadedByUsername)
+          console.log("uploadedByUsername", uploadedByUsername);
           
           return (
             <div key={blog.id} className="w-[30.33%] mt-8 mx-4">
