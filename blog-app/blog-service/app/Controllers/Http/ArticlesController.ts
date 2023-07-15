8fbdca3c6b44497eee225c549df50b3c833ad2a7
huzidev <huzaifa.iqdev@gmail.com>
2023-07-15 12:34:23 +0500

    update blog

--- blog-app/blog-service/app/Controllers/Http/ArticlesController.ts
+++ blog-app/blog-service/app/Controllers/Http/ArticlesController.ts
@@ -1,99 +1,108 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import Article from "App/Models/Article";
-import User from 'App/Models/User';
+import User from "App/Models/User";
 import { CreateArticle, UpdateArticle } from "App/Validators/ArticleValidator";
 
 // Controller used to call the functions created in routes here so routes won't get messed up
 
-const noArticle = { message: "No such article is found ", status: 404 }
-const noPermission = { message: "You didn't have permission", status: 401 }
+const noArticle = { message: "No such article is found ", status: 404 };
+const noPermission = { message: "You didn't have permission", status: 401 };
 export default class ArticlesController {
-    public async getBlogs({ params }: HttpContextContract) {
-            const userId = params.id;
-            const query = Article.query();
-    
-            // if user wanted to see allBlogs uploaded by him
-            if (userId) {
-                query.where('owner_id', userId);
-            }
-            const response = await query.paginate(params.page || 1, 5);
-            return response;
-    }
-    
-    public async addBlog({ request, auth }: HttpContextContract) {
-        try {
-            // body is receiving title, image, content as of request.body, we used request.validate instead of req.body
-            const body = await request.validate(CreateArticle);
-            await Article.create({ 
-                ...body,
-                ownerId: auth.user?.id
-            });
-            return { 
-                data: body, 
-                message: "Blog created successfully" 
-            };
-        } catch (e) {
-          throw e
-        }
-    }
-        
-    public async getById({ params }: HttpContextContract) {
-        try {
-            const article = await Article.findBy("slug", params.slug);
-            // using .first() because we are receving array[]
-            let user;
-            if (article) {
-                user = await User.query().where("id", article!.ownerId).first();
-            }
-            if (!article) {
-                throw noArticle
-            } else {
-                return { 
-                    article, 
-                    message: `Blog by ${user?.username} fetched successfully`
-                }
-            }
-        } catch (e) {
-            throw e
-        }
-    }
+  public async getBlogs({ params }: HttpContextContract) {
+    const userId = params.id;
+    const query = Article.query();
 
-    public async updateBlog({ request, auth, params }: HttpContextContract) {
-        try {
-            const body = await request.validate(UpdateArticle);
-            const article = await Article.findBy("id", params.id);
-            // const articleId: number = params.id;
-            if (!article) {
-                throw noArticle
-            // isAdmin created in User Model
-            } else if (article.ownerId !== auth.user?.id && !auth.user?.isAdmin()) {
-                throw noPermission
-            } else {
-                // await Article.query().where("id", articleId).update(body);
-                article.fill({ ...article, ...body });
-                article.merge(body);
-                await article.save();
-                return { 
-                    data: article,
-                    message: "Blog updated successfully" 
-                }
-            }
-        } catch (e) {
-            throw e
-        }
+    // if user wanted to see allBlogs uploaded by him
+    if (userId) {
+      query.where("owner_id", userId);
     }
+    const response = await query.paginate(params.page || 1, 5);
+    return response;
+  }
 
-    public async deleteBlog({ auth, params }: HttpContextContract) {
-        const article = await Article.findBy("id", params.id);
-        if (!article) {
-            throw noArticle
-        } else if (article.ownerId !== auth.user?.id && !auth.user?.isAdmin()) {
-            throw noPermission
-        } 
-        else {
-            await article.delete();
-            // returning id because we'll call filter on ours redux store to updated the state after filter out the deleted blog with id
-            return { message: `Blog with id ${params.id} deleted successfully`, id: Number(params.id) };
-        }
-    }    
+  public async addBlog({ request, auth }: HttpContextContract) {
+    try {
+      // body is receiving title, image, content as of request.body, we used request.validate instead of req.body
+      const body = await request.validate(CreateArticle);
+      await Article.create({
+        ...body,
+        ownerId: auth.user?.id,
+      });
+      return {
+        data: body,
+        message: "Blog created successfully",
+      };
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  public async getById({ params }: HttpContextContract) {
+    try {
+      const article = await Article.findBy("slug", params.slug);
+      // using .first() because we are receving array[]
+      let user;
+      if (article) {
+        user = await User.query().where("id", article!.ownerId).first();
+      }
+      if (!article) {
+        throw noArticle;
+      } else {
+        return {
+          article,
+          message: `Blog by ${user?.username} fetched successfully`,
+        };
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  public async updateBlog({ request, auth, params }: HttpContextContract) {
+    try {
+      const body = await request.validate(UpdateArticle);
+      const article = await Article.findBy("id", params.id);
+      // const articleId: number = params.id;
+      // if user didn't change any value and tries to update
+      if (body.title === article?.title && body.content === article?.content) {
+        throw {
+          message: "Values are same as of before",
+          status: 401,
+        };
+      }
+      if (!article) {
+        throw noArticle;
+        // isAdmin created in User Model
+      } else if (article.ownerId !== auth.user?.id && !auth.user?.isAdmin()) {
+        throw noPermission;
+      } else {
+        // await Article.query().where("id", articleId).update(body);
+        article.fill({ ...article, ...body });
+        article.merge(body);
+        await article.save();
+        return {
+          data: article,
+          message: "Blog updated successfully",
+        };
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  public async deleteBlog({ auth, params }: HttpContextContract) {
+    const article = await Article.findBy("id", params.id);
+    if (!article) {
+      throw noArticle;
+    } else if (article.ownerId !== auth.user?.id && !auth.user?.isAdmin()) {
+      throw noPermission;
+    } else {
+      await article.delete();
+      // returning id because we'll call filter on ours redux store to updated the state after filter out the deleted blog with id
+      return {
+        message: `Blog with id ${params.id} deleted successfully`,
+        id: Number(params.id),
+      };
+    }
+  }
 }
