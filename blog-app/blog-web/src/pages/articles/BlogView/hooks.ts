ce77e33d187ef87f35b3208ccf84a7f928ba287d
huzidev <huzaifa.iqdev@gmail.com>
2023-07-17 09:33:26 +0500

    ts custom hook

--- blog-app/blog-web/src/pages/articles/BlogView/hooks.ts
+++ blog-app/blog-web/src/pages/articles/BlogView/hooks.ts
@@ -2,6 +2,7 @@ import ROUTE_PATHS from "Router/paths";
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useBlogs } from "store/articles";
+import { useAuth } from "store/auth";
 import { useUser } from "store/user";
 import { usePrevious } from "utils/hooks";
 import { successNotification } from "utils/notifications";
@@ -10,10 +11,12 @@ export function useGetBlogPageHooks(): void {
   const params: any = useParams();
   const blog = useBlogs();
   const user = useUser();
+  const auth = useAuth();
   const state = blog.state;
   const prev = usePrevious(state);
   const navigate = useNavigate();
   const ownerId: any = blog.state.getBlog.data?.ownerId;
+  const byMe = ownerId === auth.state.user?.id;
 
   // seprately making with different useEffect because while creating in same useEffect as of ownerId it's fetching data multiple times
   useEffect(() => {
@@ -29,8 +32,11 @@ export function useGetBlogPageHooks(): void {
   useEffect(() => {
     if (prev?.getBlog.loading) {
       if (!state.getBlog.loading && !state.getBlog.error) {
-        // if success then it'll show notification of success
-        successNotification(state.getBlog.message);
+        if (byMe) {
+          successNotification("Blog by you fetched successfully");
+        } else {
+          successNotification(state.getBlog.message);
+        }
       } else if (!state.getBlog.loading && state.getBlog.error) {
         navigate(ROUTE_PATHS.NOT_FOUND);
       }
