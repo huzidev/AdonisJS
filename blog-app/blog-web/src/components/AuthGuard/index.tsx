e73c32eca529576f07e30f5ffd7ca5117f6c5e50
huzidev <huzaifa.iqdev@gmail.com>
2023-07-17 19:51:17 +0500

    protected path

--- blog-app/blog-web/src/components/AuthGuard/index.tsx
+++ blog-app/blog-web/src/components/AuthGuard/index.tsx
@@ -5,7 +5,6 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "store/auth";
 import UrlPattern from "url-pattern";
-import { notAllowedPaths } from "./data";
 
 interface AuthGuardProps {
   children: JSX.Element;
@@ -24,8 +23,6 @@ export default function AuthGuard({ children }: AuthGuardProps): JSX.Element {
     // Ex user can't access the path for blogger and user and blogger and can't access the path for admins 
     const isProtected = !!route?.role;
 
-    
-
     useEffect(() => {
       const { initState } = auth.state;
       if (!initState.init && !initState.loading) {
@@ -39,19 +36,22 @@ export default function AuthGuard({ children }: AuthGuardProps): JSX.Element {
       // if (!auth.state.user) {
       //   Navigate("/")
       // } 
-      if (!auth.state.user && notAllowedPaths.includes(currentPath)) {
-        navigate("/")
+      // if (!auth.state.user && notAllowedPaths.includes(currentPath)) {
+      //   navigate("/")
+      // }
+      if (isProtected && !auth.state.user) {
+        navigate('/')
       }
       else if (auth.state.user) {
         const { isVerified, isBanned } = auth.state.user;
         if (isBanned && currentPath !== ROUTE_PATHS.BANNED_USER) {
           navigate(ROUTE_PATHS.BANNED_USER);  
         } else if (currentPath === ROUTE_PATHS.AUTH_SIGNIN || currentPath === ROUTE_PATHS.AUTH_SIGNUP) {
-          navigate("/");
+          navigate('/')
         } else if ((!isVerified) && currentPath !== ROUTE_PATHS.VERIFY_USER) {
           navigate(ROUTE_PATHS.VERIFY_USER)
         } else if (isVerified && currentPath === ROUTE_PATHS.VERIFY_USER) {
-          navigate("/")
+          navigate('/')
         }
         else {
           setState(true)
