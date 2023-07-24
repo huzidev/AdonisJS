b70b3e0cc537a692ac8bd7e795424ef5b27e326c
huzidev <huzaifa.iqdev@gmail.com>
2023-07-25 00:13:55 +0500

    indenataions

--- blog-app/blog-web/src/pages/user/ManageUsers/data.ts
+++ blog-app/blog-web/src/pages/user/ManageUsers/data.ts
@@ -1,30 +1,59 @@
-
 // these columnKeys will be shown at top when Admin clicked on manage users
 const columnsKeys = [
-  'sno',
-  'id',
-  'username',
-  'email',
-  'role',
-  'isActive',
-  'isBanned',
-  'isVerified',
-  'createdAt',
-  'updatedAt',
-  'actions'
+  "sno",
+  "id",
+  "username",
+  "email",
+  "role",
+  "isActive",
+  "isBanned",
+  "isVerified",
+  "createdAt",
+  "updatedAt",
+  "actions",
 ];
 
 // boolKeys means they'll either be true(yes) or false(no)
-export const booleanKeys = ['isActive', 'isBanned', 'isVerified'];
-export const notBooleanResult = ['asc', 'desc', 'admin', 'super-admin', 'user', 'blogger'];
-export const notAltResult = ['true', 'false', 'admin', "super-admin", "user", "blogger"];
-export const notRoleResult = ['asc', 'desc', 'true', 'false'];
-export const alternateKeys = ['id', 'createdAt', 'email', 'username', 'createdAt', 'updatedAt'];
-export const typeResult = ["asc", "desc", "true", "false", "admin", "super-admin", "user", "blogger"];
-export const constKeys = ['sno', 'actions'];
+export const booleanKeys = ["isActive", "isBanned", "isVerified"];
+export const notRoleResult = ["asc", "desc", "true", "false"];
+export const notBooleanResult = [
+  "asc",
+  "desc",
+  "admin",
+  "super-admin",
+  "user",
+  "blogger",
+];
+export const notAltResult = [
+  "true",
+  "false",
+  "admin",
+  "super-admin",
+  "user",
+  "blogger",
+];
+export const alternateKeys = [
+  "id",
+  "createdAt",
+  "email",
+  "username",
+  "createdAt",
+  "updatedAt",
+];
+export const typeResult = [
+  "asc",
+  "desc",
+  "true",
+  "false",
+  "admin",
+  "super-admin",
+  "user",
+  "blogger",
+];
+export const constKeys = ["sno", "actions"];
 
 export const columns = columnsKeys.map((key) => ({
   key,
   dataIndex: key,
-  title: key
+  title: key,
 }));
