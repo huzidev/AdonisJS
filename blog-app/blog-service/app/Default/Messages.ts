328fbd8e9df96dc01f2c1c1fb340267572046085
huzidev <huzaifa.iqdev@gmail.com>
2023-07-24 11:41:05 +0500

    indentations

--- blog-app/blog-service/app/Default/Messages.ts
+++ blog-app/blog-service/app/Default/Messages.ts
@@ -1,21 +1,27 @@
-export const noArticle = { message: "No such article is found ", status: 404 };
+export const noArticle = { 
+  message: "No such article is found ", 
+  status: 404 
+};
 export const noPermission = {
   message:
     "Insufficient access, you do not have permission to perform this action",
-  status: 401,
+    status: 401
 };
-export const blogsFetched = "blogs fetched successfully";
 export const usernameExist = {
   message: "Username already exist",
-  status: 409,
+  status: 409
 };
 export const emailExist = {
   message: "Email already exist",
-  status: 409,
+  status: 409
 };
 export const invalidCredentials = {
-    message: "Email or Password is incorrect", status: 400
+  message: "Email or Password is incorrect",
+  status: 400
 };
 export const noEmailFound = {
-  message: 'No user is registered with this email', status: 404
+  message: "No user is registered with this email",
+  status: 404
 };
+
+export const blogsFetched = "blogs fetched successfully";
