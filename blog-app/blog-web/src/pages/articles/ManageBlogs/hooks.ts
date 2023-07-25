cb34fd55b819f5feeb55f9f0b45a352146622142
huzidev <huzaifa.iqdev@gmail.com>
2023-07-25 23:20:54 +0500

    indentations

--- blog-app/blog-web/src/pages/articles/ManageBlogs/hooks.ts
+++ blog-app/blog-web/src/pages/articles/ManageBlogs/hooks.ts
@@ -1,13 +1,13 @@
-import qs from 'query-string';
+import qs from "query-string";
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { useBlogs } from 'store/articles';
-import { useAuth } from 'store/auth';
-import { useUser } from 'store/user';
-import { hasPermission } from 'utils';
+import { useBlogs } from "store/articles";
+import { useAuth } from "store/auth";
+import { useUser } from "store/user";
+import { hasPermission } from "utils";
 import { usePrevious } from "utils/hooks";
 import { successNotification } from "utils/notifications";
-import { PayloadReq } from './types';
+import { PayloadReq } from "./types";
 
 export function useManageBlogsPageHooks(): void {
   const blogs = useBlogs();
@@ -17,12 +17,15 @@ export function useManageBlogsPageHooks(): void {
   const state = blogs.state;
   const prev = usePrevious(state);
   const isMe: any = auth.state.user;
-  const isAdmin = hasPermission(("admin" || "super-admin"), auth.state.user?.role);
+  const isAdmin = hasPermission(
+    "admin" || "super-admin",
+    auth.state.user?.role
+  );
   const navigate = useNavigate();
   const payload: PayloadReq = {
     userId: isMe.id,
-    page: Number(params.page) || 1
-  }
+    page: Number(params.page) || 1,
+  };
 
   // no need for create !allUsers.data or !allUsers.length because new list of users will fetch every time when admin clicked on next buttton
   useEffect(() => {
@@ -30,11 +33,11 @@ export function useManageBlogsPageHooks(): void {
     // but after using it through qs.parse(search) it'll send {sort: '{"id":"desc"}'} which is parsed value
 
     user.allUser();
-  const search: any = qs.parse(window.location.search);
+    const search: any = qs.parse(window.location.search);
     if (isAdmin) {
-      blogs.getBlogsList({page: params.page || 1, ...search})
+      blogs.getBlogsList({ page: params.page || 1, ...search });
     } else {
-      blogs.getMyList(payload)
+      blogs.getMyList(payload);
     }
   }, [params.page, window.location.search]);
 
@@ -42,11 +45,11 @@ export function useManageBlogsPageHooks(): void {
     if (prev?.getBlogs.loading) {
       if (!state.getBlogs.loading && !state.getBlogs.error) {
         successNotification(state.getBlogs.message);
-      } 
+      }
       // if user enter the page exceeding the page limit
       else if (!state.getBlogs.loading && state.getBlogs.error) {
         navigate("/");
       }
     }
-  }, [state])
+  }, [state]);
 }
