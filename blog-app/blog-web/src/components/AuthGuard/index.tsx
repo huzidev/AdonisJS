9dbb2bc11c2434ed080f0c4adfd92f1bed5b80fa
huzidev <huzaifa.iqdev@gmail.com>
2023-07-31 18:52:58 +0500

    fixing loading type

--- blog-app/blog-web/src/components/AuthGuard/index.tsx
+++ blog-app/blog-web/src/components/AuthGuard/index.tsx
@@ -38,7 +38,7 @@ export default function AuthGuard({ children }: AuthGuardProps): JSX.Element {
     }
 
     // if user is not loggedIn and tries to access paths like edit user, create user etc
-    if (isProtected && !user && initState.init) {
+    if (isProtected && !user && !initState.loading) {
       navigate("/");
       toast.error("You can't access the requested path kindly signin first");
     } else if (user) {
