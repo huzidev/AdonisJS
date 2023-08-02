4a846bd9452a52ce19b03b031c341029108d998a
huzidev <huzaifa.iqdev@gmail.com>
2023-08-02 23:53:48 +0500

    comments for blogs

---
+++ blog-app/blog-service/database/migrations/1691002338838_comments.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'comments'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.integer('user_id').unsigned().references('id').inTable('users').notNullable()
+      table.integer('article_id').unsigned().references('id').inTable('articles').onDelete('CASCADE').notNullable()
+      table.string("comment", 100).notNullable();
+      
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
