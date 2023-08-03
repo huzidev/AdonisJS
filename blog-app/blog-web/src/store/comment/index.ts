28999109da74cd162b030937c7d80e91e1b85b0f
huzidev <huzaifa.iqdev@gmail.com>
2023-08-03 21:42:09 +0500

    index for acitons

--- blog-app/blog-web/src/store/comment/index.ts
+++ blog-app/blog-web/src/store/comment/index.ts
@@ -1,15 +1,17 @@
 import { useSelector } from 'react-redux';
 import { useAppDispatch } from 'store/hooks/hooks';
 import * as actions from "./actions";
-import { AddCommentPayload, CommentState } from './types';
+import { AddCommentPayload, CommentState, GetCommentsState } from './types';
 
 export const useComments = () => {
     const dispatch = useAppDispatch();
     const state = useSelector((state: any) => state.blogs) as CommentState;
     const addComment = (payload: AddCommentPayload) => dispatch(actions.addComment(payload));
+    const getComments = (payload: GetCommentsState) => dispatch(actions.getComments(payload));
     
     return {
         state,
-        addComment
+        addComment,
+        getComments
     }
 }
