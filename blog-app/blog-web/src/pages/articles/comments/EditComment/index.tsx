23134abefad5a85936297031b6d807cb16ca7659
huzidev <huzaifa.iqdev@gmail.com>
2023-08-06 00:31:26 +0500

    route updated

--- blog-app/blog-web/src/pages/articles/comments/EditComment/index.tsx
+++ blog-app/blog-web/src/pages/articles/comments/EditComment/index.tsx
@@ -1,5 +1,13 @@
+import { useEffect, useState } from "react";
+import { useComments } from "store/comment";
 
 export default function EditCommentPage(): JSX.Element {
+    const comment = useComments();
+
+    const [content, setContent] = useState();
+    useEffect(() => {
+        
+    }, [])
   return (
     <div>
         Edit Comment
--- blog-app/blog-web/src/store/comment/endpoints.ts
+++ blog-app/blog-web/src/store/comment/endpoints.ts
@@ -1,3 +1,3 @@
 export const ADD_COMMENT = "/comment/add";
-export const GET_COMMENTS = "/comment/get/";
+export const GET_COMMENTS = "/comment/get_all/";
 export const DELETE_COMMENT = "/comment/delete/";
