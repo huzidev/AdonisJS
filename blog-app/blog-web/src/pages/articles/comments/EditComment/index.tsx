eb8320f442ddcb23022ae117ee553179df6396ca
huzidev <huzaifa.iqdev@gmail.com>
2023-08-06 00:48:07 +0500

    fixing fetch comment

--- blog-app/blog-web/src/pages/articles/comments/EditComment/index.tsx
+++ blog-app/blog-web/src/pages/articles/comments/EditComment/index.tsx
@@ -7,27 +7,27 @@ export default function EditCommentPage(): JSX.Element {
   const params: any = useParams();
   const commentResp = comment.state.getCommentById.data;
   console.log("Comment resp", commentResp);
-  const [content, setContent] = useState<any>();
+  const [content, setContent] = useState<any>({comment: ""});
 
   useEffect(() => {
     comment.getById(params.id);
-    setContent({ ...content, commentResp });
+    setContent({ ...content, comment: commentResp });
   }, []);
 
   return (
     <>
       <label
-        htmlFor="username"
+        htmlFor="comment"
         className="block text-sm font-medium leading-6 text-gray-900"
       >
-        Username
+        Edit Comment
       </label>
       <div className="mt-2">
         <input
-          id="username"
-          name="username"
+          id="comment"
+          name="comment"
           type="text"
-          value={content}
+          value={content.comment}
           onChange={(e) => e.target.value}
           required
           className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -35,7 +35,7 @@ export default function EditCommentPage(): JSX.Element {
         <input
           className="flex mt-6 w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           type="submit"
-          value="Update Details"
+          value="Update Comment"
         />
       </div>
     </>
