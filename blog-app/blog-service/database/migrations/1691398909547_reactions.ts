0812c26ebac3dd06c27f4dfdc74977e6e61b3640
huzidev <huzaifa.iqdev@gmail.com>
2023-08-07 15:28:48 +0500

    reactions table

--- blog-app/blog-service/database/migrations/1691398909547_reactions.ts
+++ blog-app/blog-service/database/migrations/1691398909547_reactions.ts
@@ -1,7 +1,7 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'likes'
+  protected tableName = 'reactions'
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
@@ -10,6 +10,9 @@ export default class extends BaseSchema {
       table.integer('user_id').unsigned().references('id').inTable('users').notNullable()
       table.integer('article_id').unsigned().references('id').inTable('articles').onDelete('CASCADE').notNullable()
       
+      table.boolean('is_like').defaultTo(false).notNullable()
+      table.boolean('is_dislike').defaultTo(false).notNullable()
+
       table.timestamp('updated_at', { useTz: true }).notNullable()
       table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(this.now())
     })
