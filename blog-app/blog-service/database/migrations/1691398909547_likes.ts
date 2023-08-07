da56199b343c94d9e28b93baa1622eea62fdf1f8
huzidev <huzaifa.iqdev@gmail.com>
2023-08-07 14:04:55 +0500

    table for likes

---
+++ blog-app/blog-service/database/migrations/1691398909547_likes.ts
@@ -0,0 +1,21 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'likes'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.integer('user_id').unsigned().references('id').inTable('users').notNullable()
+      table.integer('article_id').unsigned().references('id').inTable('articles').onDelete('CASCADE').notNullable()
+      
+      table.timestamp('updated_at', { useTz: true }).notNullable()
+      table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(this.now())
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
