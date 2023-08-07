9a8ec236f344a6823f41c5ff2f6ea4d3494b59e6
huzidev <huzaifa.iqdev@gmail.com>
2023-08-07 12:06:18 +0500

    reply cancel button

--- blog-app/blog-web/src/pages/articles/comments/ShowComments/index.tsx
+++ blog-app/blog-web/src/pages/articles/comments/ShowComments/index.tsx
@@ -13,7 +13,9 @@ export default function CommentsPage(props: PropsState): JSX.Element {
   const auth = useAuth();
   const userData = auth.state.user;
   const { content, setContent, allComments, allUsers } = useCommentPageHooks();
-  const [replyState, setReplyState] = useState<boolean>(false);
+  const [replyState, setReplyState] = useState<any>({
+    id: null,
+  });
   const [reply, setReply] = useState<any>()
 
   function submit(e: React.FormEvent<HTMLFormElement>) {
@@ -76,7 +78,7 @@ export default function CommentsPage(props: PropsState): JSX.Element {
                         </p>
                         <p>{value.comment}</p>
                       </div>
-                      {replyState && (
+                      {replyState.id === value.id && (
                         <div className="mt-2">
                           <input
                             id="reply"
@@ -87,11 +89,16 @@ export default function CommentsPage(props: PropsState): JSX.Element {
                             required
                             className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                           />
-                          <input
-                            className="flex mt-6 w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            type="submit"
-                            value="post"
-                          />
+                          <div>
+                            <input
+                              className="flex mt-6 w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                              type="submit"
+                              value="post"
+                            />
+                            <button onClick={() => setReplyState({ id: null })}>
+                              Cancel
+                            </button>
+                          </div>
                         </div>
                       )}
                       <p>{new Date(value.createdAt).toLocaleDateString()}</p>
@@ -115,7 +122,7 @@ export default function CommentsPage(props: PropsState): JSX.Element {
                       )}
                       {" "}
                       <button
-                        onClick={() => setReplyState(!replyState)}
+                        onClick={() => setReplyState({ id: value.id })}
                       >
                         Reply
                       </button>
