5a5c92b8b08a6fde87fd3eff93ae0c435962dd14
huzidev <huzaifa.iqdev@gmail.com>
2023-08-07 15:34:33 +0500

    models updated

---
+++ blog-app/blog-service/app/Models/Reaction.ts
@@ -0,0 +1,25 @@
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from 'luxon'
+
+export default class Reaction extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public userId: number
+
+  @column()
+  public articleId: number
+
+  @column({ consume: (v) => !!v })
+  public isLike: boolean = true
+
+  @column({ consume: (v) => !!v })
+  public isDislike: boolean = false
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
