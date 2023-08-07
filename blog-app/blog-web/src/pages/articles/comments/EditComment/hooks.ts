21e986884e70bb38950c171986e2d06e8a6b96a4
huzidev <huzaifa.iqdev@gmail.com>
2023-08-07 10:29:07 +0500

    fixing message state

--- blog-app/blog-web/src/pages/articles/comments/EditComment/hooks.ts
+++ blog-app/blog-web/src/pages/articles/comments/EditComment/hooks.ts
@@ -1,11 +1,23 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useComments } from "store/comment";
+import { usePrevious } from "utils/hooks";
+import { successNotification } from "utils/notifications";
 
 export function useEditCommentPageHooks() {
     const comment = useComments();
     const params: any = useParams();
+    const commentState = comment.state;
+    const prev = usePrevious(commentState);
     useEffect(() => {
         comment.getById(params.id);
     }, []);
+
+    useEffect(() => {
+        if (prev?.getCommentById.loading) {
+            if (!commentState.getCommentById.loading && !commentState.getCommentById.error) {
+                successNotification(commentState.getCommentById.message)
+            }
+        }
+    }, [commentState])
 }
