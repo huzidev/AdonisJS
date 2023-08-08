804c2d84d52c71a2f66c7ef4a93cd8681c2531f9
huzidev <huzaifa.iqdev@gmail.com>
2023-08-08 11:46:45 +0500

    redux actions

--- blog-app/blog-web/src/store/reactions/actions.ts
+++ blog-app/blog-web/src/store/reactions/actions.ts
@@ -1,12 +1,16 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import api from "services/api";
+import { mapErrorToState } from "store/utils";
+import { errorNotification } from "utils/notifications";
 import * as endpoints from "./endpoints";
+import { AddReactionState } from "./types";
 
 
 export const addReaction = createAsyncThunk(
   endpoints.ADD_REACTION,
-  async (data: AddCommentPayload) => {
+  async (data: AddReactionState) => {
     try {
-      const response = await api.post(endpoints.ADD_COMMENT, data);
+      const response = await api.post(endpoints.ADD_REACTION, data);
       console.log('Add Comment response', response.data);
       return {
         message: response.data.message,
--- blog-app/blog-web/src/store/reactions/types.ts
+++ blog-app/blog-web/src/store/reactions/types.ts
@@ -1,6 +1,12 @@
-export interface AddReactionPayload {
+import { SubState } from "store/types";
+
+export interface AddReactionState extends SubState {
     userId: number;
     articleId: number;
     isLike: boolean;
     isDislike: boolean;
+}
+
+export interface ReactionState {
+  addReaction: AddReactionState;
 }
