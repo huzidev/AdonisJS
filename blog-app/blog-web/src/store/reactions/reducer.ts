57bc2c1c474fd3755910098776544aef1c6b0382
huzidev <huzaifa.iqdev@gmail.com>
2023-08-08 12:12:31 +0500

    indnetations

--- blog-app/blog-web/src/store/reactions/reducer.ts
+++ blog-app/blog-web/src/store/reactions/reducer.ts
@@ -4,44 +4,44 @@ import * as actions from "./actions";
 import { ReactionState } from "./types";
 
 const initialState: ReactionState = {
-    addReaction:  { ...subState },
-    getReactions:  { ...subState }
-}
+  addReaction: { ...subState },
+  getReactions: { ...subState },
+};
 
 export const reactionSlice = createSlice({
-    name: "reactions",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        // Add Reactions (Likes, Dislikes)
-         builder.addCase(actions.addReaction.pending, (state) => {
-          state.addReaction = { loading: true, error: false };
-        });
-        builder.addCase(actions.addReaction.fulfilled, (state, action) => {
-            state.addReaction = { loading: false, error: false };
-            if (action.payload) {
-                state.addReaction.message = action.payload.message;
-            }
-        })
-        builder.addCase(actions.addReaction.rejected, (state) => {
-          state.addReaction = { loading: false, error: true };
-        });
-        // Get Reactions (Likes, Dislikes)
-        builder.addCase(actions.getReactions.pending, (state) => {
-          state.getReactions = { loading: true, error: false };
-        });
-        builder.addCase(actions.getReactions.fulfilled, (state, action) => {
-            state.getReactions = { loading: false, error: false };
-            if (action.payload) {
-                const { message, data } = action.payload;
-                state.getReactions.data = data;
-                state.getReactions.message = message;
-            }
-        })
-        builder.addCase(actions.getReactions.rejected, (state) => {
-          state.getReactions = { loading: false, error: true };
-        });
-    }
-})
+  name: "reactions",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    // Add Reactions (Likes, Dislikes)
+    builder.addCase(actions.addReaction.pending, (state) => {
+      state.addReaction = { loading: true, error: false };
+    });
+    builder.addCase(actions.addReaction.fulfilled, (state, action) => {
+      state.addReaction = { loading: false, error: false };
+      if (action.payload) {
+        state.addReaction.message = action.payload.message;
+      }
+    });
+    builder.addCase(actions.addReaction.rejected, (state) => {
+      state.addReaction = { loading: false, error: true };
+    });
+    // Get Reactions (Likes, Dislikes)
+    builder.addCase(actions.getReactions.pending, (state) => {
+      state.getReactions = { loading: true, error: false };
+    });
+    builder.addCase(actions.getReactions.fulfilled, (state, action) => {
+      state.getReactions = { loading: false, error: false };
+      if (action.payload) {
+        const { message, data } = action.payload;
+        state.getReactions.data = data;
+        state.getReactions.message = message;
+      }
+    });
+    builder.addCase(actions.getReactions.rejected, (state) => {
+      state.getReactions = { loading: false, error: true };
+    });
+  },
+});
 
 export default reactionSlice.reducer;
