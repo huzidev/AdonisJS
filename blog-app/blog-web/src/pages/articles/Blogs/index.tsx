d77af44a0e27aa954c750d4674fc1c6ce8b9b933
huzidev <huzaifa.iqdev@gmail.com>
2023-08-10 00:25:21 +0500

    updating filter

--- blog-app/blog-web/src/pages/articles/Blogs/index.tsx
+++ blog-app/blog-web/src/pages/articles/Blogs/index.tsx
@@ -29,18 +29,13 @@ export default function ViewBlogsPage(): JSX.Element {
 
   const { loadMore, handleSort, sortValue, isLoading, allReactions } = useBlogsPageHooks();
   
-  if (allReactions) {
-    console.log("all reactions", allReactions.filter((value: any) => value.likeCount));
-  }
-  
-
   return (
     <div className="w-10/12 m-auto flex flex-col">
       <div>
         {sortValue.value && (
           <button
             className="text-white mr-5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            onClick={() => handleSort("")}
+            onClick={() => handleSort('')}
           >
             Reset Filters
           </button>
