b17f0428e5ccbe19aa6e29439c1d349ff52814c0
huzidev <huzaifa.iqdev@gmail.com>
2023-08-11 16:44:12 +0500

    onDelete

--- blog-app/blog-service/database/migrations/1691753942769_replies.ts
+++ blog-app/blog-service/database/migrations/1691753942769_replies.ts
@@ -1,17 +1,19 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'comments'
+  protected tableName = 'replies'
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
 
+      // not using onDelete cascade here because USER can't be deleted
       table.integer('user_id').unsigned().references('id').inTable('users').notNullable()
+      // if that artcile is delete then delte comment and replies as well related to this
       table.integer('article_id').unsigned().references('id').inTable('articles').onDelete('CASCADE').notNullable()
-      table.string("comment", 100).notNullable();
-      table.integer('reply_id').unsigned().references('id').inTable('comments');
-      
+      // delete all replies if comment with that id is deleted
+      table.integer('comment_id').unsigned().references('id').inTable('comments').onDelete('CASCADE').notNullable()
+
       table.timestamp("created_at", { useTz: true }).notNullable().defaultTo(this.now());
       table.timestamp("updated_at", { useTz: true }).notNullable();
     })
