ce63b1fbd8d0a358cabc79754a89ca87ef19fb92
huzidev <huzaifa.iqdev@gmail.com>
2023-08-11 21:51:55 +0500

    fetch replies

--- blog-app/blog-service/app/Controllers/Http/RepliesController.ts
+++ blog-app/blog-service/app/Controllers/Http/RepliesController.ts
@@ -1,24 +1,41 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import Reply from 'App/Models/Reply';
-import User from 'App/Models/User';
-import { AddReply } from 'App/Validators/ReplyValidator';
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import Reply from "App/Models/Reply";
+import User from "App/Models/User";
+import { AddReply } from "App/Validators/ReplyValidator";
 
 export default class RepliesController {
-    public async add({ request }: HttpContextContract) {
+  public async add({ request }: HttpContextContract) {
     try {
       const body = await request.validate(AddReply);
 
       const { userId } = body;
 
       const user: any = await User.findBy("id", userId);
-      
+
       await Reply.create(body);
 
       return {
         message: `Reply added successfully by ${user.username}`,
-        data: body
+        data: body,
       };
     } catch (e) {
       throw e;
     }
+  }
+
+  public async getAll({ params }: HttpContextContract) {
+    try {
+      const commentId = params.id;
+      const response = await Reply.query()
+            .where("comment_id", commentId)
+            .select('*');
+      // const response = await Article.query().whereIn("id", query); 
+      return {
+        message: "Replies fetched successfully",
+        data: response
+      };
+    } catch (e) {
+      throw e
+    }
+  }
 }
