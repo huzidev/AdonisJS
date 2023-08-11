b26b03c9c9962f3122ee41336e5b4cccb1368f44
huzidev <huzaifa.iqdev@gmail.com>
2023-08-11 22:27:57 +0500

    redux reducers

--- blog-app/blog-web/src/store/reply/actions.ts
+++ blog-app/blog-web/src/store/reply/actions.ts
@@ -3,11 +3,11 @@ import api from "services/api";
 import { mapErrorToState } from "store/utils";
 import { errorNotification } from "utils/notifications";
 import * as endpoints from "./endpoints";
-import { AddCommentPayload, EditCommentPayload } from "./types";
+import { AddReplyPayload, EditReplyPayload } from "./types";
 
 export const addComment = createAsyncThunk(
   endpoints.ADD_REPLY,
-  async (data: AddCommentPayload) => {
+  async (data: AddReplyPayload) => {
     try {
       const response = await api.post(endpoints.ADD_REPLY, data);
       console.log('Add Reply response', response.data);
@@ -56,11 +56,11 @@ export const getCommentById = createAsyncThunk(
 
 export const editComment = createAsyncThunk(
   endpoints.EDIT_REPLY,
-  async (data: EditCommentPayload) => {
+  async (data: EditReplyPayload) => {
     console.log("data", data);
     try {
-      const response = await api.put(endpoints.EDIT_REPLY + data.id, {comment: data.comment});
-      console.log("Edit COMMENT RESP", response.data);
+      const response = await api.put(endpoints.EDIT_REPLY + data.id, {comment: data.reply});
+      console.log("Edit Reply RESP", response.data);
       return response.data;
     } catch (e: any) {
       const err = mapErrorToState(e);
@@ -76,7 +76,7 @@ export const deleteComment = createAsyncThunk(
   async (articleId: number) => {
     try {
       const response = await api.delete(endpoints.DELETE_REPLY + articleId);
-      console.log("RESPONSE fro delete comment", response);
+      console.log("RESPONSE from delete REPLY", response);
       return response.data
     } catch (e: any) {
       const err = mapErrorToState(e);
