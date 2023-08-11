d6b0547badcd28b52415d610ac5618a7105e868c
huzidev <huzaifa.iqdev@gmail.com>
2023-08-11 21:55:37 +0500

    controller updated

--- blog-app/blog-service/app/Controllers/Http/RepliesController.ts
+++ blog-app/blog-service/app/Controllers/Http/RepliesController.ts
@@ -1,7 +1,7 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import Reply from "App/Models/Reply";
 import User from "App/Models/User";
-import { AddReply } from "App/Validators/ReplyValidator";
+import { AddReply, EditReply } from "App/Validators/ReplyValidator";
 
 export default class RepliesController {
   public async add({ request }: HttpContextContract) {
@@ -36,6 +36,53 @@ export default class RepliesController {
       };
     } catch (e) {
       throw e
+    }
+  }
+
+  public async getById({ params }: HttpContextContract) { 
+    try {
+      const reply= await Reply.findBy("id", params.id);
+      if (!reply) {
+        throw {
+          message: `No Reply found by id ${params.id}`
+        }
+      } else {
+        return {
+          message: `Reply fetched successfully by id ${params.id}`,
+          data: reply
+        }
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  public async edit({ params, request }) {
+    try {
+      const body = await request.validate(EditReply);
+      const reply: any = await Reply.findBy("id", params.id);
+
+      reply.fill({ ...reply, ...body });
+      reply.merge(body);
+      await reply.save();
+
+      return {
+        message: "Reply updated successfully",
+        data: reply?.toObject()
+      }
+
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  public async delete({ params }: HttpContextContract) {
+    const response = await Reply.findBy("id", params.id);
+    await response?.delete();
+
+    return {
+      message: `Reply with id ${response?.id} deleted successfully`,
+      response
     }
   }
 }
