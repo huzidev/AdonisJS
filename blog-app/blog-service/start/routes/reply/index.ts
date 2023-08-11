979c1274198d19156649f953a17a9088adc35425
huzidev <huzaifa.iqdev@gmail.com>
2023-08-11 22:08:48 +0500

    replies table worked

--- blog-app/blog-service/start/routes/reply/index.ts
+++ blog-app/blog-service/start/routes/reply/index.ts
@@ -3,8 +3,10 @@ import Route from "@ioc:Adonis/Core/Route";
 export default function ReplyRoutes() {
   const path = "RepliesController";
   Route.group(() => {
-    Route.post("/add", `${path}.add`).middleware("auth");
-    Route.get("/get_all/:id", `${path}.getAll`).where("id", /^[0-9]+$/);
+    Route.post("/add", `${path}.add`)
+      .middleware("auth");
+    Route.get("/get_all/:id", `${path}.getAll`)
+      .where("id", /^[0-9]+$/);
     Route.get("/get/:id", `${path}.getById`)
       .where("id", /^[0-9]+$/)
       .middleware("auth");
