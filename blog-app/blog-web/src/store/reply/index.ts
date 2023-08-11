e7f3b6399e298185ba64f6b77831e7577f1afdc1
huzidev <huzaifa.iqdev@gmail.com>
2023-08-11 22:33:43 +0500

    index for replies

--- blog-app/blog-web/src/store/reply/index.ts
+++ blog-app/blog-web/src/store/reply/index.ts
@@ -1,22 +1,23 @@
 import { useSelector } from "react-redux";
 import { useAppDispatch } from "store/hooks/hooks";
 import * as actions from "./actions";
+import { AddReplyPayload, EditReplyPayload, ReplyState } from "./types";
 
 export const useComments = () => {
   const dispatch = useAppDispatch();
   const state = useSelector((state: any) => state.replies) as ReplyState;
-  const addComment = (payload: AddCommentPayload) => dispatch(actions.addComment(payload));
-  const getComments = (payload: number) => dispatch(actions.getComments(payload));
-  const getById = (payload: number) => dispatch(actions.getCommentById(payload));
-  const deleteComment = (payload: number) => dispatch(actions.deleteComment(payload));
-  const editComment = (payload: EditCommentPayload) => dispatch(actions.editComment(payload));
+  const addReply = (payload: AddReplyPayload) => dispatch(actions.addReply(payload));
+  const getReplies = (payload: number) => dispatch(actions.getReplies(payload));
+  const getById = (payload: number) => dispatch(actions.getReplyById(payload));
+  const deleteReply = (payload: number) => dispatch(actions.deleteReply(payload));
+  const editReply = (payload: EditReplyPayload) => dispatch(actions.editReply(payload));
 
   return {
     state,
-    addComment,
-    getComments,
-    deleteComment,
-    editComment,
+    addReply,
+    getReplies,
+    deleteReply,
+    editReply,
     getById
   };
 };
