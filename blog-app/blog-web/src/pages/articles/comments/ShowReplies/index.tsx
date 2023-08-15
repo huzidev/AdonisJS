d115236a56c3f3593c82370ac3d67341dc2febfd
huzidev <huzaifa.iqdev@gmail.com>
2023-08-15 19:23:12 +0500

    reply button

--- blog-app/blog-web/src/pages/articles/comments/ShowReplies/index.tsx
+++ blog-app/blog-web/src/pages/articles/comments/ShowReplies/index.tsx
@@ -135,6 +135,7 @@ export default function CommentWithReplies({
             </div>
           </div>
         )}
+        {/* reply button wouldn't be shown when user has clicked on reply button */}
         {replyState.id !== comment.id && (
           <button
             className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
