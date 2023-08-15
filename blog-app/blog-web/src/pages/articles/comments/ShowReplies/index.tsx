b8335161a96a781ecd192a73524307ad4849e32a
huzidev <huzaifa.iqdev@gmail.com>
2023-08-15 17:34:49 +0500

    drop down updated

--- blog-app/blog-web/src/pages/articles/comments/ShowReplies/index.tsx
+++ blog-app/blog-web/src/pages/articles/comments/ShowReplies/index.tsx
@@ -22,6 +22,7 @@ export default function CommentWithReplies({
   const uploadedByUser = allUsers.find(
     (user: User) => user.id === comment.userId
   );
+  const [dropDown, setDropDown] = useState<boolean>(false);
   const [reply, setReply] = useState<any>('');
   const isCommentAuthor = comment.userId === userData?.id;
   const isAuthorSuperAdmin = uploadedByUser === "super-admin";
@@ -40,6 +41,31 @@ export default function CommentWithReplies({
           - {commentBy} {uploadedByUserRole === "super-admin" && "*"}{" "}
         </p>
         <p>{new Date(comment.createdAt).toLocaleDateString()}</p>
+        <button id="dropdownComment1Button" data-dropdown-toggle="dropdownComment1"
+                className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                type="button" onClick={() => setDropDown(!dropDown)}>
+                <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z">
+                    </path>
+                </svg>
+                <span className="sr-only">Comment settings</span>
+            </button>
+            <div id="dropdownComment1"
+                className={`${dropDown ? "block": "hidden"} z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600`}>
+                <ul className="py-1 text-sm text-gray-700 dark:text-gray-200"
+                    aria-labelledby="dropdownMenuIconHorizontalButton">
+                    <li>
+                        <a href="#"
+                            className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Delete</a>
+                    </li>
+                </ul>
+            </div>
       </div>
       <p className="text-gray-500 dark:text-gray-400 ml-6">{comment.content}</p>
       <div>
