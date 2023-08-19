5e88d3028cb9cb78ee4deab9fd96312cdf37e42e
huzidev <huzaifa.iqdev@gmail.com>
2023-08-19 20:16:38 +0500

    fixing auth loading

--- blog-app/blog-web/src/pages/auth/Form/index.tsx
+++ blog-app/blog-web/src/pages/auth/Form/index.tsx
@@ -287,7 +287,7 @@ export default function UserFormPage(): JSX.Element {
           <input
             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             type="submit"
-            value={title + (loading && (<LoaderSpin />))}
+            value={title + (loading ? <LoaderSpin /> : '')}
            />
         </div>
         <p className="mt-3 text-center text-sm text-gray-500">
