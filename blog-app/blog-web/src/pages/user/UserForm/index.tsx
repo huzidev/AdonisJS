3df141413c7bcdd09fffac33985ee4fbca532938
huzidev <huzaifa.iqdev@gmail.com>
2023-08-21 10:50:49 +0500

    toggle icon

--- blog-app/blog-web/src/pages/user/UserForm/index.tsx
+++ blog-app/blog-web/src/pages/user/UserForm/index.tsx
@@ -1,3 +1,5 @@
+import LightModeIcon from '@mui/icons-material/LightMode';
+import NightsStayIcon from '@mui/icons-material/NightsStay';
 import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import { useState } from "react";
@@ -12,6 +14,7 @@ import { BooleanState } from "./types";
 export default function UserFormPage() {
   const auth = useAuth();
   const user = useUser();
+  const [isDark, setIsDark] = useState<boolean>();
   const [booleanState, setBooleanState] =
     useState<BooleanState>(detailsBoolean);
   const isLoading = user.state.getUser.loading;
@@ -279,7 +282,7 @@ export default function UserFormPage() {
                     <input type="checkbox" value="" className="sr-only peer" />
                     <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                     <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-                      Toggle me
+                      {isDark ? <NightsStayIcon /> : <LightModeIcon />}
                     </span>
                   </label>
                   <input
