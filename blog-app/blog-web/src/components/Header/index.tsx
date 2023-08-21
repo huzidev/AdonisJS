2a44f0d446c8fd841db783ec0590457a0d4b33ae
huzidev <huzaifa.iqdev@gmail.com>
2023-08-21 15:27:04 +0500

    fixing text colors

--- blog-app/blog-web/src/components/Header/index.tsx
+++ blog-app/blog-web/src/components/Header/index.tsx
@@ -55,7 +55,7 @@ export default function Header(): JSX.Element {
             </svg>
           </button>
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white bg-gray-800 md:bg-gray-900 border-gray-700">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white bg-gray-800 md:bg-gray-900 border-gray-700">
               {auth.state.initState.init && links.map((data, dataIndex) =>
                 // user has to be verified to access the links and if user role is "user" then not to show addBlog Page
                 user && (user.role === "blogger" && loggedInPathsBlogger.includes(data.link) || user.role === "user" && loggedInPathsUser.includes(data.link)) && user.isVerified ? (
