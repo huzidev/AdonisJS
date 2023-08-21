625d00c8b544316837fe1fe2faccac876de9bc72
huzidev <huzaifa.iqdev@gmail.com>
2023-08-21 10:51:36 +0500

    onClick toggle updated

--- blog-app/blog-web/src/pages/user/UserForm/index.tsx
+++ blog-app/blog-web/src/pages/user/UserForm/index.tsx
@@ -279,7 +279,7 @@ export default function UserFormPage() {
                     className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                   <label className="relative inline-flex items-center cursor-pointer">
-                    <input type="checkbox" value="" className="sr-only peer" />
+                    <input onClick={() => setIsDark(!isDark)} type="checkbox" value="" className="sr-only peer" />
                     <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                     <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
                       {isDark ? <NightsStayIcon /> : <LightModeIcon />}
