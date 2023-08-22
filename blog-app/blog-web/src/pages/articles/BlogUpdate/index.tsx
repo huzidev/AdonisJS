c7ddbd7607cde6e8f7793e9c4b00b50fd65e0283
huzidev <huzaifa.iqdev@gmail.com>
2023-08-22 20:28:08 +0500

    fixing create user page

--- blog-app/blog-web/src/pages/articles/BlogUpdate/index.tsx
+++ blog-app/blog-web/src/pages/articles/BlogUpdate/index.tsx
@@ -105,7 +105,7 @@ export default function UpdateBlogPage(): JSX.Element {
                 type="file" 
                 name="image" 
                 onChange={inputHandler}
-                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
+                className="form-input" 
               />
             </div> */}
             <div className="mb-6">
--- blog-app/blog-web/src/pages/user/UserForm/index.tsx
+++ blog-app/blog-web/src/pages/user/UserForm/index.tsx
@@ -46,8 +46,8 @@ export default function UserFormPage() {
   } = useUserFormHook();
   return (
     <div>
-      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+      <div className="main">
+        <div className="form">
           <img
             className="mx-auto h-10 w-auto"
             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
