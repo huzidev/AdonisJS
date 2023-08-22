319a62571cb2509d02a722d43cf8be0862ed5a0a
huzidev <huzaifa.iqdev@gmail.com>
2023-08-22 18:59:17 +0500

    placeholder

--- blog-app/blog-web/src/pages/articles/BlogUpdate/index.tsx
+++ blog-app/blog-web/src/pages/articles/BlogUpdate/index.tsx
@@ -60,7 +60,7 @@ export default function UpdateBlogPage(): JSX.Element {
               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               alt="Your Company"
             />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-white">
               {/* show three dots loading when users data is in loading state */}
               {isLoading
                 ? <LoadingThreeDots />
@@ -120,9 +120,10 @@ export default function UpdateBlogPage(): JSX.Element {
                     cols={30}
                     rows={10}
                     value={content}
+                    placeholder={`Edit ${isOwner ? "Yours" : `${ownerName}'s`} Blog`}
                     required
                     onChange={inputHandler}
-                    className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:text-white dark:bg-gray-800"
                   ></textarea>
                 </div>
               </div>
