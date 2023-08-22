3287917c7013794db550d935987e9d7b638e4292
huzidev <huzaifa.iqdev@gmail.com>
2023-08-22 19:13:44 +0500

    text-area for edit comment

--- blog-app/blog-web/src/pages/articles/comments/EditComment/index.tsx
+++ blog-app/blog-web/src/pages/articles/comments/EditComment/index.tsx
@@ -39,10 +39,11 @@ export default function EditCommentPage(): JSX.Element {
           id="content"
           name="content"
           type="text"
+          placeholder="Edit Comment"
           value={content}
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEditComment({...editComment, content: e.target.value})}
           required
-          className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 dark:bg-gray-800 dark:text-white"
         />
         <input
           className="flex mt-6 w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
