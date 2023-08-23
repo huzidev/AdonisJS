0457e1f73060fb0a71ae9f858229c3c8472f4508
huzidev <huzaifa.iqdev@gmail.com>
2023-08-23 13:24:02 +0500

    reset password page updated

--- blog-app/blog-web/src/pages/auth/ResetPassword/index.tsx
+++ blog-app/blog-web/src/pages/auth/ResetPassword/index.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useResetPassword } from "store/resetPassword";
+import 'utils/form/index.css';
+import 'utils/table/index.css';
 import { resetPasswordState } from "./data";
 import useResetPasswordPageHooks from "./hooks";
 import { ResetPasswordCode } from "./types";
@@ -23,7 +25,7 @@ export default function ResetPasswordPage(): JSX.Element {
     }
     // so if user tries to access resetPassword state without email then navigate user to / homepage
     else {
-      navigate("/")
+      navigate('/')
       toast.error("You've to provide valid email")
     }
   }, [])
@@ -58,23 +60,19 @@ export default function ResetPasswordPage(): JSX.Element {
   }
 
   return (
-    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50">
-      <div className="relative bg-white px-6 pt-10 pb-9 shadow-xl mx-auto w-full max-w-lg rounded-2xl">
+    <div>
+      <div className="relative bg-white px-6 mt-10 py-10 shadow-xl mx-auto w-full max-w-lg rounded-2xl dark:bg-gray-900">
         <div className="mx-auto flex w-full max-w-md flex-col space-y-16">
           <div className="flex flex-col items-center justify-center text-center space-y-2">
-            <div className="font-semibold text-3xl">
-              <p>Reset Password</p>
-            </div>
-            <div className="flex flex-row text-sm font-medium text-gray-400">
-              <p>We have sent a code to your email {resetState.email}</p>
-            </div>
+              <p className="font-semibold text-3xl dark:text-white">Reset Password</p>
+              <p className="form-message">We have sent a code to your email {resetState.email}</p>
           </div>
           <form onSubmit={submit}>
             <div className="flex flex-col space-y-16">
               <div>
                 <label
                   htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  className="form-heading"
                 >
                   New Password
                 </label>
@@ -87,14 +85,14 @@ export default function ResetPasswordPage(): JSX.Element {
                     onChange={inputHandler}
                     autoComplete="email"
                     required
-                    className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="form-input"
                   />
                 </div>
               </div>
               <div>
                 <label
                   htmlFor="cpassword"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  className="form-heading"
                 >
                   Confirm New Password
                 </label>
@@ -107,7 +105,7 @@ export default function ResetPasswordPage(): JSX.Element {
                     value={resetState.passwordConfirmation}
                     autoComplete="email"
                     required
-                    className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="form-input"
                   />
                 </div>
                 <p
@@ -125,7 +123,7 @@ export default function ResetPasswordPage(): JSX.Element {
                   {Array.from({ length: 6 }, (_, index) => (
                     <div className="w-16 h-16" key={index}>
                       <input
-                        className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
+                        className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700 dark:bg-gray-800 dark:text-white"
                         type="text"
                         maxLength={1}
                         onChange={handleOtpChange}
@@ -143,12 +141,12 @@ export default function ResetPasswordPage(): JSX.Element {
                     value="Change Password"
                   />
                 </div>
-                <div className="flex flex-row items-center justify-center text-center text-sm font-medium space-x-1 text-gray-500">
-                  <p className="text-center text-sm text-gray-500">
+                <div className="flex flex-row items-center justify-center text-center text-sm font-medium space-x-1">
+                  <p className="form-message">
                     Remembered your account ?
                     <Link
                       to={ROUTE_PATHS.AUTH_SIGNIN}
-                      className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                      className="form-auth-action"
                     >
                       {" "}
                       Sign In
@@ -158,10 +156,10 @@ export default function ResetPasswordPage(): JSX.Element {
               </div>
             </div>
           </form>
-          <div className="flex flex-row items-center justify-center text-center text-sm font-medium space-x-1 text-gray-500">
+          <div className="flex flex-row items-center justify-center text-center text-sm font-medium space-x-1 text-gray-500 dark:text-gray-400">
             <p>Didn't recieved code?</p>
             <button
-              className="flex flex-row items-center text-blue-600"
+              className="form-auth-action"
               onClick={() => state.resendResetPasswordCode(params)}
               // no need for sending id of user because only non-verified user can send this request and id will get fetched automatically in backend
             >
