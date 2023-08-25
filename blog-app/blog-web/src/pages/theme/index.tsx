3adc318241167da18f05c12c9cab61ec7625414d
huzidev <huzaifa.iqdev@gmail.com>
2023-08-25 15:20:23 +0500

    fixing transtition error tailwind

--- blog-app/blog-web/src/pages/theme/index.tsx
+++ blog-app/blog-web/src/pages/theme/index.tsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react";
 import { useAuth } from "store/auth";
 
 export default function ToggleThemePopUpPage() {
-  const auth = useAuth();
+    const auth = useAuth();
+    const [state, setState] = useState<boolean>(false);
+    const [text, setText] = useState<boolean>(false);
+
+    useEffect(() => {
+        // setTimeout(() => {
+        //     setState(true);
+        // }, 700)
+        setTimeout(() => {
+            setText(true);
+        }, 500)
+    }, [])
+
   return (
-    <div className="h-screen flex justify-center items-center dark:bg-[#181a1b]">
-      <h1 className="dark:text-white transition ease-in-out duration-700">
-        Yours Theme has been changed to {auth.state.isDark ? "Dark" : "Light"}{" "}
-        Mode
-      </h1>
+    <div className={`h-screen flex justify-center items-center dark:bg-[#181a1b]`}>
+        {/* {state && ( */}
+            <h1 className={` dark:text-white ${text ? 'dark:text-neutral-700' : 'dark:text-white'}`}>
+                Yours Theme has been changed to {auth.state.isDark ? "Dark" : "Light"}{" "}
+                Mode
+            </h1>
+        {/* )} */}
     </div>
   );
 }
