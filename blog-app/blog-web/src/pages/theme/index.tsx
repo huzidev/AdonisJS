c94a227daf6247064950ff9ebaab69699ca419ab
huzidev <huzaifa.iqdev@gmail.com>
2023-08-25 23:09:01 +0500

    fixing show theme message

--- blog-app/blog-web/src/pages/theme/index.tsx
+++ blog-app/blog-web/src/pages/theme/index.tsx
@@ -20,11 +20,10 @@ export default function ToggleThemePopUpPage(props: PropsType): JSX.Element {
   return (
     <div
       className={`h-screen flex justify-center items-center transition ease-in-out duration-700 ${props.isDark ? 'bg-white' : 'bg-[#181a1b]'} ${
-        darkTheme && "dark:bg-[#181a1b]"
-      } ${lightTheme && "bg-white"}`}
+        darkTheme && "dark:bg-[#181a1b]" || lightTheme && "bg-white"}`}
     >
       <div>
-        <h1 className="dark:text-white">
+        <h1 className={`${darkTheme && 'opacity-1 text-white' || lightTheme && 'opacity-1 text-black' }`}>
           Yours Theme has been changed to {props.isDark ? "Dark" : "Light"} Mode
         </h1>
             <div className={`transition ease-in-out delay-750  ${darkTheme || lightTheme ? 'opacity-1' : 'opacity-0'}`}>
