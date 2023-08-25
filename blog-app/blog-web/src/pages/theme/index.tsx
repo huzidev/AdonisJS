bedb406a9472a4e53be8a7c3bc0212004bb87f08
huzidev <huzaifa.iqdev@gmail.com>
2023-08-25 23:17:20 +0500

    ts types

--- blog-app/blog-web/src/pages/theme/index.tsx
+++ blog-app/blog-web/src/pages/theme/index.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
-import { PropsType } from "./types";
 
+interface PropsType {
+    isDark: boolean;
+}
 export default function ToggleThemePopUpPage(props: PropsType): JSX.Element {
   const [darkTheme, setDarkTheme] = useState<boolean>(false);
   const [lightTheme, setLightTheme] = useState<boolean>(false);
@@ -23,7 +25,7 @@ export default function ToggleThemePopUpPage(props: PropsType): JSX.Element {
         darkTheme && "dark:bg-[#181a1b]" || lightTheme && "bg-white"}`}
     >
       <div>
-        <h1 className={`transition ease-in-out duration-700 ${props.isDark ? 'text-black' : 'text-white'} ${darkTheme && 'text-white' || lightTheme && 'text-black'}`}>
+        <h1 className={`transition ease-in-out duration-700 ${props.isDark ? 'text-blue' : 'text-white'} ${darkTheme && 'text-white' || lightTheme && 'text-black'}`}>
           Yours Theme has been changed to {props.isDark ? "Dark" : "Light"} Mode
         </h1>
             <div className={`transition ease-in-out delay-750  ${darkTheme || lightTheme ? 'opacity-1' : 'opacity-0'}`}>
