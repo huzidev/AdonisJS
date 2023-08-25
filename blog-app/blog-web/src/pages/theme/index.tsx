bb7450992461526ac2ab7b33ce8feab16782fb48
huzidev <huzaifa.iqdev@gmail.com>
2023-08-25 23:19:23 +0500

    transition error for text fix

--- blog-app/blog-web/src/pages/theme/index.tsx
+++ blog-app/blog-web/src/pages/theme/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import './styles.css';
 
 interface PropsType {
     isDark: boolean;
@@ -21,14 +22,14 @@ export default function ToggleThemePopUpPage(props: PropsType): JSX.Element {
 
   return (
     <div
-      className={`h-screen flex justify-center items-center transition ease-in-out duration-700 ${props.isDark ? 'bg-white' : 'bg-[#181a1b]'} ${
+      className={`h-screen flex justify-center items-center animation ${props.isDark ? 'bg-white' : 'bg-[#181a1b]'} ${
         darkTheme && "dark:bg-[#181a1b]" || lightTheme && "bg-white"}`}
     >
       <div>
-        <h1 className={`transition ease-in-out duration-700 ${props.isDark ? 'text-blue' : 'text-white'} ${darkTheme && 'text-white' || lightTheme && 'text-black'}`}>
+        <h1 className={`animation  ${props.isDark ? 'text-blue' : 'text-white'} ${darkTheme && 'text-white' || lightTheme && 'text-black'}`}>
           Yours Theme has been changed to {props.isDark ? "Dark" : "Light"} Mode
         </h1>
-            <div className={`transition ease-in-out delay-750  ${darkTheme || lightTheme ? 'opacity-1' : 'opacity-0'}`}>
+            <div className={`animation   ${darkTheme || lightTheme ? 'opacity-1' : 'opacity-0'}`}>
               <h1 className="text-white dark:text-white">
                 {darkTheme && 'Moon Icon'}
                 {lightTheme && 'Sun Icon'}
