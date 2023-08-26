135a58cb511901d0e8ed8dff83c7a48dfb7e76cb
huzidev <huzaifa.iqdev@gmail.com>
2023-08-26 22:46:10 +0500

    dark-mode transition error fix

--- blog-app/blog-web/src/pages/theme/index.tsx
+++ blog-app/blog-web/src/pages/theme/index.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
-import './styles.css';
+import "./styles.css";
 
 interface PropsType {
-    isDark: boolean;
+  isDark: boolean;
 }
 export default function ToggleThemePopUpPage(props: PropsType): JSX.Element {
   const [darkTheme, setDarkTheme] = useState<boolean>(false);
@@ -17,41 +17,43 @@ export default function ToggleThemePopUpPage(props: PropsType): JSX.Element {
     } else {
       setTimeout(() => {
         setLightTheme(true);
-      }, 200)
+      }, 200);
     }
     setTimeout(() => {
       setIsClicked(true);
-    }, 200)
+    }, 200);
   }, [props.isDark]);
 
   return (
-    // <div
-    //   className={`h-screen flex justify-center items-center animation ${props.isDark ? 'bg-white' : 'bg-[#181a1b]'} ${
-    //     darkTheme && "dark:bg-[#181a1b]" || lightTheme && "bg-white"}`}
-    // >
-      // <div
-      // className={`h-screen flex justify-center items-center animation ${props.isDark ? 'bg-white' : 'bg-[#181a1b]'} ${
-      //   (darkTheme && !lightTheme) && "bg-[#181a1b]" || (lightTheme && !darkTheme) && "bg-white"}`}
-      // >
-      <div className={`h-screen flex justify-center items-center animation ${
-        props.isDark ? (!darkTheme && 'bg-white') || (darkTheme && 'bg-[#181a1b]') : (!lightTheme && 'bg-[#181a1b]') || (lightTheme && 'bg-white')
+    <div
+      className={`h-screen flex justify-center items-center animation ${
+        props.isDark
+          ? (!darkTheme && "bg-white") || (darkTheme && "bg-[#181a1b]")
+          : (!lightTheme && "bg-[#181a1b]") || (lightTheme && "bg-white")
       }`}
-
-      >
+    >
       <div>
-        {/* <h1 className={`animation  ${props.isDark || (!darkTheme || !lightTheme) ? 'text-black' : 'text-white'} ${darkTheme && 'text-white' || lightTheme && 'text-blue-300'}`}> */}
         <h1
           className={`animation ${
-            props.isDark ? (!darkTheme && 'text-black') || (darkTheme && 'text-white') : (!lightTheme && 'text-white') || (lightTheme && 'text-black') 
+            props.isDark
+              ? (!darkTheme && "text-black") || (darkTheme && "text-white")
+              : (!lightTheme && "text-white") || (lightTheme && "text-black")
           }`}
         >
           Yours Theme has been changed to {props.isDark ? "Dark" : "Light"} Mode
         </h1>
-          <div className="container">
-              <label htmlFor="theme-toggle" className={`custom-checkbox animation ${
-                props.isDark ? (!darkTheme && 'bg-orange-400') || (darkTheme && 'bg-transparent checked-box-shadow') : (!lightTheme && 'bg-transparent checked-box-shadow') || (lightTheme && 'bg-orange-400')
-              }`}></label>
-          </div>
+        <div className="container">
+          <label
+            htmlFor="theme-toggle"
+            className={`custom-checkbox animation ${
+              props.isDark
+                ? (!darkTheme && "bg-orange-400") ||
+                  (darkTheme && "bg-transparent checked-box-shadow")
+                : (!lightTheme && "bg-transparent checked-box-shadow") ||
+                  (lightTheme && "bg-orange-400")
+            }`}
+          ></label>
+        </div>
       </div>
     </div>
   );
