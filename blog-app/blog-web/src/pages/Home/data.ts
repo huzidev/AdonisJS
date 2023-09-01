859e2d8562068be38a87fc7b93c31ba6be5549d5
huzidev <huzaifa.iqdev@gmail.com>
2023-09-01 15:11:53 +0500

    data for mapping

--- blog-app/blog-web/src/pages/Home/data.ts
+++ blog-app/blog-web/src/pages/Home/data.ts
@@ -1,17 +1,52 @@
 export const slides = [
     {
-      src: "adonis.png",
-      heading: "Image 1",
-      description: "This is the description for Image 1.",
+      src: "astronomy.jpg",
+      heading: "Astronomy",
+      description: "Get to know more about Astronomy by exploring blogs about Astronomy",
     },  
     {
-      src: "react.png",
-      heading: "Image 2",
-      description: "This is the description for Image 2.",
+      src: "food.jpg",
+      heading: "Food",
+      description: "Get to know more about Foods and by exploring blogs about Foods",
     },
     {
-      src: "redux.png",
-      heading: "Image 3",
-      description: "This is the description for Image 3.",
+      src: "fitness.jpg",
+      heading: "Fitness",
+      description: "Get to know more about Fitness and by exploring blogs about Fitness",
+    },
+    {
+      src: "pets.jpg",
+      heading: "Pets",
+      description: "Get to know more about Pets and by exploring blogs about Pets",
+    },
+    {
+      src: "travel.jpg",
+      heading: "Travel",
+      description: "Get to know more about Travel and by exploring blogs about Travel",
+    },
+    {
+      src: "sports.jpg",
+      heading: "Sports",
+      description: "Get to know more about Sports and by exploring blogs about Sports",
+    },
+    {
+      src: "lifestyle.jpg",
+      heading: "Lifestyle",
+      description: "Get to know more about Lifestyle and by exploring blogs about Lifestyle",
+    },
+    {
+      src: "nature.jpg",
+      heading: "Nature",
+      description: "Get to know more about Foods and by exploring blogs about Nature",
+    },
+    {
+      src: "business.jpg",
+      heading: "Business",
+      description: "Get to know more about Business and by exploring blogs about Business",
+    },
+    {
+      src: "sci-fi.jpg",
+      heading: "Sci-Fi",
+      description: "Get to know more about Sci-Fi and by exploring blogs about Sci-Fi",
     },
   ];
