f296a7aed14d857a19b3f4d4b32e97475997a4bd
huzidev <huzaifa.iqdev@gmail.com>
2023-09-01 11:48:42 +0500

    updating UI

--- blog-app/blog-web/src/pages/Home/data.ts
+++ blog-app/blog-web/src/pages/Home/data.ts
@@ -1,20 +1,17 @@
 export const slides = [
-  {
-    image: 'adonis'
-  },
-  {
-    image: 'mysql'
-  },
-  {
-    image: 'react'
-  },
-  {
-    image: 'redux'
-  },
-  {
-    image: 'tailwind'
-  },
-  {
-    image: 'typescript'
-  }
-];
+    {
+      src: "adonis.png",
+      heading: "Image 1",
+      description: "This is the description for Image 1.",
+    },  
+    {
+      src: "react.png",
+      heading: "Image 2",
+      description: "This is the description for Image 2.",
+    },
+    {
+      src: "redux.png",
+      heading: "Image 3",
+      description: "This is the description for Image 3.",
+    },
+  ];
--- blog-app/blog-web/src/pages/Home/index.tsx
+++ blog-app/blog-web/src/pages/Home/index.tsx
@@ -1,14 +1,27 @@
-import ROUTE_PATHS from "Router/paths";
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import { useAuth } from "store/auth";
+import { slides } from "./data";
 
 export default function HomePage() {
   const auth = useAuth();
   const user = auth.state.user;
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex: any) =>
+      prevIndex === 0 ? slides.length - 1 : prevIndex - 1
+    );
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex: any) =>
+      prevIndex === slides.length - 1 ? 0 : prevIndex + 1
+    );
+  };
 
   return (
     <div>
-      <div 
+      {/* <div 
           className="w-11/12 my-8 mx-auto  border rounded-lg shadow bg-gray-800 border-gray-700 dark:bg-#19212c">
           <div className="p-5">
             <h1 className="mb-2 text-3xl text-center font-bold tracking-tight text-white">
@@ -53,7 +66,39 @@ export default function HomePage() {
               </section>
             </main>
           </div>
-        </div>
+        </div> */}
+      <div className="relative">
+        <button
+          className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full"
+          onClick={prevSlide}
+        >
+          &lt;
+        </button>
+        <button
+          className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full"
+          onClick={nextSlide}
+        >
+          &gt;
+        </button>
+        {slides.map((image, index) => (
+          <div
+            key={index}
+            className={`carousel-slide h-screen w-full flex ${
+              index === currentIndex ? "visible" : "hidden"
+            }`}
+          >
+            <img
+              src={`/assets/${image.src}`}
+              alt={image.heading}
+              className="w-full h-full object-cover"
+            />
+            <div className="absolute left-0 top-0 p-6 text-white">
+              <h2 className="text-4xl font-bold">{image.heading}</h2>
+              <p className="mt-2">{image.description}</p>
+            </div>
+          </div>
+        ))}
       </div>
-  )
+    </div>
+  );
 }
