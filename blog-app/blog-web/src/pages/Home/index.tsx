7a93bcb9d644eff0348bafbf0d00b990a1df81ef
huzidev <huzaifa.iqdev@gmail.com>
2023-09-02 11:02:25 +0500

    updating carousel affect

--- blog-app/blog-web/src/pages/Home/index.tsx
+++ blog-app/blog-web/src/pages/Home/index.tsx
@@ -1,50 +1,57 @@
 import ROUTE_PATHS from "Router/paths";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "store/auth";
 import { slides } from "./data";
+import "./styles.css";
 
 export default function HomePage() {
-  const auth = useAuth();
-  const user = auth.state.user;
   const navigate = useNavigate();
   const [currentIndex, setCurrentIndex] = useState(0);
   const [height, setHeight] = useState(window.innerHeight - 64);
   const [width, setWidth] = useState(window.innerWidth);
 
   useEffect(() => {
-    // Function to update height and width based on window dimensions
     const updateDimensions = () => {
       setHeight(window.innerHeight - 64);
       setWidth(window.innerWidth);
     };
 
-    // Initial update
     updateDimensions();
-
-    // Attach event listener to update dimensions on window resize
     window.addEventListener("resize", updateDimensions);
 
-    // Remove event listener when component unmounts
     return () => {
       window.removeEventListener("resize", updateDimensions);
     };
   }, []);
 
   const prevSlide = () => {
-    setCurrentIndex((prevIndex: any) =>
+    setCurrentIndex((prevIndex) =>
       prevIndex === 0 ? slides.length - 1 : prevIndex - 1
     );
   };
 
   const nextSlide = () => {
-    setCurrentIndex((prevIndex: any) =>
+    setCurrentIndex((prevIndex) =>
       prevIndex === slides.length - 1 ? 0 : prevIndex + 1
     );
   };
+
+  // Function to advance to the next slide every 4.5 seconds
+  const autoAdvance = () => {
+    setTimeout(() => {
+      nextSlide();
+      autoAdvance();
+    }, 4500);
+  };
+
+  useEffect(() => {
+    autoAdvance(); // Start the auto-advance when the component mounts
+  }, []);
+
   return (
     <div>
       <div className="relative">
+        {/* Buttons for previous and next slides */}
         <button
           className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full"
           onClick={prevSlide}
@@ -53,43 +60,47 @@ export default function HomePage() {
         </button>
         <button
           className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full"
-          onClick={nextSlide} 
+          onClick={nextSlide}
         >
           &gt;
         </button>
-        {slides.map((image, index: number) => (
+        {slides.map((image, index) => (
           <div
             key={index}
-            style={{ 
-              height: `${height}px`, 
-              width: `${width}px`, 
+            style={{
+              height: `${height}px`,
+              width: `${width}px`,
             }}
-            className={` ${ 
+            className={`${
               index === currentIndex ? "visible" : "hidden"
             }`}
           >
             <img
-              src={require(`assets/${image.src}`)}
-              alt={image.heading} 
-              style={{ height: `${height}px`, width: `${width}px`}}
+              src={require(`assets/${image.src}`)} // Added .default to fix image loading
+              alt={image.heading}
+              style={{ height: `${height}px`, width: `${width}px` }}
+              className={`fade-in`} // Apply the fade-in animation
             />
             <div className="w-lg m-auto">
               <div className="absolute top-1/3 m-auto text-white">
-                <h2 className="text-4xl font-bold font-outline-2">{image.heading}</h2>
-                <p className="mt-2 text-xl font-outline-2">Explore 
-                  <span 
+                <h2
+                  className={`text-4xl font-bold font-outline-2 fade-in`}
+                >
+                  {image.heading}
+                </h2>
+                <p
+                  className={`mt-2 text-xl font-outline-2 fade-in`}
+                >
+                  Explore
+                  <span
                     className="text-blue-500 cursor-pointer"
                     onClick={() => navigate(ROUTE_PATHS.ARTICLES)}
                   >
-                    {" "}blogs{" "}   
+                    {" "}
+                    blogs{" "}
                   </span>
-                  {/* <Link 
-                    to={ROUTE_PATHS.ARTICLES}
-                    className="text-blue-500 cursor-pointer"
-                  >
-                    {" "}blogs{" "}   
-                  </Link> */}
-                about {image.heading}</p>
+                  about {image.heading}
+                </p>
               </div>
             </div>
           </div>
