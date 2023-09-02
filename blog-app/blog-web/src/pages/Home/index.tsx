3a83fbe128413aaf54e7aa9b3312a1247f3d9b5c
huzidev <huzaifa.iqdev@gmail.com>
2023-09-02 11:28:54 +0500

    fade affects tailwind

--- blog-app/blog-web/src/pages/Home/index.tsx
+++ blog-app/blog-web/src/pages/Home/index.tsx
@@ -18,10 +18,7 @@ export default function HomePage() {
 
     updateDimensions();
     window.addEventListener("resize", updateDimensions);
-
-    return () => {
-      window.removeEventListener("resize", updateDimensions);
-    };
+    return () => window.removeEventListener("resize", updateDimensions);
   }, []);
 
   const prevSlide = () => {
@@ -36,33 +33,74 @@ export default function HomePage() {
     );
   };
 
-  // Function to advance to the next slide every 4.5 seconds
-  const autoAdvance = () => {
-    setTimeout(() => {
-      nextSlide();
-      autoAdvance();
-    }, 4500);
-  };
+  // const autoAdvance = () => {
+  //   setTimeout(() => {
+  //     nextSlide();
+  //     autoAdvance();
+  //   }, 4500);
+  // };
 
-  useEffect(() => {
-    autoAdvance(); // Start the auto-advance when the component mounts
-  }, []);
+  // useEffect(() => {
+  //   autoAdvance(); // Start the auto-advance when the component mounts
+  // }, []);
 
   return (
     <div>
       <div className="relative">
         {/* Buttons for previous and next slides */}
         <button
-          className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full"
+          className="absolute bottom-0 left-0 top-0 z-[1] flex w-[10%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition ease-in-out duration-500 hover:bg-[rgba(0,0,0,0.2)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+          type="button"
+          data-te-target="#carouselExampleCaptions"
+          data-te-slide="prev"
           onClick={prevSlide}
         >
-          &lt;
+          <span className="inline-block h-8 w-8">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.75 19.5L8.25 12l7.5-7.5"
+              />
+            </svg>
+          </span>
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Previous
+          </span>
         </button>
         <button
-          className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full"
           onClick={nextSlide}
+          className="absolute bottom-0 right-0 top-0 z-[1] flex w-[10%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+          type="button"
+          data-te-target="#carouselExampleCaptions"
+          data-te-slide="next"
         >
-          &gt;
+          <span className="inline-block h-8 w-8">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </span>
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Next
+          </span>
         </button>
         {slides.map((image, index) => (
           <div
@@ -71,9 +109,7 @@ export default function HomePage() {
               height: `${height}px`,
               width: `${width}px`,
             }}
-            className={`${
-              index === currentIndex ? "visible" : "hidden"
-            }`}
+            className={`${index === currentIndex ? "visible" : "hidden"}`}
           >
             <img
               src={require(`assets/${image.src}`)} // Added .default to fix image loading
@@ -83,14 +119,10 @@ export default function HomePage() {
             />
             <div className="w-lg m-auto">
               <div className="absolute top-1/3 m-auto text-white">
-                <h2
-                  className={`text-4xl font-bold font-outline-2 fade-in`}
-                >
+                <h2 className={`text-4xl font-bold font-outline-2 fade-in`}>
                   {image.heading}
                 </h2>
-                <p
-                  className={`mt-2 text-xl font-outline-2 fade-in`}
-                >
+                <p className={`mt-2 text-xl font-outline-2 fade-in`}>
                   Explore
                   <span
                     className="text-blue-500 cursor-pointer"
