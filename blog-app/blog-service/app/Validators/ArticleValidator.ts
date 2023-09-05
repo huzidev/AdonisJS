3e19a8bef1c3965ed205b85a4c2a8a7fbe7a8a71
huzidev <huzaifa.iqdev@gmail.com>
2023-09-05 11:20:05 +0500

    article validation

--- blog-app/blog-service/app/Validators/ArticleValidator.ts
+++ blog-app/blog-service/app/Validators/ArticleValidator.ts
@@ -6,8 +6,8 @@ export class CreateArticle {
   public schema = schema.create({
     title: schema.string({ trim: true }, [rules.minLength(6), rules.regex(/^[a-zA-Z0-9\s]+$/)]),
     content: schema.string({ trim: true }, [rules.minLength(12)]),
-    image: schema.string(),
-    category: schema.enum.optional(Article.categories)
+    category: schema.enum.optional(Article.categories),
+    image: schema.string()
   })
 
   public messages: CustomMessages = {
