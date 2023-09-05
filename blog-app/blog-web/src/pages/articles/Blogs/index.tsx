52b08d598bfa44527b49d0915b2f4b0adcfc1ca6
huzidev <huzaifa.iqdev@gmail.com>
2023-09-05 11:43:38 +0500

    filters sort for categories

--- blog-app/blog-web/src/pages/articles/Blogs/index.tsx
+++ blog-app/blog-web/src/pages/articles/Blogs/index.tsx
@@ -33,7 +33,7 @@ export default function ViewBlogsPage(): JSX.Element {
       <div>
         {sortValue.value && (
           <button
-            className="text-white mr-5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
+            className="text-white mr-5 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
             onClick={() => handleSort('')}
           >
             Reset Filters
@@ -46,7 +46,7 @@ export default function ViewBlogsPage(): JSX.Element {
               <button
                 id="dropdownDefaultButton"
                 data-dropdown-toggle="dropdown"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
+                className="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
                 type="button"
                 onClick={() => setDropDown(!dropDown)}
               >
