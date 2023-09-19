a3f8cc4cd72983e893f76e970285d209bf4dd96d
huzidev <huzaifa.iqdev@gmail.com>
2023-09-20 00:30:20 +0500

    blogs message added

--- blog-app/blog-web/src/pages/articles/ShowBlogs/index.tsx
+++ blog-app/blog-web/src/pages/articles/ShowBlogs/index.tsx
@@ -46,19 +46,23 @@ export default function ShowBlogs(props: any): JSX.Element {
     <div className="w-[1500px] m-auto flex flex-col">
       <div className="relative mb-8 mt-2.5 bg-blue-100">
         {
-          isViewProfile && (
             <div className="absolute">
               <h1 className="text-2xl font-bold tracking-tight dark:text-white">
                 {/* if role is user then user can't upload the blogs hence show blogs Liked by you else if blogger loggedIn then show blogs Uploaded by You */}
                 {/* isLoadingUser ? "" so when userState is in loading then don't show anything otherwise it'll show Blogs Uploaded while user state is in loading */}
-                {props.isLoading
-                  ? ''
-                  : (isUser && isMe) || props.isRole === "user"
-                  ? `Blogs Liked By ${isMe ? "You" : props.userDetails.username}`
-                  : `Blogs Uploaded By ${!isMe ? props.userDetails.username : "You"}`}
+                {
+                  isViewProfile ? (
+                    props.isLoading
+                      ? ''
+                      : (isUser && isMe) || props.isRole === "user"
+                      ? `Blogs Liked By ${isMe ? "You" : props.userDetails.username}`
+                      : `Blogs Uploaded By ${!isMe ? props.userDetails.username : "You"}`
+                  ) : (
+                    (!props.isFilter ? 'All' : startCase(props.isFilter)) + ' Blogs'
+                  )
+                }
               </h1>
             </div>
-          )
         }
         {(props.allBlogs.length === 0 && props.sortValue.value) || props.userDetails?.role !== "user" && (
           <div>
