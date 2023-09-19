2d4437051939693d8a907a41648a392035693575
huzidev <huzaifa.iqdev@gmail.com>
2023-09-19 20:55:47 +0500

    is filters state

--- blog-app/blog-web/src/pages/user/ViewProfile/hooks.ts
+++ blog-app/blog-web/src/pages/user/ViewProfile/hooks.ts
@@ -96,6 +96,7 @@ export function useViewProfilePageHook(): ViewProfileStateHandler {
       // setIsRole is for checking conditions when user has clicked on user's profile whose role is not user then fetch blogsById of that user
       setIsRole(userState.getUser.data?.role);
       // not using isRole for checking isRole === user because when the clickedUser role is storing in isRole state the JS will run rest of that code and then isRole will be empty
+      // therefore directly checking the role validation with userState.getUser.data?.role
       if (userState.getUser.data?.role === "user" && !isMe) {
         const payloadForClicked: any = {
           // if clicked profile is of user then fetch
