1c5e1e88a9ab930824b99bddd51cbe2ecf6c7d15
huzidev <huzaifa.iqdev@gmail.com>
2023-09-20 19:24:04 +0500

    isBanned user page fix

--- blog-app/blog-web/src/pages/user/BannedUser/index.tsx
+++ blog-app/blog-web/src/pages/user/BannedUser/index.tsx
@@ -11,10 +11,10 @@ export default function BannedUserPage(): JSX.Element {
     Navigate(ROUTE_PATHS.HOME); // Redirect to home page after signing out
   }
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="max-w-md mx-auto p-6 bg-white shadow-lg rounded-lg">
-        <h1 className="text-4xl font-bold mb-6">Banned User</h1>
-        <p className="text-lg text-gray-600 mb-8">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-[#181a1b]">
+      <div className="max-w-md mx-auto p-6 bg-white shadow-lg rounded-lg dark:bg-gray-800">
+        <h1 className="text-4xl font-bold mb-6 dark:text-white">Banned User</h1>
+        <p className="text-lg text-gray-600 mb-8 dark:text-white">
           Cannot process, You are banned from the web.
         </p>
         <button
