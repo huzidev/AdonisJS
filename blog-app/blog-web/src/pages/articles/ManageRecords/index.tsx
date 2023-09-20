2cd8de3f5d05dc2a9fd3be4a844a159103b05096
huzidev <huzaifa.iqdev@gmail.com>
2023-09-20 18:13:05 +0500

    create user button

--- blog-app/blog-web/src/pages/articles/ManageRecords/index.tsx
+++ blog-app/blog-web/src/pages/articles/ManageRecords/index.tsx
@@ -36,6 +36,30 @@ export default function ManageRecords(props: any) {
             }
           </span>
         </div>
+        {
+            isUsersPage && (
+                <div className="flex items-center justify-between">
+                    <div className="ml-10 space-x-8 lg:ml-40">
+                        <button
+                        className="flex items-center gap-2 rounded-md bg-blue-600 px-4 py-2 text-sm font-semibold text-white focus:outline-none focus:ring hover:bg-blue-700"
+                        title="Create User"
+                        onClick={() => navigate(ROUTE_PATHS.USER_CREATE)}
+                        >
+                        <svg
+                            className="w-6 h-6 text-gray-800 dark:text-white"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 18 20"
+                        >
+                            <path d="M16 0H4a2 2 0 0 0-2 2v1H1a1 1 0 0 0 0 2h1v2H1a1 1 0 0 0 0 2h1v2H1a1 1 0 0 0 0 2h1v2H1a1 1 0 0 0 0 2h1v1a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4.5a3 3 0 1 1 0 6 3 3 0 0 1 0-6ZM13.929 17H7.071a.5.5 0 0 1-.5-.5 3.935 3.935 0 1 1 7.858 0 .5.5 0 0 1-.5.5Z" />
+                        </svg>
+                        Create User
+                        </button>
+                    </div>
+                </div>
+            )
+        }
       </div>
       <div className="overflow-y-hidden rounded-lg border">
         <div className="overflow-x-auto">
