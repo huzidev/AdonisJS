15b07de5a35408f212e233c142b82532f09aed35
huzidev <huzaifa.iqdev@gmail.com>
2023-09-21 11:47:23 +0500

    Not found page buttons

--- blog-app/blog-web/src/pages/NotFound/index.tsx
+++ blog-app/blog-web/src/pages/NotFound/index.tsx
@@ -5,7 +5,7 @@ export default function NotFoundPage(): JSX.Element {
   const navigate = useNavigate();
   return (
     <div
-    className="
+      className="
         flex
         items-center
         justify-center
@@ -15,17 +15,17 @@ export default function NotFoundPage(): JSX.Element {
         dark:border-gray-700
       "
     >
-      <div className="px-40 py-20 rounded-md shadow-xl bg-white border-gray-200 dark:bg-gray-900">
+      <div className="px-40 py-20 rounded-md shadow-xl bg-gray-800 border-gray-700">
         <div className="flex flex-col items-center">
           <h1 className="font-bold text-blue-600 text-9xl">404</h1>
-          <h6 className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
+          <h6 className="mb-2 text-2xl font-bold text-center text-gray-300 md:text-3xl">
             <span className="text-red-500">Oops!</span> Page not found
           </h6>
           <p className="mb-8 text-center text-gray-400 md:text-lg">
             Sorry, the page you requested for does not exist.
           </p>
-          <button 
-            className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" 
+          <button
+            className="px-6 py-2 bg-blue-600 text-white rounded-md font-semibold hover:bg-blue-700"
             onClick={() => navigate(ROUTE_PATHS.ARTICLES)}
           >
             Back Home
