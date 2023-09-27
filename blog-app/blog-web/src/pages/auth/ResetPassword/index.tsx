32b55c9edd043628dfa0f0c2d25f0d791f9fe17f
huzidev <huzaifa.iqdev@gmail.com>
2023-09-27 14:19:58 +0500

    spacing fix

--- blog-app/blog-web/src/pages/auth/ResetPassword/index.tsx
+++ blog-app/blog-web/src/pages/auth/ResetPassword/index.tsx
@@ -75,8 +75,8 @@ export default function ResetPasswordPage(): JSX.Element {
 
   return (
     <div className="main">
-      <div className="relative bg-white px-6 mt-10 py-10 shadow-xl mx-auto w-full max-w-lg rounded-2xl dark:bg-gray-900">
-        <div className="mx-auto flex w-full max-w-md flex-col space-y-16">
+      <div className="relative bg-white px-6 py-10 shadow-xl mx-auto max-w-lg rounded-2xl dark:bg-gray-900">
+        <div className="mx-auto flex max-w-md flex-col space-y-6">
           <div className="flex flex-col items-center justify-center text-center space-y-2">
             <p className="font-semibold text-3xl dark:text-white">
               Reset Password
