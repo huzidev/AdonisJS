bae1dd0e5b7ab15ceb5e86dab9e4b4fd59ed618a
huzidev <huzaifa.iqdev@gmail.com>
2023-10-01 18:47:31 +0500

    contact user icon

--- blog-app/blog-web/src/pages/user/ManageUsers/index.tsx
+++ blog-app/blog-web/src/pages/user/ManageUsers/index.tsx
@@ -1,3 +1,4 @@
+import ContactPageIcon from '@mui/icons-material/ContactPage';
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
@@ -47,15 +48,7 @@ export default function ManageUsersPage(): JSX.Element {
               title="Create User"
               onClick={() => navigate(ROUTE_PATHS.USER_CREATE)}
             >
-              <svg
-                className="w-6 h-6 text-gray-800 dark:text-white"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 18 20"
-              >
-                <path d="M16 0H4a2 2 0 0 0-2 2v1H1a1 1 0 0 0 0 2h1v2H1a1 1 0 0 0 0 2h1v2H1a1 1 0 0 0 0 2h1v2H1a1 1 0 0 0 0 2h1v1a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4.5a3 3 0 1 1 0 6 3 3 0 0 1 0-6ZM13.929 17H7.071a.5.5 0 0 1-.5-.5 3.935 3.935 0 1 1 7.858 0 .5.5 0 0 1-.5.5Z" />
-              </svg>
+              <ContactPageIcon />
               Create User
             </button>
           </div>
